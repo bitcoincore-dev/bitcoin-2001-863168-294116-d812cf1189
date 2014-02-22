3241ef4b304a871770a96b2af52c9d00a2aea382
Luke Dashjr <luke-jr+git@utopios.org>
2014-02-22 02:41:01 +0000

    Allow disabling "non-standard" checks on mainnet

--- src/main.cpp
+++ src/main.cpp
@@ -1491,7 +1491,7 @@ bool AcceptToMemoryPoolWorker(CTxMemPool& pool, CValidationState& state, const C
         }
 
         unsigned int scriptVerifyFlags = STANDARD_SCRIPT_VERIFY_FLAGS;
-        if (!Params().RequireStandard()) {
+        if (!Params().RequireStandardDefault()) {
             scriptVerifyFlags = GetArg("-promiscuousmempoolflags", scriptVerifyFlags);
         }
 
