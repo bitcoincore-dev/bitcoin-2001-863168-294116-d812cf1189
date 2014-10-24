f4514380ba89fea04675b8c09a1d718fe897e491
Suhas Daftuar <sdaftuar@gmail.com>
2014-10-24 16:20:05 -0400

    Testing for the ccl datalogger.

---
+++ qa/rpc-tests/testlogging.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+#
+# Test datalogging code
+#
+
+from test_framework import BitcoinTestFramework
+from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+from util import *
+
+class DataLoggingTest(BitcoinTestFramework):
+
+    def setup_network(self, test_dir):
+        nodes = []
+        nodes.append(start_node(0, test_dir,
+                            ["-writemempool", "-dlogdir=" + test_dir]))
+
+        
+        nodes.append(start_node(1, test_dir));
+        nodes.append(start_node(2, test_dir));
+        connect_nodes(nodes[1], 0)
+        connect_nodes(nodes[1], 2)
+
+        sync_blocks(nodes)
+        return nodes
+        
+
+    def run_test(self, nodes):
+        # Prime the memory pool with pairs of transactions
+        # (high-priority, random fee and zero-priority, random fee)
+        min_fee = Decimal("0.001")
+        fees_per_kb = [];
+        txnodes = [nodes[1], nodes[2]]
+        for i in range(12):
+            (txid, txhex, fee) = random_transaction(txnodes, Decimal("1.1"),
+                                                            min_fee, min_fee, 20)
+
+        sync_mempools(nodes)
+
+        # Mine blocks with node1 until the memory pool clears:
+        count_start = nodes[1].getblockcount()
+        while len(nodes[1].getrawmempool()) > 0:
+            nodes[1].setgenerate(True, 1)
+            sync_blocks(nodes)
+
+        for i in range(12):
+            (txid, txhex, fee) = random_transaction(txnodes, Decimal("1.1"),
+                                                            min_fee, min_fee, 20)
+        sync_mempools(nodes)
+        stop_nodes(nodes)
+
+        time.sleep(5) # Need to wait for files to be written out
+
+        today = time.strftime("%Y%m%d")
+
+        # Check that the size of the tx log is correct
+        alltx = subprocess.check_output([ "txprinter", self.options.tmpdir+"/tx."+today])
+        txheader = re.findall('CTransaction', alltx)
+        if len(txheader) != 24:
+                raise AssertionError("Wrong number of logged tx's, expected 24, got %d"%(len(txheader)))
+    
+        # Check that the size of the block log is correct
+        allblocks = subprocess.check_output([ "blockprinter", self.options.tmpdir+"/block."+today])
+        blockheaders = re.findall('CBlock', allblocks)
+        if len(blockheaders) != 1:
+                raise AssertionError("Wrong number of logged blocks, expected 1, got %d" % len(blockheaders))
+
+        # Check that the size of the mempool log is correct
+        allmptx = subprocess.check_output([ "mempoolprinter", self.options.tmpdir+"/mempool."+today])
+        mpheaders = re.findall('CTransaction', allmptx)
+        if len(mpheaders) != 12:
+                raise AssertionError("Wrong number of mempool entries, expected 12, got %d" % len(mpheaders))
+
+if __name__ == '__main__':
+    DataLoggingTest().main()
