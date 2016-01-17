0ece5ffcce4c0fb08aa43060959f537ac561e0e3
Luke Dashjr <luke-jr+git@utopios.org>
2016-01-17 03:24:00 +0000

    AcceptToMemoryPool: Refactor overrideable rejections (previously fOverrideMempoolLimit) with flexible std::set of rejections to ignore

--- src/policy/policy.h
+++ src/policy/policy.h
@@ -58,13 +58,13 @@ bool IsStandard(const CScript& scriptPubKey, txnouttype& whichType, const bool w
      * Check for standard transaction types
      * @return True if all outputs (scriptPubKeys) use only standard transaction forms
      */
-bool IsStandardTx(const CTransaction& tx, std::string& reason, const bool witnessEnabled = false);
+bool IsStandardTx(const CTransaction& tx, std::string& reason, const bool witnessEnabled = false, const std::set<std::string>& setIgnoreRejects=std::set<std::string>());
     /**
      * Check for standard transaction types
      * @param[in] mapInputs    Map of previous transactions that have outputs we're spending
      * @return True if all inputs (scriptSigs) use only standard transaction forms
      */
-bool AreInputsStandard(const CTransaction& tx, const CCoinsViewCache& mapInputs, std::string& reason);
+bool AreInputsStandard(const CTransaction& tx, const CCoinsViewCache& mapInputs, std::string& reason, const std::set<std::string>& setIgnoreRejects=std::set<std::string>());
 
 /** Compute the virtual transaction size (weight reinterpreted as bytes). */
 int64_t GetVirtualTransactionSize(int64_t nWeight);
