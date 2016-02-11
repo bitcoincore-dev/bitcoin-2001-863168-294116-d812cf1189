9bcd8e190069f519e1af1f75f9d12bb5098234d4
Luke Dashjr <luke-jr+git@utopios.org>
2016-02-11 05:58:01 +0000

    Qt/Options: Expose maxuploadtarget in GUI using rwconf

--- src/qt/optionsmodel.h
+++ src/qt/optionsmodel.h
@@ -56,6 +56,7 @@ public:
         DatabaseCache,          // int
         SpendZeroConfChange,    // bool
         Listen,                 // bool
+        maxuploadtarget,
         OptionIDRowCount,
     };
 
