d7c32d303202b7b49f15c8aa08f0bb6e942a4584
Luke Dashjr <luke-jr+git@utopios.org>
2016-02-15 07:09:36 +0000

    Qt/Options: Configure maxorphantx using rwconf

--- src/main.h
+++ src/main.h
@@ -374,6 +374,8 @@ void UpdateCoins(const CTransaction& tx, CCoinsViewCache& inputs, int nHeight);
 /** Context-independent validity checks */
 bool CheckTransaction(const CTransaction& tx, CValidationState& state);
 
+unsigned int LimitOrphanTxSize(unsigned int nMaxOrphans) EXCLUSIVE_LOCKS_REQUIRED(cs_main);
+
 /**
  * Check if transaction is final and can be included in a block with the
  * specified height and time. Consensus critical.
