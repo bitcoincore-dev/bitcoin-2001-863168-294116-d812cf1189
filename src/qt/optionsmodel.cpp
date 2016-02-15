f026f1187e969ec041b396c718daf6219b878520
Luke Dashjr <luke-jr+git@utopios.org>
2016-02-15 08:56:08 +0000

    Qt/Options: Configure mempoolexpiry using rwconf

--- src/qt/optionsmodel.cpp
+++ src/qt/optionsmodel.cpp
@@ -15,7 +15,7 @@
 #include "chainparams.h"
 #include "init.h"
 #include "policy/policy.h" // for DEFAULT_MAX_MEMPOOL_SIZE
-#include "validation.h" // For DEFAULT_SCRIPTCHECK_THREADS
+#include "validation.h" // For DEFAULT_SCRIPTCHECK_THREADS, DEFAULT_MEMPOOL_EXPIRY
 #include "net.h"
 #include "net_processing.h"  // for maxorphantx
 #include "netbase.h"
@@ -293,6 +293,8 @@ QVariant OptionsModel::data(const QModelIndex & index, int role) const
             return qlonglong(gArgs.GetArg("-maxorphantx", DEFAULT_MAX_ORPHAN_TRANSACTIONS));
         case maxmempool:
             return qlonglong(gArgs.GetArg("-maxmempool", DEFAULT_MAX_MEMPOOL_SIZE));
+        case mempoolexpiry:
+            return qlonglong(gArgs.GetArg("-mempoolexpiry", DEFAULT_MEMPOOL_EXPIRY));
         default:
             return QVariant();
         }
@@ -517,6 +519,20 @@ bool OptionsModel::setData(const QModelIndex & index, const QVariant & value, in
                 std::string strNv = value.toString().toStdString();
                 gArgs.ForceSetArg("-maxmempool", strNv);
                 gArgs.ModifyRWConfigFile("maxmempool", strNv);
+                if (nNv < nOldValue) {
+                    LimitMempoolSize();
+                }
+            }
+            break;
+        }
+        case mempoolexpiry:
+        {
+            long long nOldValue = gArgs.GetArg("-mempoolexpiry", DEFAULT_MEMPOOL_EXPIRY);
+            long long nNv = value.toLongLong();
+            if (nNv != nOldValue) {
+                std::string strNv = value.toString().toStdString();
+                gArgs.ForceSetArg("-mempoolexpiry", strNv);
+                gArgs.ModifyRWConfigFile("mempoolexpiry", strNv);
                 if (nNv < nOldValue) {
                     LimitMempoolSize();
                 }
