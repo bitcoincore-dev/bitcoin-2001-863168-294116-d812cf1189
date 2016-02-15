6a19a2b4e81d9cb8382b77c659df08eb9c279c17
Luke Dashjr <luke-jr+git@utopios.org>
2016-02-15 10:21:16 +0000

    Qt/Options: Configure limitdescendant{count,size} using rwconf

--- src/qt/optionsmodel.h
+++ src/qt/optionsmodel.h
@@ -68,6 +68,8 @@ public:
         bytespersigopstrict,
         limitancestorcount,
         limitancestorsize,
+        limitdescendantcount,
+        limitdescendantsize,
         OptionIDRowCount,
     };
 
