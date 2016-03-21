90250d98e067a40bbc4cf1525f1c6ed345724fab
Wladimir J. van der Laan <laanwj@gmail.com>
2016-03-21 16:55:02 +0100

    rpc: introduce 'label' API for wallet
    Add label API to wallet RPC.
    
    This is one step towards #3816 ("Remove bolt-on account system") although it doesn't
    actually remove anything yet.
    
    These initially mirror the account functions, with the following differences:
    
    - These functions aren't DEPRECATED in the help
    - Help mentions 'label' instead of accounts. In the language used, labels are
      associated with addresses, instead of addresses associated with labels. (unlike
      with accounts.)
    - Labels have no balance
    - `listlabels` has no minconf or watchonly argument
    - Like in the GUI, labels can be set on any address, not just receiving addreses
    - Unlike accounts, labels can be deleted.
      Being unable to delete them is a common annoyance (see #1231).
      Currently only by reassigning all addresses using `setlabel`, but an explicit
      call `deletelabel` which assigns all address to the default label may make
      sense.

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -565,7 +565,7 @@ public:
 };
 
 /**
- * Internal transfers.
+ * DEPRECATED Internal transfers.
  * Database key is acentry<account><counter>.
  */
 class CAccountingEntry
@@ -1141,7 +1141,7 @@ public:
 
 
 /** 
- * Account information.
+ * DEPRECATED Account information.
  * Stored in wallet with key "acc"+string account name.
  */
 class CAccount
