5560cd011b4bb6f65604eb03d73a62a9229a2721
Jonas Schnelli <dev@jonasschnelli.ch>
2016-08-12 13:04:44 +0200

    Add configuration options for mempool stats

--- src/init.cpp
+++ src/init.cpp
@@ -66,6 +66,7 @@
 #include <rpc/util.h>
 #include <scheduler.h>
 #include <script/sigcache.h>
+#include <stats/stats.h>
 #include <sync.h>
 #include <timedata.h>
 #include <torcontrol.h>
@@ -671,6 +672,8 @@ void SetupServerArgs(ArgsManager& argsman)
     hidden_args.emplace_back("-daemonwait");
 #endif
 
+    CStats::AddStatsOptions();
+
     // Add the hidden options
     argsman.AddHiddenArgs(hidden_args);
 }
@@ -1046,6 +1049,8 @@ bool AppInitParameterInteraction(const ArgsManager& args)
             return InitError(util::ErrorString(blockman_result));
         }
     }
+
+    if (!CStats::parameterInteraction()) return false;
 
     return true;
 }
