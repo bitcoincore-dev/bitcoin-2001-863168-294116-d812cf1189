2da1d2832a29ac39e48c90b5b6cc3546989ce628
Luke Dashjr <luke-jr+git@utopios.org>
2016-08-13 22:31:26 +0000

    Recognise NODE_XTHIN service bit

--- src/protocol.h
+++ src/protocol.h
@@ -268,6 +268,9 @@ enum ServiceFlags : uint64_t {
     // witness data.
     NODE_WITNESS = (1 << 3),
 
+    // NODE_XTHIN means the node supports Xtreme Thinblocks
+    NODE_XTHIN = (1 << 4),
+
     // Bits 24-31 are reserved for temporary experiments. Just pick a bit that
     // isn't getting used, or one not being used much, and notify the
     // bitcoin-development mailing list. Remember that service bits are just
