b0a256d73cacaca3fb3bd436a66759fff662e074
Luke Dashjr <luke-jr+git@utopios.org>
2016-09-09 10:43:54 +0000

    Qt: Add a combobox to toolbar to select from multiple wallets
    Github-Pull: #8694
    Rebased-From: 6f3f87fe0fcb10929a9a4f367e72767a7b49aa98

--- src/qt/bitcoingui.h
+++ src/qt/bitcoingui.h
@@ -34,6 +34,7 @@ class CWallet;
 
 QT_BEGIN_NAMESPACE
 class QAction;
+class QComboBox;
 class QProgressBar;
 class QProgressDialog;
 QT_END_NAMESPACE
@@ -63,7 +64,6 @@ public:
         functionality.
     */
     bool addWallet(const QString& name, WalletModel *walletModel);
-    bool setCurrentWallet(const QString& name);
     void removeAllWallets();
 #endif // ENABLE_WALLET
     bool enableWallet;
@@ -110,6 +110,9 @@ private:
     QAction *openRPCConsoleAction;
     QAction *openAction;
     QAction *showHelpMessageAction;
+
+    QLabel *WalletSelectorLabel;
+    QComboBox *WalletSelector;
 
     QSystemTrayIcon *trayIcon;
     QMenu *trayIconMenu;
@@ -162,6 +165,8 @@ public Q_SLOTS:
     void message(const QString &title, const QString &message, unsigned int style, bool *ret = NULL);
 
 #ifdef ENABLE_WALLET
+    bool setCurrentWallet(const QString& name);
+
     /** Set the encryption status as shown in the UI.
        @param[in] status            current encryption status
        @see WalletModel::EncryptionStatus
