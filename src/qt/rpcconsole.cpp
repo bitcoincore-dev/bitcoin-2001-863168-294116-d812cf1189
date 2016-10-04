0550407ab4c3d59f8a0705a5b11e257909d38186
Luke Dashjr <luke-jr+git@utopios.org>
2016-10-04 04:22:11 +0000

    Qt/RPCConsole: Refactor RPCConsole::WriteCommandHistory out of ~RPCConsole

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -470,10 +470,8 @@ RPCConsole::RPCConsole(const PlatformStyle *_platformStyle, QWidget *parent) :
     settings.endArray();
 }
 
-RPCConsole::~RPCConsole()
+void RPCConsole::WriteCommandHistory()
 {
-    GUIUtil::saveWindowGeometry("nRPCConsoleWindow", this);
-
     // persist history
     QSettings settings;
     settings.beginWriteArray("nRPCConsoleWindowHistory");
@@ -482,6 +480,13 @@ RPCConsole::~RPCConsole()
         settings.setValue("cmd", history.at(i));
     }
     settings.endArray();
+}
+
+RPCConsole::~RPCConsole()
+{
+    GUIUtil::saveWindowGeometry("nRPCConsoleWindow", this);
+
+    WriteCommandHistory();
 
     RPCUnsetTimerInterface(rpcTimerInterface);
     delete rpcTimerInterface;
