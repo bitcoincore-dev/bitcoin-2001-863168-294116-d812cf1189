dda19afb8902841f335fae6886f9374f901cfd2c
Luke Dashjr <luke-jr+git@utopios.org>
2016-10-04 14:07:50 +0000

    Qt/RPCConsole: Add signmessagewithprivkey to list of commands filtered from history

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -61,6 +61,7 @@ namespace {
 // don't add private key handling cmd's to the history
 const QStringList historyFilter = QStringList()
     << "importprivkey"
+    << "signmessagewithprivkey"
     << "signrawtransaction"
     << "walletpassphrase"
     << "walletpassphrasechange"
