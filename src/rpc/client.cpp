08bf266efa1ea4cfe9614e71c8ec2bf7f84f4b71
Luke Dashjr <luke-jr+git@utopios.org>
2016-11-13 22:05:20 +0000

    RPC: sweepprivkeys method to scan UTXO set and send to local wallet

--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -91,6 +91,7 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "scanblocks", 2, "start_height" },
     { "scanblocks", 3, "stop_height" },
     { "scanblocks", 5, "options" },
+    { "sweepprivkeys", 0, "options" },
     { "scantxoutset", 1, "scanobjects" },
     { "addmultisigaddress", 0, "nrequired" },
     { "addmultisigaddress", 1, "keys" },
