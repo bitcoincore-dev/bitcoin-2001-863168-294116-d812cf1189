4f19f3c02b14a1fe2e3a0f8b78634df08ff0b9ba
Russell Yanofsky <russ@yanofsky.org>
2016-12-15 21:37:25 -0500

    immature
    Fix CWalletTx::GetImmatureCredit() returning stale values.
    
    Add missing assignment to CWalletTx::MarkDirty() to clear the cached immature
    credit flag.

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -398,6 +398,7 @@ public:
     void MarkDirty()
     {
         fCreditCached = false;
+        fImmatureCreditCached = false;
         fAvailableCreditCached = false;
         fWatchDebitCached = false;
         fWatchCreditCached = false;
