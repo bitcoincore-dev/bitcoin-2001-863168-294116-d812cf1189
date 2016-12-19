31ec1e6a29495daf9d86227deab1b07c1d78e2e9
Russell Yanofsky <russ@yanofsky.org>
2016-12-19 11:25:15 -0500

    [wallet] Remove CWalletTx merging logic from AddToWallet
    Instead of AddToWallet taking a temporary CWalletTx object and then potentially
    merging it with a pre-existing CWalletTx, have it take a callback so callers
    can update the pre-existing CWalletTx directly.
    
    This makes AddToWallet simpler because now it is only has to be concerned with
    saving CWalletTx objects and not merging them.
    
    This makes AddToWallet calls clearer because they can now make direct updates to
    CWalletTx entries without having to make temporary objects and then worry about
    how they will be merged.
    
    This is a pure refactoring, no behavior is changing.

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -2962,7 +2962,13 @@ UniValue bumpfee(const JSONRPCRequest& request)
     }
 
     // mark the original tx as bumped
-    if (!pwalletMain->MarkReplaced(wtx.GetHash(), txHash)) {
+    auto updateEntry = [&](TxEntry& entry, bool fNew) {
+        // Ensure for now that we're not overwriting data
+        assert(wtx.mapValue.count("replaced_by_txid") == 0);
+        wtx.mapValue["replaced_by_txid"] = txHash.ToString();
+        return true;
+    };
+    if (!pwalletMain->AddToWallet(wtx.tx, updateEntry)) {
         // TODO: see if JSON-RPC has a standard way of returning a response
         // along with an exception. It would be good to return information about
         // wtxBumped to the caller even if marking the original transaction
