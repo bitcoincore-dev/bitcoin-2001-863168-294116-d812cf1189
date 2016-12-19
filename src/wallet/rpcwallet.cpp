d988ec9a3ca501f874b72d7c184133da4d73a23d
Russell Yanofsky <russ@yanofsky.org>
2016-12-19 11:25:15 -0500

    Remove CWalletTx merging logic from AddToWallet
    Instead of AddToWallet taking a temporary CWalletTx object and then potentially
    merging it with a pre-existing CWalletTx, have it take a callback so callers
    can update the pre-existing CWalletTx directly.
    
    This makes AddToWallet simpler because now it is only has to be concerned with
    saving CWalletTx objects and not merging them.
    
    This makes AddToWallet calls clearer because they can now make direct updates to
    CWalletTx entries without having to make temporary objects and then worry about
    how they will be merged.
    
    This is a pure refactoring, no behavior is changing.

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -336,7 +336,7 @@ UniValue getaddressesbyaccount(const JSONRPCRequest& request)
     return ret;
 }
 
-static void SendMoney(const CTxDestination &address, CAmount nValue, bool fSubtractFeeFromAmount, CWalletTx& wtxNew)
+static void SendMoney(const CTxDestination& address, CAmount nValue, bool fSubtractFeeFromAmount, const string& comment, const string& to, const string& strFromAccount, CTransactionRef& txNew)
 {
     CAmount curBalance = pwalletMain->GetBalance();
 
@@ -361,13 +361,26 @@ static void SendMoney(const CTxDestination &address, CAmount nValue, bool fSubtr
     int nChangePosRet = -1;
     CRecipient recipient = {scriptPubKey, nValue, fSubtractFeeFromAmount};
     vecSend.push_back(recipient);
-    if (!pwalletMain->CreateTransaction(vecSend, wtxNew, reservekey, nFeeRequired, nChangePosRet, strError)) {
+    if (!pwalletMain->CreateTransaction(vecSend, txNew, reservekey, nFeeRequired, nChangePosRet, strError)) {
         if (!fSubtractFeeFromAmount && nValue + nFeeRequired > curBalance)
             strError = strprintf("Error: This transaction requires a transaction fee of at least %s", FormatMoney(nFeeRequired));
         throw JSONRPCError(RPC_WALLET_ERROR, strError);
     }
+
+    auto updateEntry = [&](TxEntry& entry, bool fNew) {
+        assert(fNew);
+        entry.second.fFromMe = true;
+        entry.second.fTimeReceivedIsTxTime = true;
+        entry.second.strFromAccount = strFromAccount;
+        if (!comment.empty())
+            entry.second.mapValue["comment"] = comment;
+        if (!to.empty())
+            entry.second.mapValue["to"] = to;
+        return true;
+    };
+
     CValidationState state;
-    if (!pwalletMain->CommitTransaction(wtxNew, reservekey, g_connman.get(), state)) {
+    if (!pwalletMain->CommitTransaction(txNew, updateEntry, reservekey, g_connman.get(), state)) {
         strError = strprintf("Error: The transaction was rejected! Reason given: %s", state.GetRejectReason());
         throw JSONRPCError(RPC_WALLET_ERROR, strError);
     }
@@ -414,11 +427,12 @@ UniValue sendtoaddress(const JSONRPCRequest& request)
         throw JSONRPCError(RPC_TYPE_ERROR, "Invalid amount for send");
 
     // Wallet comments
-    CWalletTx wtx;
-    if (request.params.size() > 2 && !request.params[2].isNull() && !request.params[2].get_str().empty())
-        wtx.mapValue["comment"] = request.params[2].get_str();
-    if (request.params.size() > 3 && !request.params[3].isNull() && !request.params[3].get_str().empty())
-        wtx.mapValue["to"]      = request.params[3].get_str();
+    string comment;
+    if (request.params.size() > 2 && !request.params[2].isNull())
+        comment = request.params[2].get_str();
+    string to;
+    if (request.params.size() > 3 && !request.params[3].isNull())
+        to = request.params[3].get_str();
 
     bool fSubtractFeeFromAmount = false;
     if (request.params.size() > 4)
@@ -426,9 +440,10 @@ UniValue sendtoaddress(const JSONRPCRequest& request)
 
     EnsureWalletIsUnlocked();
 
-    SendMoney(address.Get(), nAmount, fSubtractFeeFromAmount, wtx);
+    CTransactionRef txNew;
+    SendMoney(address.Get(), nAmount, fSubtractFeeFromAmount, comment, to, {}, txNew);
 
-    return wtx.GetHash().GetHex();
+    return txNew->GetHash().GetHex();
 }
 
 UniValue listaddressgroupings(const JSONRPCRequest& request)
@@ -835,12 +850,12 @@ UniValue sendfrom(const JSONRPCRequest& request)
     if (request.params.size() > 3)
         nMinDepth = request.params[3].get_int();
 
-    CWalletTx wtx;
-    wtx.strFromAccount = strAccount;
-    if (request.params.size() > 4 && !request.params[4].isNull() && !request.params[4].get_str().empty())
-        wtx.mapValue["comment"] = request.params[4].get_str();
-    if (request.params.size() > 5 && !request.params[5].isNull() && !request.params[5].get_str().empty())
-        wtx.mapValue["to"]      = request.params[5].get_str();
+    string comment;
+    if (request.params.size() > 4 && !request.params[4].isNull())
+        comment = request.params[4].get_str();
+    string to;
+    if (request.params.size() > 5 && !request.params[5].isNull())
+        to = request.params[5].get_str();
 
     EnsureWalletIsUnlocked();
 
@@ -849,9 +864,10 @@ UniValue sendfrom(const JSONRPCRequest& request)
     if (nAmount > nBalance)
         throw JSONRPCError(RPC_WALLET_INSUFFICIENT_FUNDS, "Account has insufficient funds");
 
-    SendMoney(address.Get(), nAmount, false, wtx);
+    CTransactionRef txNew;
+    SendMoney(address.Get(), nAmount, false, comment, to, strAccount, txNew);
 
-    return wtx.GetHash().GetHex();
+    return txNew->GetHash().GetHex();
 }
 
 
@@ -907,10 +923,9 @@ UniValue sendmany(const JSONRPCRequest& request)
     if (request.params.size() > 2)
         nMinDepth = request.params[2].get_int();
 
-    CWalletTx wtx;
-    wtx.strFromAccount = strAccount;
-    if (request.params.size() > 3 && !request.params[3].isNull() && !request.params[3].get_str().empty())
-        wtx.mapValue["comment"] = request.params[3].get_str();
+    string comment;
+    if (request.params.size() > 3 && !request.params[3].isNull())
+        comment = request.params[3].get_str();
 
     UniValue subtractFeeFromAmount(UniValue::VARR);
     if (request.params.size() > 4)
@@ -960,16 +975,26 @@ UniValue sendmany(const JSONRPCRequest& request)
     CAmount nFeeRequired = 0;
     int nChangePosRet = -1;
     string strFailReason;
-    bool fCreated = pwalletMain->CreateTransaction(vecSend, wtx, keyChange, nFeeRequired, nChangePosRet, strFailReason);
+    CTransactionRef txNew;
+    bool fCreated = pwalletMain->CreateTransaction(vecSend, txNew, keyChange, nFeeRequired, nChangePosRet, strFailReason);
     if (!fCreated)
         throw JSONRPCError(RPC_WALLET_INSUFFICIENT_FUNDS, strFailReason);
     CValidationState state;
-    if (!pwalletMain->CommitTransaction(wtx, keyChange, g_connman.get(), state)) {
+    auto updateEntry = [&](TxEntry& entry, bool fNew) {
+        assert(fNew);
+        entry.second.fFromMe = true;
+        entry.second.fTimeReceivedIsTxTime = true;
+        entry.second.strFromAccount = strAccount;
+        if (!comment.empty())
+            entry.second.mapValue["comment"] = comment;
+        return true;
+    };
+    if (!pwalletMain->CommitTransaction(txNew, updateEntry, keyChange, g_connman.get(), state)) {
         strFailReason = strprintf("Transaction commit failed:: %s", state.GetRejectReason());
         throw JSONRPCError(RPC_WALLET_ERROR, strFailReason);
     }
 
-    return wtx.GetHash().GetHex();
+    return txNew->GetHash().GetHex();
 }
 
 // Defined in rpc/misc.cpp
--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -826,16 +826,17 @@ void CWallet::MarkDirty()
     }
 }
 
-bool CWallet::AddToWallet(const CWalletTx& wtxIn, bool fFlushOnClose)
+bool CWallet::AddToWallet(CTransactionRef tx, UpdateEntry updateEntry, bool fFlushOnClose)
 {
     LOCK(cs_wallet);
 
     CWalletDB walletdb(strWalletFile, "r+", fFlushOnClose);
 
-    uint256 hash = wtxIn.GetHash();
+    uint256 hash = tx->GetHash();
 
     // Inserts only if not already there, returns tx inserted or tx found
-    pair<map<uint256, CWalletTx>::iterator, bool> ret = mapWallet.insert(make_pair(hash, wtxIn));
+    auto ret = mapWallet.emplace(std::piecewise_construct, std::forward_as_tuple(hash), std::forward_as_tuple(this, std::move(tx)));
+    bool fUpdated = updateEntry(*ret.first, ret.second);
     CWalletTx& wtx = (*ret.first).second;
     wtx.BindWallet(this);
     bool fInsertedNew = ret.second;
@@ -846,9 +847,9 @@ bool CWallet::AddToWallet(const CWalletTx& wtxIn, bool fFlushOnClose)
         wtxOrdered.insert(make_pair(wtx.nOrderPos, TxPair(&wtx, (CAccountingEntry*)0)));
 
         wtx.nTimeSmart = wtx.nTimeReceived;
-        if (!wtxIn.hashUnset())
+        if (!wtx.hashUnset())
         {
-            if (mapBlockIndex.count(wtxIn.hashBlock))
+            if (mapBlockIndex.count(wtx.hashBlock))
             {
                 int64_t latestNow = wtx.nTimeReceived;
                 int64_t latestEntry = 0;
@@ -881,54 +882,24 @@ bool CWallet::AddToWallet(const CWalletTx& wtxIn, bool fFlushOnClose)
                     }
                 }
 
-                int64_t blocktime = mapBlockIndex[wtxIn.hashBlock]->GetBlockTime();
+                int64_t blocktime = mapBlockIndex[wtx.hashBlock]->GetBlockTime();
                 wtx.nTimeSmart = std::max(latestEntry, std::min(blocktime, latestNow));
             }
             else
                 LogPrintf("AddToWallet(): found %s in block %s not in index\n",
-                         wtxIn.GetHash().ToString(),
-                         wtxIn.hashBlock.ToString());
+                         wtx.GetHash().ToString(),
+                         wtx.hashBlock.ToString());
         }
         AddToSpends(hash);
     }
 
-    bool fUpdated = false;
-    if (!fInsertedNew)
-    {
-        // Merge
-        if (!wtxIn.hashUnset() && wtxIn.hashBlock != wtx.hashBlock)
-        {
-            wtx.hashBlock = wtxIn.hashBlock;
-            fUpdated = true;
-        }
-        // If no longer abandoned, update
-        if (wtxIn.hashBlock.IsNull() && wtx.isAbandoned())
-        {
-            wtx.hashBlock = wtxIn.hashBlock;
-            fUpdated = true;
-        }
-        if (wtxIn.nIndex != -1 && (wtxIn.nIndex != wtx.nIndex))
-        {
-            wtx.nIndex = wtxIn.nIndex;
-            fUpdated = true;
-        }
-        if (wtxIn.fFromMe && wtxIn.fFromMe != wtx.fFromMe)
-        {
-            wtx.fFromMe = wtxIn.fFromMe;
-            fUpdated = true;
-        }
-    }
-
     //// debug print
-    LogPrintf("AddToWallet %s  %s%s\n", wtxIn.GetHash().ToString(), (fInsertedNew ? "new" : ""), (fUpdated ? "update" : ""));
+    LogPrintf("AddToWallet %s  %s%s\n", hash.ToString(), (fInsertedNew ? "new" : ""), (fUpdated ? "update" : ""));
 
     // Write to disk
     if (fInsertedNew || fUpdated)
         if (!walletdb.WriteTx(wtx))
             return false;
-
-    // Break debit/credit balance caches:
-    wtx.MarkDirty();
 
     // Notify UI of new or updated transaction
     NotifyTransactionChanged(this, hash, fInsertedNew ? CT_NEW : CT_UPDATED);
@@ -938,7 +909,7 @@ bool CWallet::AddToWallet(const CWalletTx& wtxIn, bool fFlushOnClose)
 
     if ( !strCmd.empty())
     {
-        boost::replace_all(strCmd, "%s", wtxIn.GetHash().GetHex());
+        boost::replace_all(strCmd, "%s", hash.GetHex());
         boost::thread t(runCommand, strCmd); // thread runs free
     }
 
@@ -993,13 +964,33 @@ bool CWallet::AddToWalletIfInvolvingMe(const CTransaction& tx, const CBlockIndex
         if (fExisted && !fUpdate) return false;
         if (fExisted || IsMine(tx) || IsFromMe(tx))
         {
-            CWalletTx wtx(this, MakeTransactionRef(tx));
+            auto updateEntry = [&](TxEntry& entry, bool fNew) {
+                bool fWriteTx = fNew;
 
-            // Get merkle branch if transaction was found in a block
-            if (posInBlock != -1)
-                wtx.SetMerkleBranch(pIndex, posInBlock);
+                // Update block position if transaction was found in a block
+                if (posInBlock != -1) {
+                    uint256 hashBlock = pIndex->GetBlockHash();
+                    if (hashBlock != entry.second.hashBlock || posInBlock != entry.second.nIndex) {
+                        entry.second.hashBlock = hashBlock;
+                        entry.second.nIndex = posInBlock;
+                        fWriteTx = true;
+                    }
+                }
 
-            return AddToWallet(wtx, false);
+                // Abandoned state is cleared under the assumption that further
+                // notification of an abandoned transaction is an indication
+                // that it is not actually safe to be considered abandoned.
+                if (entry.second.isAbandoned()) {
+                    entry.second.hashBlock.SetNull();
+                    fWriteTx = true;
+                }
+
+                // Break debit/credit balance caches.
+                entry.second.MarkDirty();
+
+                return fWriteTx;
+            };
+            return AddToWallet(MakeTransactionRef(tx), updateEntry, false);
         }
     }
     return false;
@@ -2204,15 +2195,15 @@ bool CWallet::FundTransaction(CMutableTransaction& tx, CAmount& nFeeRet, bool ov
         coinControl.Select(txin.prevout);
 
     CReserveKey reservekey(this);
-    CWalletTx wtx;
-    if (!CreateTransaction(vecSend, wtx, reservekey, nFeeRet, nChangePosInOut, strFailReason, &coinControl, false))
+    CTransactionRef txNew;
+    if (!CreateTransaction(vecSend, txNew, reservekey, nFeeRet, nChangePosInOut, strFailReason, &coinControl, false))
         return false;
 
     if (nChangePosInOut != -1)
-        tx.vout.insert(tx.vout.begin() + nChangePosInOut, wtx.tx->vout[nChangePosInOut]);
+        tx.vout.insert(tx.vout.begin() + nChangePosInOut, txNew->vout[nChangePosInOut]);
 
     // Add new txins (keeping original txin scriptSig/order)
-    BOOST_FOREACH(const CTxIn& txin, wtx.tx->vin)
+    BOOST_FOREACH(const CTxIn& txin, txNew->vin)
     {
         if (!coinControl.IsSelected(txin.prevout))
         {
@@ -2229,7 +2220,7 @@ bool CWallet::FundTransaction(CMutableTransaction& tx, CAmount& nFeeRet, bool ov
     return true;
 }
 
-bool CWallet::CreateTransaction(const vector<CRecipient>& vecSend, CWalletTx& wtxNew, CReserveKey& reservekey, CAmount& nFeeRet,
+bool CWallet::CreateTransaction(const vector<CRecipient>& vecSend, CTransactionRef& tx, CReserveKey& reservekey, CAmount& nFeeRet,
                                 int& nChangePosInOut, std::string& strFailReason, const CCoinControl* coinControl, bool sign)
 {
     CAmount nValue = 0;
@@ -2253,8 +2244,6 @@ bool CWallet::CreateTransaction(const vector<CRecipient>& vecSend, CWalletTx& wt
         return false;
     }
 
-    wtxNew.fTimeReceivedIsTxTime = true;
-    wtxNew.BindWallet(this);
     CMutableTransaction txNew;
 
     // Discourage fee sniping.
@@ -2303,7 +2292,6 @@ bool CWallet::CreateTransaction(const vector<CRecipient>& vecSend, CWalletTx& wt
                 txNew.vin.clear();
                 txNew.vout.clear();
                 txNew.wit.SetNull();
-                wtxNew.fFromMe = true;
                 bool fFirst = true;
 
                 CAmount nValueToSelect = nValue;
@@ -2498,17 +2486,17 @@ bool CWallet::CreateTransaction(const vector<CRecipient>& vecSend, CWalletTx& wt
                     txNew.wit.SetNull();
                 }
 
-                // Embed the constructed transaction data in wtxNew.
-                wtxNew.SetTx(MakeTransactionRef(std::move(txNew)));
+                // Return the constructed transaction data.
+                tx = MakeTransactionRef(std::move(txNew));
 
                 // Limit size
-                if (GetTransactionWeight(wtxNew) >= MAX_STANDARD_TX_WEIGHT)
+                if (GetTransactionWeight(*tx) >= MAX_STANDARD_TX_WEIGHT)
                 {
                     strFailReason = _("Transaction too large");
                     return false;
                 }
 
-                dPriority = wtxNew.tx->ComputePriority(dPriority, nBytes);
+                dPriority = tx->ComputePriority(dPriority, nBytes);
 
                 // Allow to override the default confirmation target over the CoinControl instance
                 int currentConfirmationTarget = nTxConfirmTarget;
@@ -2556,21 +2544,22 @@ bool CWallet::CreateTransaction(const vector<CRecipient>& vecSend, CWalletTx& wt
 /**
  * Call after CreateTransaction unless you want to abort
  */
-bool CWallet::CommitTransaction(CWalletTx& wtxNew, CReserveKey& reservekey, CConnman* connman, CValidationState& state)
+bool CWallet::CommitTransaction(CTransactionRef txNew, UpdateEntry updateEntry, CReserveKey& reservekey, CConnman* connman, CValidationState& state)
 {
     {
         LOCK2(cs_main, cs_wallet);
-        LogPrintf("CommitTransaction:\n%s", wtxNew.tx->ToString());
+        LogPrintf("CommitTransaction:\n%s", txNew->ToString());
+        TxEntry* pEntry = nullptr;
         {
             // Take key pair from key pool so it won't be used again
             reservekey.KeepKey();
 
             // Add tx to wallet, because if it has change it's also ours,
             // otherwise just for transaction history.
-            AddToWallet(wtxNew);
+            AddToWallet(txNew, [&](TxEntry& entry, bool fNew) { pEntry = &entry; return updateEntry(entry, fNew); });
 
             // Notify that old coins are spent
-            BOOST_FOREACH(const CTxIn& txin, wtxNew.tx->vin)
+            BOOST_FOREACH(const CTxIn& txin, pEntry->second.tx->vin)
             {
                 CWalletTx &coin = mapWallet[txin.prevout.hash];
                 coin.BindWallet(this);
@@ -2579,16 +2568,16 @@ bool CWallet::CommitTransaction(CWalletTx& wtxNew, CReserveKey& reservekey, CCon
         }
 
         // Track how many getdata requests our transaction gets
-        mapRequestCount[wtxNew.GetHash()] = 0;
+        mapRequestCount[pEntry->second.GetHash()] = 0;
 
         if (fBroadcastTransactions)
         {
             // Broadcast
-            if (!wtxNew.AcceptToMemoryPool(maxTxFee, state)) {
+            if (!pEntry->second.AcceptToMemoryPool(maxTxFee, state)) {
                 LogPrintf("CommitTransaction(): Transaction cannot be broadcast immediately, %s\n", state.GetRejectReason());
                 // TODO: if we expect the failure to be long term or permanent, instead delete wtx from the wallet and return failure.
             } else {
-                wtxNew.RelayWalletTransaction(connman);
+                pEntry->second.RelayWalletTransaction(connman);
             }
         }
     }
