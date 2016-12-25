b14300ffcef6c12280caa387992feee4693b349d
Luke Dashjr <luke-jr+git@utopios.org>
2016-12-25 06:04:55 +0000

    Store mempool min fee state in mempool.dat

--- src/validation.cpp
+++ src/validation.cpp
@@ -4195,7 +4195,17 @@ bool LoadMempool(void)
         std::map<std::string, std::vector<unsigned char>> mapData;
         file >> mapData;
 
-        auto it = mapData.find("deltas");
+        auto it = mapData.find("minfee");
+        if (it == mapData.end()) {
+            try {
+                CDataStream ss(it->second, SER_DISK, CLIENT_VERSION);
+                mempool.LoadMinFeeInternal(ss);
+            } catch (const std::exception& e) {
+                LogPrintf("Failed to deserialize mempool %s from disk: %s. Continuing anyway.\n", "minfee", e.what());
+            }
+        }
+
+        it = mapData.find("deltas");
         if (it == mapData.end()) {
             try {
                 CDataStream ss(it->second, SER_DISK, CLIENT_VERSION);
@@ -4293,6 +4303,11 @@ void DumpMempool(void)
             }
 
             mapData["txs"] = SerializeToVector(txMapDatas);
+        }
+        {
+            CDataStream ss(SER_DISK, CLIENT_VERSION);
+            mempool.DumpMinFeeInternal(ss);
+            mapData["minfee"] = std::vector<unsigned char>(ss.begin(), ss.end());
         }
 
         FILE* filestr = fopen((GetDataDir() / "mempool.dat.new").string().c_str(), "w");
