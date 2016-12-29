d1ce7b93d1be7883c530bd9d7d63e00ce229cdcf
Luke Dashjr <luke-jr+git@utopios.org>
2016-12-29 13:22:03 +0000

    GUI/RPCConsole: Include sweepprivkeys in history sensitive-command filter

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -70,6 +70,7 @@ const QStringList historyFilter = QStringList()
     << "importmulti"
     << "signmessagewithprivkey"
     << "signrawtransaction"
+    << "sweepprivkeys"
     << "walletpassphrase"
     << "walletpassphrasechange"
     << "encryptwallet";
