tmagik/catoshi

Factor out CWallet::nTimeSmart computation into a method.
No change in behavior, this change just pulls some code out of
CWallet::AddToWallet that was making it very long into a separate method.

src/wallet/wallet.h
@@ -776,6 +776,7 @@ public:
     bool EncryptWallet(const SecureString& strWalletPassphrase);
 
     void GetKeyBirthTimes(std::map<CTxDestination, int64_t> &mapKeyBirth) const;
+    unsigned int ComputeTimeSmart(const CWalletTx& wtx) const;
 
     /** 
      * Increment the next transaction order id

bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

src/wallet/wallet.h
@@ -748,6 +748,7 @@ public:
     bool EncryptWallet(const SecureString& strWalletPassphrase);
 
     void GetKeyBirthTimes(std::map<CKeyID, int64_t> &mapKeyBirth) const;
+    unsigned int GetTimeSmart(const CWalletTx& wtx) const;
 
     /** 
      * Increment the next transaction order id