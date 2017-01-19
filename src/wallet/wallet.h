a0a4d8205498c38e53fb85e25cf6428ead1e93e1
Russell Yanofsky <russ@yanofsky.org>
2017-01-19 16:08:03 -0500

    [wallet] Get rid of CWalletTx default constructor
    No change in behavior. This makes CWalletTx construction more explicit and will
    now trigger compiler errors on mapWallet[hash] lookups that could inadvertently
    create dummy transaction entries.

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -335,11 +335,6 @@ public:
     mutable CAmount nAvailableWatchCreditCached;
     mutable CAmount nChangeCached;
 
-    CWalletTx()
-    {
-        Init(nullptr);
-    }
-
     CWalletTx(const CWallet* pwalletIn, CTransactionRef arg) : CMerkleTx(std::move(arg))
     {
         Init(pwalletIn);
