2fdc2217ff1af02aee093fe345143b31671290ac
Luke Dashjr <luke-jr+git@utopios.org>
2017-02-02 22:15:55 +0000

    RPC: rawtransaction: Add RBF support for createrawtransaction

--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -86,6 +86,7 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "createrawtransaction", 0, "transactions" },
     { "createrawtransaction", 1, "outputs" },
     { "createrawtransaction", 2, "locktime" },
+    { "createrawtransaction", 3, "optintorbf" },
     { "signrawtransaction", 1, "prevtxs" },
     { "signrawtransaction", 2, "privkeys" },
     { "sendrawtransaction", 1, "allowhighfees" },
