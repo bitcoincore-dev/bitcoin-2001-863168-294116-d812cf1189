tmagik/catoshi

[wallet] Add GetLegacyBalance method to simplify getbalance RPC
This adds a simpler new implementation of getbalance logic along with asserts
to confirm it behaves identically to the old logic. The old logic is removed in
the next commit.

src/wallet/rpcwallet.cpp
@@ -729,6 +729,8 @@ UniValue getbalance(const JSONRPCRequest& request)
     if (request.params.size() == 0)
         return  ValueFromAmount(pwallet->GetBalance());
 
+    const std::string* account = request.params[0].get_str() != "*" ? &request.params[0].get_str() : nullptr;
+
     int nMinDepth = 1;
     if (request.params.size() > 1)
         nMinDepth = request.params[1].get_int();
@@ -736,6 +738,8 @@ UniValue getbalance(const JSONRPCRequest& request)
     if(request.params.size() > 2)
         if(request.params[2].get_bool())
             filter = filter | ISMINE_WATCH_ONLY;
+
+    CAmount legacyBalance = pwallet->GetLegacyBalance(filter, nMinDepth, account);
 
     if (request.params[0].get_str() == "*") {
         // Calculate total balance in a very different way from GetBalance().
@@ -764,6 +768,7 @@ UniValue getbalance(const JSONRPCRequest& request)
                 nBalance -= s.amount;
             nBalance -= allFee;
         }
+        assert(nBalance == legacyBalance);
         return  ValueFromAmount(nBalance);
     }
 
@@ -771,6 +776,7 @@ UniValue getbalance(const JSONRPCRequest& request)
 
     CAmount nBalance = pwallet->GetAccountBalance(strAccount, nMinDepth, filter);
 
+    assert(nBalance == legacyBalance);
     return ValueFromAmount(nBalance);
 }
 
@@ -902,6 +908,8 @@ UniValue sendfrom(const JSONRPCRequest& request)
 
     // Check funds
     CAmount nBalance = pwallet->GetAccountBalance(strAccount, nMinDepth, ISMINE_SPENDABLE);
+    CAmount legacyBalance = pwallet->GetLegacyBalance(ISMINE_SPENDABLE, nMinDepth, &strAccount);
+    assert(nBalance == legacyBalance);
     if (nAmount > nBalance)
         throw JSONRPCError(RPC_WALLET_INSUFFICIENT_FUNDS, "Account has insufficient funds");
 
@@ -1011,6 +1019,8 @@ UniValue sendmany(const JSONRPCRequest& request)
 
     // Check funds
     CAmount nBalance = pwallet->GetAccountBalance(strAccount, nMinDepth, ISMINE_SPENDABLE);
+    CAmount legacyBalance = pwallet->GetLegacyBalance(ISMINE_SPENDABLE, nMinDepth, &strAccount);
+    assert(nBalance == legacyBalance);
     if (totalAmount > nBalance)
         throw JSONRPCError(RPC_WALLET_INSUFFICIENT_FUNDS, "Account has insufficient funds");
 

bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

src/wallet/rpcwallet.cpp
@@ -698,6 +698,8 @@ UniValue getbalance(const JSONRPCRequest& request)
     if (request.params.size() == 0)
         return  ValueFromAmount(pwalletMain->GetBalance());
 
+    const string* account = request.params[0].get_str() != "*" ? &request.params[0].get_str() : nullptr;
+
     int nMinDepth = 1;
     if (request.params.size() > 1)
         nMinDepth = request.params[1].get_int();
@@ -705,6 +707,8 @@ UniValue getbalance(const JSONRPCRequest& request)
     if(request.params.size() > 2)
         if(request.params[2].get_bool())
             filter = filter | ISMINE_WATCH_ONLY;
+
+    CAmount legacyBalance = pwalletMain->GetLegacyBalance(filter, nMinDepth, account);
 
     if (request.params[0].get_str() == "*") {
         // Calculate total balance in a very different way from GetBalance().
@@ -734,6 +738,7 @@ UniValue getbalance(const JSONRPCRequest& request)
                 nBalance -= s.amount;
             nBalance -= allFee;
         }
+        assert(nBalance == legacyBalance);
         return  ValueFromAmount(nBalance);
     }
 
@@ -741,6 +746,7 @@ UniValue getbalance(const JSONRPCRequest& request)
 
     CAmount nBalance = pwalletMain->GetAccountBalance(strAccount, nMinDepth, filter);
 
+    assert(nBalance == legacyBalance);
     return ValueFromAmount(nBalance);
 }
 
@@ -862,6 +868,8 @@ UniValue sendfrom(const JSONRPCRequest& request)
 
     // Check funds
     CAmount nBalance = pwalletMain->GetAccountBalance(strAccount, nMinDepth, ISMINE_SPENDABLE);
+    CAmount legacyBalance = pwalletMain->GetLegacyBalance(ISMINE_SPENDABLE, nMinDepth, &strAccount);
+    assert(nBalance == legacyBalance);
     if (nAmount > nBalance)
         throw JSONRPCError(RPC_WALLET_INSUFFICIENT_FUNDS, "Account has insufficient funds");
 
@@ -968,6 +976,8 @@ UniValue sendmany(const JSONRPCRequest& request)
 
     // Check funds
     CAmount nBalance = pwalletMain->GetAccountBalance(strAccount, nMinDepth, ISMINE_SPENDABLE);
+    CAmount legacyBalance = pwalletMain->GetLegacyBalance(ISMINE_SPENDABLE, nMinDepth, &strAccount);
+    assert(nBalance == legacyBalance);
     if (totalAmount > nBalance)
         throw JSONRPCError(RPC_WALLET_INSUFFICIENT_FUNDS, "Account has insufficient funds");
 