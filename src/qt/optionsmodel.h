d7a57e81dfc3cd61f226b6815dcadb14dfe53866
Luke Dashjr <luke-jr+git@utopios.org>
2017-02-07 23:53:41 +0000

    Qt/Options: Configure incrementalrelayfee using rwconf

--- src/qt/optionsmodel.h
+++ src/qt/optionsmodel.h
@@ -71,6 +71,7 @@ public:
         mempoolreplacement,
         maxorphantx,
         maxmempool,
+        incrementalrelayfee,
         mempoolexpiry,
         rejectunknownscripts,   // bool
         minrelaytxfee,
