f0a8d2fbeb75d21d860c0f44c0b8bfc493da0f55
Luke Dashjr <luke-jr+git@utopios.org>
2017-02-08 01:09:28 +0000

    Qt/Options: Configure blockmintxfee using rwconf

--- src/qt/optionsmodel.h
+++ src/qt/optionsmodel.h
@@ -77,6 +77,7 @@ public:
         rejectbaremultisig,     // bool
         datacarriersize,
         dustrelayfee,
+        blockmintxfee,
         blockmaxsize,
         blockprioritysize,
         blockmaxweight,
