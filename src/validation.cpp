c44c12cfaadebf7dcbe9851be251b14993656aa1
Luke Dashjr <luke-jr+git@utopios.org>
2017-02-13 10:11:24 +0000

    Support overriding txn-spk-reused rejections via sendrawtransaction

--- src/validation.cpp
+++ src/validation.cpp
@@ -619,6 +619,9 @@ static bool AcceptToMemoryPoolWorker(const CChainParams& chainparams, CTxMemPool
     }
 
     auto spk_reuse_mode = SpkReuseMode;
+    if (ignore_rejects.count("txn-spk-reused")) {
+        spk_reuse_mode = SRM_ALLOW;
+    }
     SPKStates_t mapSPK;
 
     // Check for conflicts with in-memory transactions
@@ -681,7 +684,7 @@ static bool AcceptToMemoryPoolWorker(const CChainParams& chainparams, CTxMemPool
                 }
             }
             if (mapSPK.find(hashSPK) != mapSPK.end()) {
-                return state.DoS(0, false, REJECT_NONSTANDARD, "txn-spk-reused-twinoutputs");
+                MaybeReject(REJECT_NONSTANDARD, "txn-spk-reused-twinoutputs");
             }
             mapSPK[hashSPK] = MemPool_SPK_State(mapSPK[hashSPK] | MSS_CREATED);
         }
@@ -745,7 +748,7 @@ static bool AcceptToMemoryPoolWorker(const CChainParams& chainparams, CTxMemPool
                 SPKStates_t::iterator mssit = mapSPK.find(hashSPK);
                 if (mssit != mapSPK.end()) {
                     if (mssit->second & MSS_CREATED) {
-                        return state.DoS(0, false, REJECT_NONSTANDARD, "txn-spk-reused-change");
+                        MaybeReject(REJECT_NONSTANDARD, "txn-spk-reused-change");
                     }
                 }
                 const auto& SPKit = pool.mapUsedSPK.find(hashSPK);
@@ -838,8 +841,8 @@ static bool AcceptToMemoryPoolWorker(const CChainParams& chainparams, CTxMemPool
             if (conflictit != setConflicts.end())
             {
                 if (!conflictit->second /* mere SPK conflict, NOT invalid */) {
-                    return state.DoS(0, false, REJECT_NONSTANDARD, "txn-spk-reused-chained");
-                }
+                    MaybeReject(REJECT_NONSTANDARD, "txn-spk-reused-chained");
+                } else
                 return state.DoS(10, false,
                                  REJECT_INVALID, "bad-txns-spends-conflicting-tx", false,
                                  strprintf("%s spends conflicting transaction %s",
