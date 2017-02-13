50bd8f4a8f341cbd2795a93c7bab8125ecdbd623
Luke Dashjr <luke-jr+git@utopios.org>
2017-02-13 00:01:55 +0000

    If -spkreuse=0, ensure transactions in mempool always have unique scriptPubKeys
    Exceptions:
    - Multiple inputs in the same transaction are allowed to spend against the same scriptPubKey
    - The same scriptPubKey may be used in the mempool as both first an output, and then spent in a later transaction's input

--- src/txmempool.h
+++ src/txmempool.h
@@ -20,6 +20,7 @@
 #include <policy/feerate.h>
 #include <policy/packages.h>
 #include <primitives/transaction.h>
+#include <script/script.h>
 #include <random.h>
 #include <sync.h>
 #include <util/epochguard.h>
@@ -54,6 +55,15 @@ struct LockPoints {
  * Test whether the LockPoints height and time are still valid on the current chain
  */
 bool TestLockPointValidity(CChain& active_chain, const LockPoints& lp) EXCLUSIVE_LOCKS_REQUIRED(cs_main);
+
+enum MemPool_SPK_State {
+    MSS_UNSEEN  = 0,
+    MSS_SPENT   = 1,
+    MSS_CREATED = 2,
+    MSS_BOTH    = 3,
+};
+
+typedef std::map<uint160, enum MemPool_SPK_State> SPKStates_t;
 
 struct CompareIteratorByHash {
     // SFINAE for T where T is either a pointer type (e.g., a txiter) or a reference_wrapper<T>
@@ -163,6 +173,8 @@ public:
 
     mutable size_t vTxHashesIdx; //!< Index in mempool's vTxHashes
     mutable Epoch::Marker m_epoch_marker; //!< epoch when last touched, useful for graph algorithms
+
+    SPKStates_t mapSPK;
 };
 
 // extracts a transaction hash from CTxMemPoolEntry or CTransactionRef
@@ -311,6 +323,8 @@ public:
         }
     }
 };
+
+uint160 ScriptHashkey(const CScript& script);
 
 // Multi_index tag names
 struct descendant_score {};
@@ -523,6 +537,9 @@ public:
     typedef std::set<txiter, CompareIteratorByHash> setEntries;
 
     uint64_t CalculateDescendantMaximum(txiter entry) const EXCLUSIVE_LOCKS_REQUIRED(cs);
+
+    SPKStates_t mapUsedSPK;
+
 private:
     typedef std::map<txiter, setEntries, CompareIteratorByHash> cacheMap;
 
