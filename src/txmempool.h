00ace64c5e8a56625034f7503dc11225e35d18fb
Luke Dashjr <luke-jr+git@utopios.org>
2017-02-13 05:09:42 +0000

    txmempool: Store pointers to transactions claiming SPKs

--- src/txmempool.h
+++ src/txmempool.h
@@ -58,8 +58,8 @@ bool TestLockPointValidity(CChain& active_chain, const LockPoints& lp) EXCLUSIVE
 
 enum MemPool_SPK_State {
     MSS_UNSEEN  = 0,
-    MSS_SPENT   = 1,
-    MSS_CREATED = 2,
+    MSS_SPENT   = 1,  // .second
+    MSS_CREATED = 2,  // .first
     MSS_BOTH    = 3,
 };
 
@@ -538,7 +538,7 @@ public:
 
     uint64_t CalculateDescendantMaximum(txiter entry) const EXCLUSIVE_LOCKS_REQUIRED(cs);
 
-    SPKStates_t mapUsedSPK;
+    std::map<uint160, std::pair<const CTransaction *, const CTransaction *>> mapUsedSPK;
 
 private:
     typedef std::map<txiter, setEntries, CompareIteratorByHash> cacheMap;
