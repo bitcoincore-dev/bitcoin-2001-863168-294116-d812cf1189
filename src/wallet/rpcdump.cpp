ee7caa09d3eb79b43e361f5c9d56836e40c64224
Russell Yanofsky <russ@yanofsky.org>
2017-02-14 09:38:29 -0500

    [wallet] Remove importmulti always-true check
    Remove "nLowestTimestamp <= chainActive.Tip()->GetBlockTimeMax()" check from
    importmulti, which is always true because nLowestTimestamp is set to the
    minimum of the most recent block time and all the imported key timestamps,
    which is necessarily lower than the maximum block time.

--- src/wallet/rpcdump.cpp
+++ src/wallet/rpcdump.cpp
@@ -1071,7 +1071,7 @@ UniValue importmulti(const JSONRPCRequest& mainRequest)
         }
     }
 
-    if (fRescan && fRunScan && requests.size() && nLowestTimestamp <= chainActive.Tip()->GetBlockTimeMax()) {
+    if (fRescan && fRunScan && requests.size()) {
         CBlockIndex* pindex = nLowestTimestamp > minimumTimestamp ? chainActive.FindEarliestAtLeast(nLowestTimestamp) : chainActive.Genesis();
 
         if (pindex) {
