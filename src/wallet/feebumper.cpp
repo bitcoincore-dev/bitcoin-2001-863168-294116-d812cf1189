731ca5c652d42806e82892695f4e43082c9d6a20
Russell Yanofsky <russ@yanofsky.org>
2017-03-09 17:48:04 -0500

    fixup! Refactor BumpFee, move relevant code into CFeeBumper

--- src/wallet/feebumper.cpp
+++ src/wallet/feebumper.cpp
@@ -20,19 +20,19 @@
 // calculation, but we should be able to refactor after priority is removed).
 // NOTE: this requires that all inputs must be in mapWallet (eg the tx should
 // be IsAllFromMe).
-int64_t CalculateMaximumSignedTxSize(const CTransaction &tx)
+int64_t CalculateMaximumSignedTxSize(const CWallet *pWallet, const CTransaction &tx)
 {
     CMutableTransaction txNew(tx);
-    std::vector<std::pair<CWalletTx *, unsigned int>> vCoins;
+    std::vector<std::pair<const CWalletTx *, unsigned int>> vCoins;
     // Look up the inputs.  We should have already checked that this transaction
     // IsAllFromMe(ISMINE_SPENDABLE), so every input should already be in our
     // wallet, with a valid index into the vout array.
     for (auto& input : tx.vin) {
-        const auto mi = pwalletMain->mapWallet.find(input.prevout.hash);
-        assert(mi != pwalletMain->mapWallet.end() && input.prevout.n < mi->second.tx->vout.size());
+        const auto mi = pWallet->mapWallet.find(input.prevout.hash);
+        assert(mi != pWallet->mapWallet.end() && input.prevout.n < mi->second.tx->vout.size());
         vCoins.emplace_back(std::make_pair(&(mi->second), input.prevout.n));
     }
-    if (!pwalletMain->DummySignTx(txNew, vCoins)) {
+    if (!pWallet->DummySignTx(txNew, vCoins)) {
         // This should never happen, because IsAllFromMe(ISMINE_SPENDABLE)
         // implies that we can sign for every input.
         return -1;
@@ -121,7 +121,7 @@ CFeeBumper::CFeeBumper(const CWallet *pWallet, const uint256 txidIn, int newConf
 
     // Calculate the expected size of the new transaction.
     int64_t txSize = GetVirtualTransactionSize(*(wtx.tx));
-    const int64_t maxNewTxSize = CalculateMaximumSignedTxSize(*wtx.tx);
+    const int64_t maxNewTxSize = CalculateMaximumSignedTxSize(pWallet, *wtx.tx);
     if (maxNewTxSize < 0) {
         vErrors.push_back("Transaction contains inputs that cannot be signed");
         currentResult = CFeeBumper::BumpFeeResult::INVALID_ADDRESS_OR_KEY;
