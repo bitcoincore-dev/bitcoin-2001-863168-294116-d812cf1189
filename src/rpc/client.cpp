sdaftuar/bitcoin

Add updatenode RPC
This commit adds an updatenode RPC for updating a single peer node. At
the moment this RPC can only be used to update the whitelisting settings
for the node, but can be extended in future to update other attributes
of the peer node.

src/rpc/client.cpp
@@ -115,6 +115,8 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "setban", 2, "bantime" },
     { "setban", 3, "absolute" },
     { "getpeerinfo", 0, "node" },
+    { "updatenode", 0, "node" },
+    { "updatenode", 1, "whitelisted" },
     { "setnetworkactive", 0, "state" },
     { "getmempoolancestors", 1, "verbose" },
     { "getmempooldescendants", 1, "verbose" },

bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

src/rpc/client.cpp
@@ -110,6 +110,8 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "setban", 2, "bantime" },
     { "setban", 3, "absolute" },
     { "getpeerinfo", 0, "node" },
+    { "updatepeer", 0, "node" },
+    { "updatepeer", 1, "whitelisted" },
     { "setnetworkactive", 0, "state" },
     { "getmempoolancestors", 1, "verbose" },
     { "getmempooldescendants", 1, "verbose" },