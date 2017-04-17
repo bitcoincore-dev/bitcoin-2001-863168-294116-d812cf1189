0a603f7bfe074096bd683cef86a259502dea5aca
Russell Yanofsky <russ@yanofsky.org>
2017-04-17 15:10:47 -0400

    Remove direct bitcoin calls from qt/splashscreen.cpp

--- src/qt/splashscreen.h
+++ src/qt/splashscreen.h
@@ -5,11 +5,17 @@
 #ifndef BITCOIN_QT_SPLASHSCREEN_H
 #define BITCOIN_QT_SPLASHSCREEN_H
 
-#include <functional>
 #include <QSplashScreen>
 
-class CWallet;
+#include <memory>
+
 class NetworkStyle;
+
+namespace ipc {
+class Handler;
+class Node;
+class Wallet;
+};
 
 /** Class for the splashscreen with information of the running client.
  *
@@ -22,7 +28,7 @@ class SplashScreen : public QWidget
     Q_OBJECT
 
 public:
-    explicit SplashScreen(Qt::WindowFlags f, const NetworkStyle *networkStyle);
+    explicit SplashScreen(ipc::Node& ipc_node, Qt::WindowFlags f, const NetworkStyle *networkStyle);
     ~SplashScreen();
 
 protected:
@@ -35,9 +41,6 @@ public Q_SLOTS:
 
     /** Show message and progress */
     void showMessage(const QString &message, int alignment, const QColor &color);
-
-    /** Sets the break action */
-    void setBreakAction(const std::function<void(void)> &action);
 protected:
     bool eventFilter(QObject * obj, QEvent * ev);
 
@@ -47,16 +50,19 @@ private:
     /** Disconnect core signals to splash screen */
     void unsubscribeFromCoreSignals();
     /** Connect wallet signals to splash screen */
-    void ConnectWallet(CWallet*);
+    void ConnectWallet(std::unique_ptr<ipc::Wallet> wallet);
 
     QPixmap pixmap;
     QString curMessage;
     QColor curColor;
     int curAlignment;
 
-    QList<CWallet*> connectedWallets;
-
-    std::function<void(void)> breakAction;
+    ipc::Node& m_ipc_node;
+    std::unique_ptr<ipc::Handler> m_handler_init_message;
+    std::unique_ptr<ipc::Handler> m_handler_show_progress;
+    std::unique_ptr<ipc::Handler> m_handler_load_wallet;
+    std::list<std::unique_ptr<ipc::Wallet>> m_connected_wallets;
+    std::list<std::unique_ptr<ipc::Handler>> m_connected_wallet_handlers;
 };
 
 #endif // BITCOIN_QT_SPLASHSCREEN_H
