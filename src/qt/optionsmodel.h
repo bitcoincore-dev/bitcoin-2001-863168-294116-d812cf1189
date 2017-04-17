bd541bbf98de5b955e9f3dc835d95986c2b4691f
Russell Yanofsky <russ@yanofsky.org>
2017-04-17 16:43:47 -0400

    Remove direct bitcoin calls from qt/optionsdialog.cpp

--- src/qt/optionsmodel.h
+++ src/qt/optionsmodel.h
@@ -76,6 +76,8 @@ public:
     void setRestartRequired(bool fRequired);
     bool isRestartRequired() const;
 
+    interface::Node& node() const { return m_node; }
+
 private:
     interface::Node& m_node;
     /* Qt-only settings */
