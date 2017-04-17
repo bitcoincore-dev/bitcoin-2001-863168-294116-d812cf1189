41f6a465d039bd12ff927deecc0cd12c76a66fd0
Russell Yanofsky <russ@yanofsky.org>
2017-04-17 15:10:47 -0400

    Remove direct bitcoin calls from qt/splashscreen.cpp

---
+++ src/interface/wallet.cpp
@@ -0,0 +1,37 @@
+#include <interface/wallet.h>
+
+#include <interface/handler.h>
+#include <rpc/server.h>
+#include <util.h>
+#include <wallet/init.h>
+#include <wallet/rpcwallet.h>
+#include <wallet/wallet.h>
+
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+class CScheduler;
+
+namespace interface {
+namespace {
+
+class WalletImpl : public Wallet
+{
+public:
+    WalletImpl(CWallet& wallet) : m_wallet(wallet) {}
+
+    std::unique_ptr<Handler> handleShowProgress(ShowProgressFn fn) override
+    {
+        return MakeHandler(m_wallet.ShowProgress.connect(fn));
+    }
+
+    CWallet& m_wallet;
+};
+
+} // namespace
+
+std::unique_ptr<Wallet> MakeWallet(CWallet& wallet) { return MakeUnique<WalletImpl>(wallet); }
+
+} // namespace interface
