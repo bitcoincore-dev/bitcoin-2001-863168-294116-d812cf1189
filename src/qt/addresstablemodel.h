33dba4a629f6f6b5a38bda90c23c504b7fb79ae1
Russell Yanofsky <russ@yanofsky.org>
2017-04-18 13:01:23 -0400

    Remove direct bitcoin calls from qt/addresstablemodel.cpp

--- src/qt/addresstablemodel.h
+++ src/qt/addresstablemodel.h
@@ -11,7 +11,9 @@
 class AddressTablePriv;
 class WalletModel;
 
-class CWallet;
+namespace interface {
+class Wallet;
+}
 
 /**
    Qt model of the address book in the core. This allows views to access and modify the address book.
@@ -21,7 +23,7 @@ class AddressTableModel : public QAbstractTableModel
     Q_OBJECT
 
 public:
-    explicit AddressTableModel(CWallet *wallet, WalletModel *parent = 0);
+    explicit AddressTableModel(WalletModel *parent = 0);
     ~AddressTableModel();
 
     enum ColumnIndex {
@@ -76,7 +78,6 @@ public:
 
 private:
     WalletModel *walletModel;
-    CWallet *wallet;
     AddressTablePriv *priv;
     QStringList columns;
     EditStatus editStatus;
