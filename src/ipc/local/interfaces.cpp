b23bf75a11624d61f8efeea0ba557b6458e9aac5
Russell Yanofsky <russ@yanofsky.org>
2017-04-18 13:23:23 -0400

    Remove direct bitcoin calls from qt/paymentserver.cpp

--- src/ipc/local/interfaces.cpp
+++ src/ipc/local/interfaces.cpp
@@ -158,6 +158,10 @@ public:
         }
         return result;
     }
+    std::set<CTxDestination> getAccountAddresses(const std::string& account) override
+    {
+        return m_wallet.GetAccountAddresses(account);
+    }
     bool addDestData(const CTxDestination& dest, const std::string& key, const std::string& value) override
     {
         LOCK(m_wallet.cs_wallet);
@@ -331,6 +335,7 @@ public:
     bool softSetArg(const std::string& arg, const std::string& value) override { return gArgs.SoftSetArg(arg, value); }
     bool softSetBoolArg(const std::string& arg, bool value) override { return gArgs.SoftSetBoolArg(arg, value); }
     void selectParams(const std::string& network) override { ::SelectParams(network); }
+    std::string getNetwork() override { return ::Params().NetworkIDString(); }
     void initLogging() override { ::InitLogging(); }
     void initParameterInteraction() override { ::InitParameterInteraction(); }
     std::string getWarnings(const std::string& type) override { return ::GetWarnings(type); }
