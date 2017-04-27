e3b11582c90343b8b4d222e0e1cc0d10ca9cc983
Pieter Wuille <pieter.wuille@gmail.com>
2017-04-27 12:25:13 -0700

    Add -stopatheight for benchmarking
    Github-Pull: #10290
    Rebased-From: b297426c9670fa83a12096f9db5666c15e1bb806

--- src/validation.cpp
+++ src/validation.cpp
@@ -2548,6 +2548,9 @@ bool ActivateBestChain(CValidationState &state, const CChainParams& chainparams,
         return false;
     }
 
+    int nStopAtHeight = GetArg("-stopatheight", DEFAULT_STOPATHEIGHT);
+    if (nStopAtHeight && pindexNewTip->nHeight >= nStopAtHeight) StartShutdown();
+
     return true;
 }
 
