fcbbe3b2a313fed78ff3bd322b8fac85e8688ceb
Russell Yanofsky <russ@yanofsky.org>
2017-05-03 15:57:56 -0400

    Remove uses of ParseConfirmTarget in wallet code
    This commit does not change behavior.

--- src/interface/chain.h
+++ src/interface/chain.h
@@ -193,6 +193,9 @@ public:
         uint64_t max_tries,
         bool keep_script) = 0;
 
+    //! Parse confirm target.
+    virtual unsigned int parseConfirmTarget(const UniValue& value) = 0;
+
     //! Chain notifications.
     class Notifications
     {
