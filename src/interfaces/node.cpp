d9f54ba5fa7af2c1a9020bae95583dd69a43f11e
Russell Yanofsky <russ@yanofsky.org>
2017-05-30 15:55:17 -0400

    Pass chain and client variables where needed
    This commit does not change behavior. All it does is pass new function
    parameters.
    
    It is easiest to review this change with:
    
        git log -p -n1 -U0 --word-diff-regex=.

--- src/interfaces/node.cpp
+++ src/interfaces/node.cpp
@@ -9,6 +9,7 @@
 #include <chain.h>
 #include <chainparams.h>
 #include <init.h>
+#include <interfaces/chain.h>
 #include <interfaces/handler.h>
 #include <interfaces/wallet.h>
 #include <net.h>
@@ -49,6 +50,8 @@ namespace {
 
 class NodeImpl : public Node
 {
+public:
+    NodeImpl() { m_interfaces.chain = MakeChain(); }
     bool parseParameters(int argc, const char* const argv[], std::string& error) override
     {
         return gArgs.ParseParameters(argc, argv, error);
@@ -67,11 +70,11 @@ class NodeImpl : public Node
         return AppInitBasicSetup() && AppInitParameterInteraction() && AppInitSanityChecks() &&
                AppInitLockDataDirectory();
     }
-    bool appInitMain() override { return AppInitMain(); }
+    bool appInitMain() override { return AppInitMain(m_interfaces); }
     void appShutdown() override
     {
         Interrupt();
-        Shutdown();
+        Shutdown(m_interfaces);
     }
     void startShutdown() override { StartShutdown(); }
     bool shutdownRequested() override { return ShutdownRequested(); }
@@ -283,6 +286,7 @@ class NodeImpl : public Node
                     GuessVerificationProgress(Params().TxData(), block));
             }));
     }
+    InitInterfaces m_interfaces;
 };
 
 } // namespace
