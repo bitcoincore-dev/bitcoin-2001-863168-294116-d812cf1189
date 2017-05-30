507dd01f7a5f596843a4b4f5350cc57268a38884
Russell Yanofsky <russ@yanofsky.org>
2017-05-30 15:55:17 -0400

    Pass chain and client variables where needed
    This commit does not change behavior. All it does is pass new function
    parameters.
    
    It is easiest to review this change with:
    
        git log -p -n1 -U0 --word-diff-regex=.

--- src/bitcoind.cpp
+++ src/bitcoind.cpp
@@ -55,6 +55,8 @@ static void WaitForShutdown()
 //
 static bool AppInit(int argc, char* argv[])
 {
+    InitInterfaces interfaces;
+    interfaces.chain = interfaces::MakeChain();
     bool fRet = false;
 
     //
@@ -161,7 +163,7 @@ static bool AppInit(int argc, char* argv[])
             // If locking the data directory failed, exit immediately
             return false;
         }
-        fRet = AppInitMain();
+        fRet = AppInitMain(interfaces);
     }
     catch (const std::exception& e) {
         PrintExceptionContinue(&e, "AppInit()");
@@ -175,7 +177,7 @@ static bool AppInit(int argc, char* argv[])
     } else {
         WaitForShutdown();
     }
-    Shutdown();
+    Shutdown(interfaces);
 
     return fRet;
 }
