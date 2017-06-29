b3b75fa8a062ba43b640d42a7608085eba398e3e
Luke Dashjr <luke-jr+git@utopios.org>
2017-06-29 00:20:35 +0000

    script/interpreter: ScriptExecutionDebugger callback for final script-end checks

--- src/script/interpreter.h
+++ src/script/interpreter.h
@@ -216,6 +216,7 @@ public:
 
 protected:
     virtual void ScriptPreStep(ScriptExecution&, const CScript::const_iterator&, opcodetype&, ScriptExecution::StackElementType&) {}
+    virtual void ScriptEOF(ScriptExecution&, const CScript::const_iterator&) {}
 
     friend bool ScriptExecution::Eval(ScriptError* serror);
 };
