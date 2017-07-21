1d34829d5b04baa1eaf4c124464ad9d06269b488
Russell Yanofsky <russ@yanofsky.org>
2017-07-21 17:43:37 -0400

    Remove uses of wallet functions in init.cpp
    This commit mostly does not change behavior. The only change is that "No wallet
    support compiled in!" and "Wallet disabled!" messages are now logged a little
    earlier.

--- src/init.cpp
+++ src/init.cpp
@@ -188,11 +188,9 @@ void Shutdown(ipc::Chain::Clients& ipc_clients)
     StopREST();
     StopRPC();
     StopHTTPServer();
-#ifdef ENABLE_WALLET
-    for (CWalletRef pwallet : vpwallets) {
-        pwallet->Flush(false);
+    for (const auto& client : ipc_clients) {
+        client->stop();
     }
-#endif
     MapPort(false);
     UnregisterValidationInterface(peerLogic.get());
     peerLogic.reset();
@@ -245,11 +243,9 @@ void Shutdown(ipc::Chain::Clients& ipc_clients)
         delete pblocktree;
         pblocktree = nullptr;
     }
-#ifdef ENABLE_WALLET
-    for (CWalletRef pwallet : vpwallets) {
-        pwallet->Flush(true);
+    for (const auto& client : ipc_clients) {
+        client->shutdown();
     }
-#endif
 
 #if ENABLE_ZMQ
     if (pzmqNotificationInterface) {
@@ -269,12 +265,7 @@ void Shutdown(ipc::Chain::Clients& ipc_clients)
     UnregisterAllValidationInterfaces();
     GetMainSignals().UnregisterBackgroundSignalScheduler();
     GetMainSignals().UnregisterWithMempoolSignals(mempool);
-#ifdef ENABLE_WALLET
-    for (CWalletRef pwallet : vpwallets) {
-        delete pwallet;
-    }
-    vpwallets.clear();
-#endif
+    ipc_clients.clear();
     globalVerifyHandle.reset();
     ECC_Stop();
     LogPrintf("%s: done\n", __func__);
@@ -1023,10 +1014,16 @@ bool AppInitParameterInteraction(ipc::Chain& ipc_chain, ipc::Chain::Clients& ipc
         fPruneMode = true;
     }
 
-    RegisterAllCoreRPCCommands(tableRPC);
 #ifdef ENABLE_WALLET
-    RegisterWalletRPCCommands(tableRPC);
+    CWallet::MakeClients(ipc_chain, ipc_clients);
+#else
+    LogPrintf("No wallet support compiled in!\n");
 #endif
+
+    RegisterAllCoreRPCCommands(tableRPC);
+    for (const auto& client : ipc_clients) {
+        client->registerRpcs();
+    }
 
     nConnectTimeout = gArgs.GetArg("-timeout", DEFAULT_CONNECT_TIMEOUT);
     if (nConnectTimeout <= 0)
@@ -1568,12 +1565,11 @@ bool AppInitMain(ipc::Chain& ipc_chain, const ipc::Chain::Clients& ipc_clients, 
     fFeeEstimatesInitialized = true;
 
     // ********************************************************* Step 8: load wallet
-#ifdef ENABLE_WALLET
-    if (!CWallet::InitLoadWallet(ipc_chain))
-        return false;
-#else
-    LogPrintf("No wallet support compiled in!\n");
-#endif
+    for (const auto& client : ipc_clients) {
+        if (!client->prepare()) {
+            return false;
+        }
+    }
 
     // ********************************************************* Step 9: data directory maintenance
 
@@ -1699,11 +1695,9 @@ bool AppInitMain(ipc::Chain& ipc_chain, const ipc::Chain::Clients& ipc_clients, 
     SetRPCWarmupFinished();
     uiInterface.InitMessage(_("Done loading"));
 
-#ifdef ENABLE_WALLET
-    for (CWalletRef pwallet : vpwallets) {
-        pwallet->postInitProcess(scheduler);
+    for (const auto& client : ipc_clients) {
+        client->start(scheduler);
     }
-#endif
 
     return !fRequestShutdown;
 }
--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -3286,9 +3286,6 @@ static const CRPCCommand commands[] =
 
 void RegisterWalletRPCCommands(CRPCTable &t)
 {
-    if (gArgs.GetBoolArg("-disablewallet", false))
-        return;
-
     for (unsigned int vcidx = 0; vcidx < ARRAYLEN(commands); vcidx++)
         t.appendCommand(commands[vcidx].name, &commands[vcidx]);
 }
