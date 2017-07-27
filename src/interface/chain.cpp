ae01d685b2b8303eb00e91de482ae966d9b96b76
Russell Yanofsky <russ@yanofsky.org>
2017-07-27 10:08:31 -0400

    Remove uses of chainActive and mapBlockIndex in wallet code
    This commit does not change behavior.

--- src/interface/chain.cpp
+++ src/interface/chain.cpp
@@ -1,10 +1,15 @@
 #include <interface/chain.h>
 
+#include <chain.h>
+#include <chainparams.h>
+#include <primitives/block.h>
 #include <sync.h>
+#include <uint256.h>
 #include <util.h>
 #include <validation.h>
 
 #include <memory>
+#include <unordered_map>
 #include <utility>
 
 namespace interface {
@@ -12,6 +17,104 @@ namespace {
 
 class LockImpl : public Chain::Lock
 {
+    Optional<int> getHeight() override
+    {
+        int height = ::chainActive.Height();
+        if (height >= 0) {
+            return height;
+        }
+        return nullopt;
+    }
+    Optional<int> getBlockHeight(const uint256& hash) override
+    {
+        auto it = ::mapBlockIndex.find(hash);
+        if (it != ::mapBlockIndex.end() && it->second) {
+            if (::chainActive.Contains(it->second)) {
+                return it->second->nHeight;
+            }
+        }
+        return nullopt;
+    }
+    int getBlockDepth(const uint256& hash) override
+    {
+        const Optional<int> tip_height = getHeight();
+        const Optional<int> height = getBlockHeight(hash);
+        return tip_height && height ? *tip_height - *height + 1 : 0;
+    }
+    uint256 getBlockHash(int height) override { return ::chainActive[height]->GetBlockHash(); }
+    int64_t getBlockTime(int height) override { return ::chainActive[height]->GetBlockTime(); }
+    int64_t getBlockMedianTimePast(int height) override { return ::chainActive[height]->GetMedianTimePast(); }
+    bool blockHasTransactions(int height) override
+    {
+        CBlockIndex* block = ::chainActive[height];
+        return block && (block->nStatus & BLOCK_HAVE_DATA) && block->nTx > 0;
+    }
+    Optional<int> findEarliestAtLeast(int64_t time) override
+    {
+        CBlockIndex* block = ::chainActive.FindEarliestAtLeast(time);
+        if (block) {
+            return block->nHeight;
+        }
+        return nullopt;
+    }
+    Optional<int> findLastBefore(int64_t time, int start_height) override
+    {
+        CBlockIndex* block = ::chainActive[start_height];
+        while (block && block->GetBlockTime() < time) {
+            block = ::chainActive.Next(block);
+        }
+        if (block) {
+            return block->nHeight;
+        }
+        return nullopt;
+    }
+    Optional<int> findPruned(int start_height, Optional<int> stop_height) override
+    {
+        if (::fPruneMode) {
+            CBlockIndex* block = stop_height ? ::chainActive[*stop_height] : ::chainActive.Tip();
+            while (block && block->nHeight >= start_height) {
+                if (!(block->nStatus & BLOCK_HAVE_DATA)) {
+                    return block->nHeight;
+                }
+                block = block->pprev;
+            }
+        }
+        return nullopt;
+    }
+    Optional<int> findFork(const uint256& hash, Optional<int>* height) override
+    {
+        const CBlockIndex *block{nullptr}, *fork{nullptr};
+        auto it = ::mapBlockIndex.find(hash);
+        if (it != ::mapBlockIndex.end()) {
+            block = it->second;
+            fork = ::chainActive.FindFork(block);
+        }
+        if (height) {
+            if (block) {
+                *height = block->nHeight;
+            } else {
+                height->reset();
+            }
+        }
+        if (fork) {
+            return fork->nHeight;
+        }
+        return nullopt;
+    }
+    bool isPotentialTip(const uint256& hash) override
+    {
+        if (::chainActive.Tip()->GetBlockHash() == hash) return true;
+        auto it = ::mapBlockIndex.find(hash);
+        return it != ::mapBlockIndex.end() && it->second->GetAncestor(::chainActive.Height()) == ::chainActive.Tip();
+    }
+    CBlockLocator getLocator() override { return ::chainActive.GetLocator(); }
+    Optional<int> findLocatorFork(const CBlockLocator& locator) override
+    {
+        if (CBlockIndex* fork = FindForkInGlobalIndex(::chainActive, locator)) {
+            return fork->nHeight;
+        }
+        return nullopt;
+    }
 };
 
 class LockingStateImpl : public LockImpl, public CCriticalBlock
@@ -31,6 +134,34 @@ public:
         return std::move(result);
     }
     std::unique_ptr<Chain::Lock> assumeLocked() override { return MakeUnique<LockImpl>(); }
+    bool findBlock(const uint256& hash, CBlock* block, int64_t* time, int64_t* time_max) override
+    {
+        CBlockIndex* index;
+        {
+            LOCK(cs_main);
+            auto it = ::mapBlockIndex.find(hash);
+            if (it == ::mapBlockIndex.end()) {
+                return false;
+            }
+            index = it->second;
+            if (time) {
+                *time = index->GetBlockTime();
+            }
+            if (time_max) {
+                *time_max = index->GetBlockTimeMax();
+            }
+        }
+        if (block && !ReadBlockFromDisk(*block, index, Params().GetConsensus())) {
+            block->SetNull();
+        }
+        return true;
+    }
+    double guessVerificationProgress(const uint256& block_hash) override
+    {
+        LOCK(cs_main);
+        auto it = ::mapBlockIndex.find(block_hash);
+        return GuessVerificationProgress(Params().TxData(), it != ::mapBlockIndex.end() ? it->second : nullptr);
+    }
 };
 
 } // namespace
