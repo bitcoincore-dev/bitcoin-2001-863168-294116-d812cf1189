228a667177f0bffc48ad891977d94590adca9741
Russell Yanofsky <russ@yanofsky.org>
2017-07-27 10:08:31 -0400

    Remove uses of chainActive and mapBlockIndex in wallet code
    This commit does not change behavior.

--- src/interface/chain.cpp
+++ src/interface/chain.cpp
@@ -1,9 +1,14 @@
 #include <interface/chain.h>
 
+#include <chain.h>
+#include <chainparams.h>
 #include <interface/util.h>
+#include <primitives/block.h>
 #include <sync.h>
+#include <uint256.h>
 #include <validation.h>
 
+#include <unordered_map>
 #include <utility>
 
 namespace interface {
@@ -11,6 +16,76 @@ namespace {
 
 class LockImpl : public Chain::Lock
 {
+public:
+    int getHeight() override { return ::chainActive.Height(); }
+    int getBlockHeight(const uint256& hash) override
+    {
+        auto it = ::mapBlockIndex.find(hash);
+        if (it != ::mapBlockIndex.end() && it->second) {
+            if (::chainActive.Contains(it->second)) {
+                return it->second->nHeight;
+            }
+        }
+        return -1;
+    }
+    int getBlockDepth(const uint256& hash) override
+    {
+        int height = getBlockHeight(hash);
+        return height < 0 ? 0 : ::chainActive.Height() - height + 1;
+    }
+    uint256 getBlockHash(int height) override { return ::chainActive[height]->GetBlockHash(); }
+    int64_t getBlockTime(int height) override { return ::chainActive[height]->GetBlockTime(); }
+    int64_t getBlockTimeMax(int height) override { return ::chainActive[height]->GetBlockTimeMax(); }
+    int64_t getBlockMedianTimePast(int height) override { return ::chainActive[height]->GetMedianTimePast(); }
+    bool blockHasTransactions(int height) override
+    {
+        CBlockIndex* block = ::chainActive[height];
+        return block && (block->nStatus & BLOCK_HAVE_DATA) && block->nTx > 0;
+    }
+    bool readBlockFromDisk(int height, CBlock& block) override
+    {
+        return ReadBlockFromDisk(block, ::chainActive[height], Params().GetConsensus());
+    }
+    double guessVerificationProgress(int height) override
+    {
+        return GuessVerificationProgress(Params().TxData(), ::chainActive[height]);
+    }
+    int findEarliestAtLeast(int64_t time) override
+    {
+        CBlockIndex* block = ::chainActive.FindEarliestAtLeast(time);
+        return block ? block->nHeight : -1;
+    }
+    int findLastBefore(int64_t time, int start_height) override
+    {
+        CBlockIndex* block = ::chainActive[start_height];
+        while (block && block->GetBlockTime() < time) {
+            block = ::chainActive.Next(block);
+        }
+        return block ? block->nHeight : -1;
+    }
+    bool isPotentialTip(const uint256& hash) override
+    {
+        if (::chainActive.Tip()->GetBlockHash() == hash) return true;
+        auto it = ::mapBlockIndex.find(hash);
+        return it != ::mapBlockIndex.end() && it->second->GetAncestor(::chainActive.Height()) == ::chainActive.Tip();
+    }
+    int findFork(const uint256& hash, int* height) override
+    {
+        const CBlockIndex *block{nullptr}, *fork{nullptr};
+        auto it = ::mapBlockIndex.find(hash);
+        if (it != ::mapBlockIndex.end()) {
+            block = it->second;
+            fork = ::chainActive.FindFork(block);
+        }
+        if (height) *height = block ? block->nHeight : -1;
+        return fork ? fork->nHeight : -1;
+    }
+    CBlockLocator getLocator() override { return ::chainActive.GetLocator(); }
+    int findLocatorFork(const CBlockLocator& locator) override
+    {
+        CBlockIndex* fork = FindForkInGlobalIndex(::chainActive, locator);
+        return fork ? fork->nHeight : -1;
+    }
 };
 
 class LockingStateImpl : public LockImpl, public CCriticalBlock
@@ -30,6 +105,21 @@ public:
         return std::move(result);
     }
     std::unique_ptr<Chain::Lock> assumeLocked() override { return MakeUnique<LockImpl>(); }
+    bool findBlock(const uint256& hash, CBlock* block, int64_t* time) override
+    {
+        LOCK(cs_main);
+        auto it = ::mapBlockIndex.find(hash);
+        if (it == ::mapBlockIndex.end()) {
+            return false;
+        }
+        if (block && !ReadBlockFromDisk(*block, it->second, Params().GetConsensus())) {
+            block->SetNull();
+        }
+        if (time) {
+            *time = it->second->GetBlockTime();
+        }
+        return true;
+    }
 };
 
 } // namespace
