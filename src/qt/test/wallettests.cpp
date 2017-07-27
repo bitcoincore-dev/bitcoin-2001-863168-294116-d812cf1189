8126afc28082ffd954e26e385f006a9a2e5d8d74
Russell Yanofsky <russ@yanofsky.org>
2017-07-27 10:08:31 -0400

    Remove uses of chainActive and mapBlockIndex in wallet code
    This commit does not change behavior.

--- src/qt/test/wallettests.cpp
+++ src/qt/test/wallettests.cpp
@@ -1,6 +1,7 @@
 #include <qt/test/wallettests.h>
 #include <qt/test/util.h>
 
+#include <init.h>
 #include <interfaces/chain.h>
 #include <interfaces/node.h>
 #include <qt/bitcoinamountfield.h>
@@ -147,7 +148,7 @@ void TestGUI()
         auto locked_chain = wallet->chain().lock();
         WalletRescanReserver reserver(wallet.get());
         reserver.reserve();
-        wallet->ScanForWalletTransactions(chainActive.Genesis(), nullptr, reserver, true);
+        wallet->ScanForWalletTransactions(locked_chain->getBlockHash(0), {} /* stop block */, reserver, true /* update */);
     }
     wallet->SetBroadcastTransactions(true);
 
