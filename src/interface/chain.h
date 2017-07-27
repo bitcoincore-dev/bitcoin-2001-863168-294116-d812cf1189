a6f300251119b8f2021ec724116b5601b5779999
Russell Yanofsky <russ@yanofsky.org>
2017-07-27 10:08:31 -0400

    Remove uses of chainActive and mapBlockIndex in wallet code
    This commit does not change behavior.

--- src/interface/chain.h
+++ src/interface/chain.h
@@ -1,11 +1,17 @@
 #ifndef BITCOIN_INTERFACE_CHAIN_H
 #define BITCOIN_INTERFACE_CHAIN_H
 
+#include <optional.h>
+
 #include <memory>
+#include <stdint.h>
 #include <string>
 #include <vector>
 
+class CBlock;
 class CScheduler;
+class uint256;
+struct CBlockLocator;
 
 namespace interface {
 
@@ -24,6 +30,72 @@ public:
     {
     public:
         virtual ~Lock() {}
+
+        //! Get current chain height, not including genesis block (returns 0 if
+        //! chain only contains genesis block, nothing if chain does not contain
+        //! any blocks).
+        virtual Optional<int> getHeight() = 0;
+
+        //! Get block height above genesis block. Returns 0 for genesis block,
+        //! for following block, and so on. Returns nothing for a block not
+        //! included in the current chain.
+        virtual Optional<int> getBlockHeight(const uint256& hash) = 0;
+
+        //! Get block depth. Returns 1 for chain tip, 2 for preceding block, and
+        //! so on. Returns 0 for a block not included in the current chain.
+        virtual int getBlockDepth(const uint256& hash) = 0;
+
+        //! Get block hash.
+        virtual uint256 getBlockHash(int height) = 0;
+
+        //! Get block time.
+        virtual int64_t getBlockTime(int height) = 0;
+
+        //! Get max time of block and all ancestors.
+        virtual int64_t getBlockTimeMax(int height) = 0;
+
+        //! Get block median time past.
+        virtual int64_t getBlockMedianTimePast(int height) = 0;
+
+        //! Check if block is empty.
+        virtual bool blockHasTransactions(int height) = 0;
+
+        //! Read block from disk.
+        virtual bool readBlockFromDisk(int height, CBlock& block) = 0;
+
+        //! Estimate fraction of total transactions verified if blocks up to
+        //! given height are verified.
+        virtual double guessVerificationProgress(int height) = 0;
+
+        //! Return height of earliest block in chain with timestamp equal or
+        //! greater than the given time, or nothing if there is no block with a
+        //! high enough timestamp.
+        virtual Optional<int> findEarliestAtLeast(int64_t time) = 0;
+
+        //! Return height of last block in chain with timestamp less than the
+        //! given, and height less than or equal to the given, or nothing if
+        //! there is no such block.
+        virtual Optional<int> findLastBefore(int64_t time, int start_height) = 0;
+
+        //! Return height of last block in the specified range which is pruned, or
+        //! nothing if no block in the range is pruned. Range is inclusive.
+        virtual Optional<int> findPruned(int start_height = 0, Optional<int> stop_height = nullopt) = 0;
+
+        //! Return height of the highest block on the chain that is an ancestor
+        //! of the specified block. Also return the height of the specified
+        //! block as an optinal output parameter.
+        virtual Optional<int> findFork(const uint256& hash, Optional<int>* height) = 0;
+
+        //! Return true if block hash points to the current chain tip, or to a
+        //! possible descendant of the current chain tip that isn't currently
+        //! connected.
+        virtual bool isPotentialTip(const uint256& hash) = 0;
+
+        //! Get locator for the current chain tip.
+        virtual CBlockLocator getLocator() = 0;
+
+        //! Return height of block on the chain using locator.
+        virtual Optional<int> findLocatorFork(const CBlockLocator& locator) = 0;
     };
 
     //! Return Lock interface. Chain is locked when this is called, and
@@ -34,6 +106,9 @@ public:
     //! method is temporary and is only used in a few places to avoid changing
     //! behavior while code is transitioned to use the LockState interface.
     virtual std::unique_ptr<Lock> assumeLocked() = 0;
+
+    //! Return whether node has the block and optionally return block metadata or contents.
+    virtual bool findBlock(const uint256& hash, CBlock* block = nullptr, int64_t* time = nullptr) = 0;
 
     //! Interface to let node manage chain clients (wallets, or maybe tools for
     //! monitoring and analysis in the future).
