4a496bdb85c4c28ad1d4134d6b26b30b3c21081f
Russell Yanofsky <russ@yanofsky.org>
2017-07-28 18:13:23 -0400

    Remove uses of CheckFinalTx in wallet code
    This commit does not change behavior.

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -2026,7 +2026,7 @@ bool CWalletTx::InMempool() const
 bool CWalletTx::IsTrusted(interfaces::Chain::Lock& locked_chain) const
 {
     // Quick answer in most cases
-    if (!CheckFinalTx(*tx))
+    if (!locked_chain.checkFinalTx(*tx))
         return false;
     int nDepth = GetDepthInMainChain(locked_chain);
     if (nDepth >= 1)
@@ -2218,7 +2218,7 @@ CAmount CWallet::GetLegacyBalance(const isminefilter& filter, int minDepth, cons
     for (const auto& entry : mapWallet) {
         const CWalletTx& wtx = entry.second;
         const int depth = wtx.GetDepthInMainChain(*locked_chain);
-        if (depth < 0 || !CheckFinalTx(*wtx.tx) || wtx.GetBlocksToMaturity(*locked_chain) > 0) {
+        if (depth < 0 || !locked_chain->checkFinalTx(*wtx.tx) || wtx.GetBlocksToMaturity(*locked_chain) > 0) {
             continue;
         }
 
@@ -2276,7 +2276,7 @@ void CWallet::AvailableCoins(interfaces::Chain::Lock& locked_chain, std::vector<
         const uint256& wtxid = entry.first;
         const CWalletTx* pcoin = &entry.second;
 
-        if (!CheckFinalTx(*pcoin->tx))
+        if (!locked_chain.checkFinalTx(*pcoin->tx))
             continue;
 
         if (pcoin->IsCoinBase() && pcoin->GetBlocksToMaturity(locked_chain) > 0)
