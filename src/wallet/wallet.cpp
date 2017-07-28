07085ada85de88d2a7241685fd68f3b51a7283d3
Russell Yanofsky <russ@yanofsky.org>
2017-07-28 19:42:27 -0400

    Remove use of TransactionWithinChainLimit in wallet code
    This commit does not change behavior.

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -2456,7 +2456,7 @@ bool CWallet::OutputEligibleForSpending(const COutput& output, const CoinEligibi
     if (output.nDepth < (output.tx->IsFromMe(ISMINE_ALL) ? eligibility_filter.conf_mine : eligibility_filter.conf_theirs))
         return false;
 
-    if (!mempool.TransactionWithinChainLimit(output.tx->GetHash(), eligibility_filter.max_ancestors))
+    if (!m_chain->transactionWithinChainLimit(output.tx->GetHash(), eligibility_filter.max_ancestors))
         return false;
 
     return true;
