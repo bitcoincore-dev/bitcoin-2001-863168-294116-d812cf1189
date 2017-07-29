b45724e6b9fde3d9f886ae24cf45c8df97e305fb
Russell Yanofsky <russ@yanofsky.org>
2017-07-28 21:40:29 -0400

    Remove uses of fee globals in wallet code
    This commit does not change behavior.

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -1173,6 +1173,8 @@ public:
     CTxDestination AddAndGetDestinationForScript(const CScript& script, OutputType);
 };
 
+CFeeRate GetDiscardRate(const CBlockPolicyEstimator& estimator);
+
 /** A key allocated from the key pool. */
 class CReserveKey final : public CReserveScript
 {
