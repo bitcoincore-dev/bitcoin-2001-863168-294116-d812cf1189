cebfe85acc5cc8f465237e650768c30cd58e2d59
Russell Yanofsky <russ@yanofsky.org>
2017-07-28 22:54:31 -0400

    Remove uses of GetAdjustedTime in wallet code
    This commit does not change behavior.

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -765,7 +765,7 @@ bool CWallet::AccountMove(std::string strFrom, std::string strTo, CAmount nAmoun
     if (!walletdb.TxnBegin())
         return false;
 
-    int64_t nNow = GetAdjustedTime();
+    int64_t nNow = m_ipc_chain->getAdjustedTime();
 
     // Debit
     CAccountingEntry debit;
@@ -884,7 +884,7 @@ bool CWallet::AddToWallet(const CWalletTx& wtxIn, bool fFlushOnClose)
     bool fInsertedNew = ret.second;
     if (fInsertedNew)
     {
-        wtx.nTimeReceived = GetAdjustedTime();
+        wtx.nTimeReceived = m_ipc_chain->getAdjustedTime();
         wtx.nOrderPos = IncOrderPosNext(&walletdb);
         wtxOrdered.insert(std::make_pair(wtx.nOrderPos, TxPair(&wtx, nullptr)));
         wtx.nTimeSmart = ComputeTimeSmart(wtx);
