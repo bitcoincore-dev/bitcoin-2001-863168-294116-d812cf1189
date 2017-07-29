065814a9ceee40d19e39e2af10ca206a467f1356
Russell Yanofsky <russ@yanofsky.org>
2017-07-28 22:54:31 -0400

    Remove uses of GetAdjustedTime in wallet code
    This commit does not change behavior.

--- src/interfaces/chain.cpp
+++ src/interfaces/chain.cpp
@@ -15,6 +15,7 @@
 #include <protocol.h>
 #include <sync.h>
 #include <threadsafety.h>
+#include <timedata.h>
 #include <txmempool.h>
 #include <uint256.h>
 #include <util/system.h>
@@ -243,6 +244,7 @@ public:
     }
     bool getPruneMode() override { return ::fPruneMode; }
     bool p2pEnabled() override { return g_connman != nullptr; }
+    int64_t getAdjustedTime() override { return GetAdjustedTime(); }
 };
 
 } // namespace
