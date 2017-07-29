f0b3561de69706b83d0c364f34b1dcdc96d05cb9
Russell Yanofsky <russ@yanofsky.org>
2017-07-28 21:40:29 -0400

    Remove uses of fee globals in wallet code
    This commit does not change behavior.

--- src/interfaces/chain.cpp
+++ src/interfaces/chain.cpp
@@ -7,6 +7,7 @@
 #include <chain.h>
 #include <chainparams.h>
 #include <net.h>
+#include <policy/fees.h>
 #include <policy/policy.h>
 #include <policy/rbf.h>
 #include <primitives/block.h>
@@ -220,6 +221,15 @@ public:
         LOCK(::mempool.cs);
         return ::mempool.CalculateMemPoolAncestors(entry, setAncestors, nLimitAncestors, nLimitAncestorSize,
             nLimitDescendants, nLimitDescendantSize, errString);
+    }
+    CFeeRate estimateSmartFee(int num_blocks, bool conservative, FeeCalculation* calc) override
+    {
+        return ::feeEstimator.estimateSmartFee(num_blocks, calc, conservative);
+    }
+    int estimateMaxBlocks() override { return ::feeEstimator.HighestTargetTracked(FeeEstimateHorizon::LONG_HALFLIFE); }
+    CFeeRate poolMinFee() override
+    {
+        return ::mempool.GetMinFee(gArgs.GetArg("-maxmempool", DEFAULT_MAX_MEMPOOL_SIZE) * 1000000);
     }
 };
 
