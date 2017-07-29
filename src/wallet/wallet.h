9df667f17373fe5c85442757da304c83b72d5ef7
Russell Yanofsky <russ@yanofsky.org>
2017-07-28 21:40:29 -0400

    Remove uses of fee globals in wallet code
    This commit does not change behavior.

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -70,8 +70,6 @@ class CCoinControl;
 class COutput;
 class CReserveKey;
 class CScript;
-class CTxMemPool;
-class CBlockPolicyEstimator;
 class CWalletTx;
 struct FeeCalculation;
 enum class FeeEstimateMode;
