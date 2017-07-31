57d5921900fafe59330f3603abf24edceaeda75b
Russell Yanofsky <russ@yanofsky.org>
2017-07-31 11:46:13 -0400

    Remove use of CRPCTable::appendCommand in wallet code
    This commit does not change behavior.

--- src/ipc/util.h
+++ src/ipc/util.h
@@ -12,6 +12,15 @@ std::unique_ptr<T> MakeUnique(Args&&... args)
     return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 
+//! Extension of std::reference_wrapper with operator< for use as map key.
+template <typename T>
+class Ref : public std::reference_wrapper<T>
+{
+public:
+    using std::reference_wrapper<T>::reference_wrapper;
+    bool operator<(const Ref<T>& other) const { return this->get() < other.get(); }
+};
+
 } // namespace ipc
 
 #endif // BITCOIN_IPC_UTIL_H
