rebroad/bitcoin

Store mempool min fee state in mempool.dat

src/validation.cpp
@@ -4167,6 +4167,16 @@ bool LoadMempool(void)
         std::map<std::string, std::vector<unsigned char>> mapData;
         file >> mapData;
 
+        auto it = mapData.find("minfee");
+        if (it == mapData.end()) {
+            try {
+                CDataStream ss(it->second, SER_NETWORK, PROTOCOL_VERSION);
+                mempool.LoadMinFeeInternal(ss);
+            } catch (const std::exception& e) {
+                LogPrintf("Failed to deserialize mempool %s from disk: %s. Continuing anyway.\n", "minfee", e.what());
+            }
+        }
+
         it = mapData.find("deltas");
         if (it == mapData.end()) {
             try {
@@ -4264,6 +4274,11 @@ void DumpMempool(void)
             }
 
             mapData["txs"] = SerializeToVector(txMapDatas);
+        }
+        {
+            CDataStream ss(SER_NETWORK, PROTOCOL_VERSION);
+            mempool.DumpMinFeeInternal(ss);
+            mapData["minfee"] = std::vector<unsigned char>(ss.begin(), ss.end());
         }
 
         FILE* filestr = fopen((GetDataDir() / "mempool.dat.new").string().c_str(), "w");

bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

src/validation.cpp
@@ -4285,7 +4285,17 @@ bool LoadMempool(void)
         std::map<std::string, std::vector<unsigned char>> mapData;
         file >> mapData;
 
-        auto it = mapData.find("deltas");
+        auto it = mapData.find("minfee");
+        if (it != mapData.end()) {
+            try {
+                CDataStream ss(it->second, SER_DISK, CLIENT_VERSION);
+                mempool.LoadMinFeeInternal(ss);
+            } catch (const std::exception& e) {
+                LogPrintf("Failed to deserialize mempool %s from disk: %s. Continuing anyway.\n", "minfee", e.what());
+            }
+        }
+
+        it = mapData.find("deltas");
         if (it != mapData.end()) {
             try {
                 CDataStream ss(it->second, SER_DISK, CLIENT_VERSION);
@@ -4391,6 +4401,11 @@ void DumpMempool(void)
             }
 
             mapData["txs"] = SerializeToVector(txMapDatas);
+        }
+        {
+            CDataStream ss(SER_DISK, CLIENT_VERSION);
+            mempool.DumpMinFeeInternal(ss);
+            mapData["minfee"] = std::vector<unsigned char>(ss.begin(), ss.end());
         }
 
         FILE* filestr = fsbridge::fopen(GetDataDir() / "mempool.dat.new", "wb");