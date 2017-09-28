f1b269308681c71e6f09a6641d955f95c5df86fc
Russell Yanofsky <russ@yanofsky.org>
2017-09-28 14:13:29 -0400

    Remove direct node->wallet calls in init.cpp
    Route calls during node initialization and shutdown that would happen between a
    node process and wallet processes through the serializable `Chain::Client`
    interface, rather than `WalletInitInterface` which is now simpler and only
    deals with early initialization and parameter interaction.
    
    This commit mostly does not change behavior. The only change is that the
    "Wallet disabled!" and "No wallet support compiled in!" messages are now logged
    earlier during startup.

--- src/init.cpp
+++ src/init.cpp
@@ -78,13 +78,8 @@ public:
 
     void AddWalletOptions() const override {}
     bool ParameterInteraction() const override {return true;}
-    void RegisterRPC(CRPCTable &) const override {}
     bool Verify() const override {return true;}
-    bool Open() const override {LogPrintf("No wallet support compiled in!\n"); return true;}
-    void Start(CScheduler& scheduler) const override {}
-    void Flush() const override {}
-    void Stop() const override {}
-    void Close() const override {}
+    void Construct(InitInterfaces& interfaces) const override {LogPrintf("No wallet support compiled in!\n");}
 };
 
 const WalletInitInterface& g_wallet_init_interface = DummyWalletInit();
@@ -206,7 +201,9 @@ void Shutdown(InitInterfaces& interfaces)
     StopREST();
     StopRPC();
     StopHTTPServer();
-    g_wallet_init_interface.Flush();
+    for (const auto& client : interfaces.chain_clients) {
+        client->stop();
+    }
     StopMapPort();
 
     // Because these depend on each-other, we make sure that neither can be
@@ -267,7 +264,9 @@ void Shutdown(InitInterfaces& interfaces)
         pcoinsdbview.reset();
         pblocktree.reset();
     }
-    g_wallet_init_interface.Stop();
+    for (const auto& client : interfaces.chain_clients) {
+        client->shutdown();
+    }
 
 #if ENABLE_ZMQ
     if (pzmqNotificationInterface) {
@@ -287,7 +286,7 @@ void Shutdown(InitInterfaces& interfaces)
     UnregisterAllValidationInterfaces();
     GetMainSignals().UnregisterBackgroundSignalScheduler();
     GetMainSignals().UnregisterWithMempoolSignals(mempool);
-    g_wallet_init_interface.Close();
+    interfaces.chain_clients.clear();
     globalVerifyHandle.reset();
     ECC_Stop();
     LogPrintf("%s: done\n", __func__);
@@ -1249,11 +1248,19 @@ bool AppInitMain(InitInterfaces& interfaces)
     GetMainSignals().RegisterBackgroundSignalScheduler(scheduler);
     GetMainSignals().RegisterWithMempoolSignals(mempool);
 
+    // Create client interfaces for wallets that are supposed to be loaded
+    // according to -wallet and -disablewallet options. Note this only creates
+    // the interface pointers. Wallets get actually loaded by the client
+    // registerRpcs(), prepare(), and start() calls below.
+    g_wallet_init_interface.Construct(interfaces);
+
     /* Register RPC commands regardless of -server setting so they will be
      * available in the GUI RPC console even if external calls are disabled.
      */
     RegisterAllCoreRPCCommands(tableRPC);
-    g_wallet_init_interface.RegisterRPC(tableRPC);
+    for (const auto& client : interfaces.chain_clients) {
+        client->registerRpcs();
+    }
 
     /* Start the RPC server already.  It will be started in "warmup" mode
      * and not really process calls already (but it will signify connections
@@ -1593,7 +1600,11 @@ bool AppInitMain(InitInterfaces& interfaces)
     }
 
     // ********************************************************* Step 9: load wallet
-    if (!g_wallet_init_interface.Open(interfaces)) return false;
+    for (const auto& client : interfaces.chain_clients) {
+        if (!client->prepare()) {
+            return false;
+        }
+    }
 
     // ********************************************************* Step 10: data directory maintenance
 
@@ -1739,7 +1750,9 @@ bool AppInitMain(InitInterfaces& interfaces)
     SetRPCWarmupFinished();
     uiInterface.InitMessage(_("Done loading"));
 
-    g_wallet_init_interface.Start(scheduler);
+    for (const auto& client : interfaces.chain_clients) {
+        client->start(scheduler);
+    }
 
     return true;
 }
