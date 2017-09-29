49cfd6c5a9815e1bfc0036892843560417b6c629
Karl-Johan Alm <karljohan-alm@garage.co.jp>
2017-09-29 11:20:27 +0900

    [test] [wallet] Test explicit fee mode for sendtoaddress

--- test/functional/wallet_basic.py
+++ test/functional/wallet_basic.py
@@ -334,6 +334,25 @@ class WalletTest(BitcoinTestFramework):
         # This will raise an exception since generate does not accept a string
         assert_raises_rpc_error(-1, "not an integer", self.nodes[0].generate, "2")
 
+        # send with explicit fee
+        self.sync_all([self.nodes[0:3]])
+        self.log.info("test explicit fee (sendtoaddress)")
+        self.nodes[0].generate(1)
+        prebalance = self.nodes[2].getbalance()
+        assert prebalance > 2
+        txid = self.nodes[2].sendtoaddress(
+            address=self.nodes[1].getnewaddress(),
+            amount=1.0,
+            conf_target=0.00002500,
+            estimate_mode='EXPLICIT',
+        )
+        tx_size = count_bytes(self.nodes[2].getrawtransaction(txid))
+        self.sync_all([self.nodes[0:3]])
+        self.nodes[0].generate(1)
+        postbalance = self.nodes[2].getbalance()
+        fee = prebalance - postbalance - Decimal('1')
+        assert_fee_amount(fee, tx_size, Decimal('0.00002500'))
+
         # Import address and private key to check correct behavior of spendable unspents
         # 1. Send some coins to generate new UTXO
         address_to_import = self.nodes[2].getnewaddress()
