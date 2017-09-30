30a9260170eb1d504a561b4f35425b39527ec44a
Thomas Kerin <thomas.kerin@bitmaintech.com>
2017-09-30 14:02:18 +0200

    Ensure whilst sorting only compressed public keys are used

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -1052,6 +1052,9 @@ static RPCHelpMan addmultisigaddress()
         } else {
             pubkeys.push_back(AddrToPubKey(spk_man, keys_or_addrs[i].get_str()));
         }
+        if (sort && !pubkeys.back().IsCompressed()) {
+            throw std::runtime_error(strprintf("Compressed key required for BIP67: %s", keys_or_addrs[i].get_str()));
+        }
     }
 
     if (!request.params[3].isNull()) {
