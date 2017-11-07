55fe98350261e38c5c55ea938f128f1e1f0dc0eb
Russell Yanofsky <russ@yanofsky.org>
2017-11-06 20:11:43 -0500

    Add BitcoinApplication & RPCConsole tests
    Add test coverage for Qt initialization code & basic RPC console functionality.

--- src/qt/test/test_main.cpp
+++ src/qt/test/test_main.cpp
@@ -7,6 +7,8 @@
 #endif
 
 #include <chainparams.h>
+#include <qt/bitcoin.h>
+#include <qt/test/apptests.h>
 #include <qt/test/rpcnestedtests.h>
 #include <util.h>
 #include <qt/test/uritests.h>
@@ -51,7 +53,7 @@ int main(int argc, char *argv[])
 {
     SetupEnvironment();
     SetupNetworking();
-    SelectParams(CBaseChainParams::MAIN);
+    SelectParams(CBaseChainParams::REGTEST);
     noui_connect();
     ClearDatadirCache();
     fs::path pathTemp = fs::temp_directory_path() / strprintf("test_bitcoin-qt_%lu_%i", (unsigned long)GetTime(), (int)GetRand(100000));
@@ -71,11 +73,15 @@ int main(int argc, char *argv[])
 
     // Don't remove this, it's needed to access
     // QApplication:: and QCoreApplication:: in the tests
-    QApplication app(argc, argv);
+    BitcoinApplication app(argc, argv);
     app.setApplicationName("Bitcoin-Qt-test");
 
     SSL_library_init();
 
+    AppTests app_tests(app);
+    if (QTest::qExec(&app_tests) != 0) {
+        fInvalid = true;
+    }
     URITests test1;
     if (QTest::qExec(&test1) != 0) {
         fInvalid = true;
