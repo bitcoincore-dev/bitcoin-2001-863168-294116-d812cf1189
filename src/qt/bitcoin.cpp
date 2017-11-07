278e6f513b26050c234e1af5673ecc800f7d16fd
Russell Yanofsky <russ@yanofsky.org>
2017-11-06 20:11:43 -0500

    Add BitcoinApplication & RPCConsole tests
    Add test coverage for Qt initialization code & basic RPC console functionality.

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -370,7 +370,7 @@ void BitcoinApplication::initializeResult(bool success)
         qWarning() << "Platform customization:" << platformStyle->getName();
 #ifdef ENABLE_WALLET
         PaymentServer::LoadRootCAs();
-        paymentServer->setOptionsModel(optionsModel);
+        if (paymentServer) paymentServer->setOptionsModel(optionsModel);
 #endif
 
         clientModel = new ClientModel(m_node, optionsModel);
@@ -405,6 +405,7 @@ void BitcoinApplication::initializeResult(bool success)
             window->show();
         }
         Q_EMIT splashFinished(window);
+        Q_EMIT windowShown(window);
 
 #ifdef ENABLE_WALLET
         // Now that initialization/startup is done, process any command-line
@@ -444,7 +445,7 @@ WId BitcoinApplication::getMainWinId() const
 }
 
 #ifndef BITCOIN_QT_TEST
-int main(int argc, char *argv[])
+int GuiMain(int argc, char* argv[])
 {
     SetupEnvironment();
 
