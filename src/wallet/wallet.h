f2d617d3fae981ded090648d101c7274c56859fc
Russell Yanofsky <russ@yanofsky.org>
2017-11-20 12:49:26 -0500

    Get rid of unneeded CWalletTx::Init parameter

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -382,14 +382,13 @@ public:
     mutable bool fInMempool;
     mutable CAmount nChangeCached;
 
-    CWalletTx(const CWallet* pwalletIn, CTransactionRef arg) : CMerkleTx(std::move(arg))
+    CWalletTx(const CWallet* wallet, CTransactionRef arg) : CMerkleTx(std::move(arg)), pwallet(wallet)
     {
-        Init(pwalletIn);
+        Init();
     }
 
-    void Init(const CWallet* pwalletIn)
+    void Init()
     {
-        pwallet = pwalletIn;
         mapValue.clear();
         vOrderForm.clear();
         fTimeReceivedIsTxTime = false;
@@ -422,7 +421,7 @@ public:
     template<typename Stream>
     void Unserialize(Stream& s)
     {
-        Init(nullptr);
+        Init();
         char fSpent;
 
         s >> static_cast<CMerkleTx&>(*this);
