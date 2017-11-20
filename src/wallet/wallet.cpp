5078457a2f822980f85c061097970a7b3a2c3533
Russell Yanofsky <russ@yanofsky.org>
2017-11-20 12:51:45 -0500

    Disable CWalletTx copy constructor
    Disable copying of CWalletTx objects to prevent bugs where instances get copied
    in and out of the mapWallet map and fields are updated in the wrong copy.

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3195,7 +3195,7 @@ DBErrors CWallet::ZapSelectTx(std::vector<uint256>& vHashIn, std::vector<uint256
 
 }
 
-DBErrors CWallet::ZapWalletTx(std::vector<CWalletTx>& vWtx)
+DBErrors CWallet::ZapWalletTx(std::list<CWalletTx>& vWtx)
 {
     DBErrors nZapWalletTxRet = CWalletDB(*dbw,"cr+").ZapWalletTx(vWtx);
     if (nZapWalletTxRet == DB_NEED_REWRITE)
@@ -3920,7 +3920,7 @@ CWallet* CWallet::CreateWalletFromFile(const std::string& name, const fs::path& 
     const std::string& walletFile = name;
 
     // needed to restore wallet transaction meta data after -zapwallettxes
-    std::vector<CWalletTx> vWtx;
+    std::list<CWalletTx> vWtx;
 
     if (gArgs.GetBoolArg("-zapwallettxes", false)) {
         uiInterface.InitMessage(_("Zapping all transactions from wallet..."));
