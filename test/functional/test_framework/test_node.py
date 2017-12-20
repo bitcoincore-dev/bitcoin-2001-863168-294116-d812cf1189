47feceea1a6e59a391a5fffd95492bc812519829
Russell Yanofsky <russ@yanofsky.org>
2017-12-20 18:38:40 -0500

    Improve TestNodeCLI output parsing
    Parse JSONRPCException errors, and avoid JSON decode exception if RPC method
    returns a plain string.

--- test/functional/test_framework/test_node.py
+++ test/functional/test_framework/test_node.py
@@ -10,6 +10,7 @@ import http.client
 import json
 import logging
 import os
+import re
 import subprocess
 import time
 
@@ -222,6 +223,13 @@ class TestNodeCLI():
         cli_stdout, cli_stderr = process.communicate(input=self.input)
         returncode = process.poll()
         if returncode:
+            match = re.match(r'error code: ([-0-9]+)\nerror message:\n(.*)', cli_stderr)
+            if match:
+                code, message = match.groups()
+                raise JSONRPCException(dict(code=int(code), message=message))
             # Ignore cli_stdout, raise with cli_stderr
             raise subprocess.CalledProcessError(returncode, self.binary, output=cli_stderr)
-        return json.loads(cli_stdout, parse_float=decimal.Decimal)
+        try:
+            return json.loads(cli_stdout, parse_float=decimal.Decimal)
+        except json.JSONDecodeError:
+            return cli_stdout.rstrip("\n")
