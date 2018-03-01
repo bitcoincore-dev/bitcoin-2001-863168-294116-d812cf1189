ryanofsky/bitcoin

Add -estlog option for saving live fee estimation data

src/test/policyestimator_tests.cpp
@@ -3,6 +3,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <policy/fees.h>
+#include <policy/fees_input.h>
 #include <policy/policy.h>
 #include <txmempool.h>
 #include <uint256.h>
@@ -17,7 +18,8 @@ BOOST_FIXTURE_TEST_SUITE(policyestimator_tests, BasicTestingSetup)
 BOOST_AUTO_TEST_CASE(BlockPolicyEstimates)
 {
     CBlockPolicyEstimator feeEst;
-    CTxMemPool mpool(&feeEst);
+    FeeEstInput feeEstInput(feeEst);
+    CTxMemPool mpool(&feeEstInput);
     LOCK2(cs_main, mpool.cs);
     TestMemPoolEntryHelper entry;
     CAmount basefee(2000);

bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

src/test/policyestimator_tests.cpp
@@ -4,6 +4,7 @@
 
 #include <policy/policy.h>
 #include <policy/fees.h>
+#include <policy/fees_input.h>
 #include <txmempool.h>
 #include <uint256.h>
 #include <util.h>
@@ -17,7 +18,8 @@ BOOST_FIXTURE_TEST_SUITE(policyestimator_tests, BasicTestingSetup)
 BOOST_AUTO_TEST_CASE(BlockPolicyEstimates)
 {
     CBlockPolicyEstimator feeEst;
-    CTxMemPool mpool(&feeEst);
+    CBlockPolicyInput feeEstInput(feeEst);
+    CTxMemPool mpool(&feeEstInput);
     TestMemPoolEntryHelper entry;
     CAmount basefee(2000);
     CAmount deltaFee(100);