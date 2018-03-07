bitcoin/bitcoin

[RPC] Add utility getsignaturehash

test/functional/test_runner.py
@@ -126,6 +126,7 @@ BASE_SCRIPTS= [
     'p2p-fingerprint.py',
     'uacomment.py',
     'p2p-acceptblock.py',
+    'getsignaturehash.py',
 ]
 
 EXTENDED_SCRIPTS = [

bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

test/functional/test_runner.py
@@ -130,6 +130,7 @@ BASE_SCRIPTS= [
     'p2p_fingerprint.py',
     'feature_uacomment.py',
     'p2p_unrequested_blocks.py',
+    'rpc_getsignaturehash.py',
     'feature_logging.py',
     'p2p_node_network_limited.py',
     'feature_config_args.py',