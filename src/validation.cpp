9c22f8a53d359abb8832b0bbd298ab6bd215d7ad
Luke Dashjr <luke-jr+git@utopios.org>
2018-03-11 03:05:30 +0000

    Enable prioritisetransaction with priority delta

--- src/validation.cpp
+++ src/validation.cpp
@@ -677,7 +677,8 @@ bool MemPoolAccept::PreChecks(ATMPArgs& args, Workspace& ws)
 
     // nModifiedFees includes any fee deltas from PrioritiseTransaction
     nModifiedFees = nFees;
-    m_pool.ApplyDelta(hash, nModifiedFees);
+    double nPriorityDummy = 0;
+    m_pool.ApplyDeltas(hash, nPriorityDummy, nModifiedFees);
 
     CAmount inChainInputValue;
     // Since entries arrive *after* the tip's height, their priority is for the height+1
@@ -5098,7 +5099,7 @@ bool DumpMempool(const CTxMemPool& pool)
     {
         LOCK(pool.cs);
         for (const auto &i : pool.mapDeltas) {
-            mapDeltas[i.first] = std::make_pair(0.0, i.second);
+            mapDeltas[i.first] = std::make_pair(0.0, i.second.second);
         }
         vinfo = pool.infoAll();
     }
