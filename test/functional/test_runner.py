ps1dr3x/bitcoin

Test the PSBT RPCs

test/functional/test_runner.py
@@ -123,6 +123,7 @@ BASE_SCRIPTS= [
     'uptime.py',
     'resendwallettransactions.py',
     'minchainwork.py',
+    "psbt.py",
 ]
 
 EXTENDED_SCRIPTS = [

bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

test/functional/test_runner.py
@@ -133,6 +133,7 @@ BASE_SCRIPTS= [
     'feature_logging.py',
     'p2p_node_network_limited.py',
     'feature_config_args.py',
+    "rpc_psbt.py",
     # Don't append tests at the end to avoid merge conflicts
     # Put them in a random line within the section that fits their approximate run-time
 ]