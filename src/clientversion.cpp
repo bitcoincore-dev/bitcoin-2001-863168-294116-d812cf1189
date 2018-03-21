4600612295ed76cafee035d36e70a580021d87fd
Luke Dashjr <luke-jr+git@utopios.org>
2018-03-21 17:41:05 +0000

    Bump version to Knots:20180321

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -101,7 +101,7 @@ std::string FormatSubVersion(const std::string& name, int nClientVersion, const 
     }
     ss << "/";
     if (!fBaseNameOnly)
-        ss << "Knots:20170901/";
+        ss << "Knots:20180321/";
     return ss.str();
 }
 
