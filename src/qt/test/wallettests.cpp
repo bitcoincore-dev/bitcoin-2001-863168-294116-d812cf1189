eb6c2beec75e3e41ac99bb731b175db4693d80d1
James O'Beirne <james.obeirne@gmail.com>
2018-03-29 10:56:04 -0400

    [tests] [qt] Introduce qt/test/util with a generalized ConfirmMessage
    ConfirmMessage is reused in future tests apart from its single usage here.

--- src/qt/test/wallettests.cpp
+++ src/qt/test/wallettests.cpp
@@ -1,4 +1,5 @@
 #include <qt/test/wallettests.h>
+#include <qt/test/util.h>
 
 #include <qt/bitcoinamountfield.h>
 #include <qt/callback.h>
@@ -32,21 +33,6 @@
 
 namespace
 {
-//! Press "Ok" button in message box dialog.
-void ConfirmMessage(QString* text = nullptr)
-{
-    QTimer::singleShot(0, makeCallback([text](Callback* callback) {
-        for (QWidget* widget : QApplication::topLevelWidgets()) {
-            if (widget->inherits("QMessageBox")) {
-                QMessageBox* messageBox = qobject_cast<QMessageBox*>(widget);
-                if (text) *text = messageBox->text();
-                messageBox->defaultButton()->click();
-            }
-        }
-        delete callback;
-    }), SLOT(call()));
-}
-
 //! Press "Yes" or "Cancel" buttons in modal send confirmation dialog.
 void ConfirmSend(QString* text = nullptr, bool cancel = false)
 {
@@ -258,7 +244,7 @@ void TestGUI()
     QCOMPARE(requestTableModel->rowCount({}), currentRowCount-1);
 }
 
-}
+} // namespace
 
 void WalletTests::walletTests()
 {
