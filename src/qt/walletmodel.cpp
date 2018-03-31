21af27fce86a7332f61eaa848f1322fd895186e3
Russell Yanofsky <russ@yanofsky.org>
2018-03-31 06:36:36 -0400

    fixup! Remove most direct bitcoin calls from qt/walletmodel.cpp
    Fix comment as suggested https://github.com/bitcoin/bitcoin/pull/10244#discussion_r177492485

--- src/qt/walletmodel.cpp
+++ src/qt/walletmodel.cpp
@@ -77,9 +77,10 @@ void WalletModel::updateStatus()
 
 void WalletModel::pollBalanceChanged()
 {
-    // Get required locks upfront. This avoids the GUI from getting stuck on
-    // periodical polls if the core is holding the locks for a longer time -
-    // for example, during a wallet rescan.
+    // Try to get balances and return early if locks can't be acquired. This
+    // avoids the GUI from getting stuck on periodical polls if the core is
+    // holding the locks for a longer time - for example, during a wallet
+    // rescan.
     interface::WalletBalances new_balances;
     int numBlocks = -1;
     if (!m_wallet->tryGetBalances(new_balances, numBlocks)) {
