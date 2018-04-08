76401da3a0cdcf07f6b1e6128647afe941e55e48
Russell Yanofsky <russ@yanofsky.org>
2018-04-08 14:37:50 -0400

    Remove use of uiInterface.LoadWallet in wallet code
    This commit does not change behavior.

--- src/interfaces/chain.h
+++ src/interfaces/chain.h
@@ -26,6 +26,8 @@ struct FeeCalculation;
 
 namespace interfaces {
 
+class Wallet;
+
 //! Interface for giving wallet processes access to blockchain state.
 class Chain
 {
@@ -185,6 +187,9 @@ public:
 
     //! Send init error.
     virtual void initError(const std::string& message) = 0;
+
+    //! Send wallet load notification.
+    virtual void loadWallet(std::unique_ptr<Wallet> wallet) = 0;
 
     //! Generate blocks
     virtual UniValue generateBlocks(std::shared_ptr<CReserveScript> coinbase_script,
