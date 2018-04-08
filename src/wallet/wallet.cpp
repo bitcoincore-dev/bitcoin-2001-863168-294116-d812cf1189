9475270518176ff93859049440021144910facfe
Russell Yanofsky <russ@yanofsky.org>
2018-04-08 14:37:50 -0400

    Remove use of uiInterface.LoadWallet in wallet code
    This commit does not change behavior.

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -12,6 +12,7 @@
 #include <consensus/validation.h>
 #include <fs.h>
 #include <interfaces/chain.h>
+#include <interfaces/wallet.h>
 #include <key.h>
 #include <key_io.h>
 #include <keystore.h>
@@ -4242,7 +4243,7 @@ std::shared_ptr<CWallet> CWallet::CreateWalletFromFile(interfaces::Chain& chain,
         }
     }
 
-    uiInterface.LoadWallet(walletInstance);
+    chain.loadWallet(interfaces::MakeWallet(walletInstance));
 
     // Register with the validation interface. It's ok to do this after rescan since we're still holding cs_main.
     RegisterValidationInterface(walletInstance.get());
