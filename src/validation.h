be4f9c576f0a5d9d1e222a2b5e1add45cda7f895
Jonas Schnelli <dev@jonasschnelli.ch>
2018-04-12 09:50:25 +0200

    Add RPC call setscriptthreadsenabled/scriptthreadsinfo: allow to disable verification threads

--- src/validation.h
+++ src/validation.h
@@ -150,6 +150,7 @@ extern uint256 g_best_block;
 extern std::atomic_bool fImporting;
 extern std::atomic_bool fReindex;
 extern int nScriptCheckThreads;
+extern std::atomic_bool g_script_threads_enabled;
 extern bool fRequireStandard;
 extern bool fCheckBlockIndex;
 extern bool fCheckpointsEnabled;
