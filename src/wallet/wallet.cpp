b37f9d490f6fc9e810dd53ab17416eab36e05034
João Barbosa <joao.paulo.barbosa@gmail.com>
2018-05-29 14:37:53 +0100

    wallet: Replace %w by wallet name in -walletnotify script

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -1005,6 +1005,7 @@ bool CWallet::AddToWallet(const CWalletTx& wtxIn, bool fFlushOnClose)
     if (!strCmd.empty())
     {
         boost::replace_all(strCmd, "%s", wtxIn.GetHash().GetHex());
+        boost::replace_all(strCmd, "%w", GetName());
         std::thread t(runCommand, strCmd);
         t.detach(); // thread runs free
     }
