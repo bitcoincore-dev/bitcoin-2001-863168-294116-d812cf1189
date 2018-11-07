c42e57643bd5a2edd16dc1078f2617ee52c500d1
Luke Dashjr <luke-jr+git@utopios.org>
2018-11-07 07:38:01 +0000

    util: SelectBaseParams in ReadConfigFiles, before getting final datadir

--- src/bitcoin-cli.cpp
+++ src/bitcoin-cli.cpp
@@ -133,13 +133,6 @@ static int AppInitRPC(int argc, char* argv[])
         tfm::format(std::cerr, "Error reading configuration file: %s\n", error.c_str());
         return EXIT_FAILURE;
     }
-    // Check for -chain, -testnet or -regtest parameter (BaseParams() calls are only valid after this clause)
-    try {
-        SelectBaseParams(gArgs.GetChainName());
-    } catch (const std::exception& e) {
-        tfm::format(std::cerr, "Error: %s\n", e.what());
-        return EXIT_FAILURE;
-    }
     return CONTINUE_EXECUTION;
 }
 
