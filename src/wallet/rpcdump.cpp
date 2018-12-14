kungluli/bitcoin

wallet: Key origin refactoring

src/wallet/rpcdump.cpp
@@ -136,8 +136,9 @@ UniValue importprivkey(const UniValue& params, bool fHelp)
         if (pwalletMain->HaveKey(vchAddress))
             return NullUniValue;
 
-        pwalletMain->mapKeyMetadata[vchAddress].nCreateTime = 1;
-        pwalletMain->mapKeyMetadata[vchAddress].keyFlags |= CKeyMetadata::KEY_ORIGIN_IMPORTED;
+        CKeyMetadata& metadata = pwalletMain->mapKeyMetadata[vchAddress];
+        metadata.nCreateTime = 1;
+        metadata.SetKeyOrigin(metadata.GetKeyOrigin() | CKeyMetadata::KEY_ORIGIN_IMPORTED);
 
         if (!pwalletMain->AddKeyPubKey(key, pubkey))
             throw JSONRPCError(RPC_WALLET_ERROR, "Error adding key to wallet");

src/wallet/rpcwallet.cpp
@@ -1179,7 +1179,7 @@ UniValue ListReceived(const UniValue& params, bool fByAccounts)
         CKeyID keyID;
         uint8_t keyFlags = 0;
         if (address.GetKeyID(keyID))
-            keyFlags = pwalletMain->mapKeyMetadata[keyID].keyFlags;
+            keyFlags = pwalletMain->mapKeyMetadata[keyID].GetKeyOrigin();
 
         std::string keyOrigin;
         if (keyFlags & CKeyMetadata::KEY_ORIGIN_UNKNOWN)

bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

src/wallet/rpcdump.cpp
@@ -174,8 +174,9 @@ UniValue importprivkey(const JSONRPCRequest& request)
 
             // whenever a key is imported, we need to scan the whole chain
             pwallet->UpdateTimeFirstKey(1);
-            pwallet->mapKeyMetadata[vchAddress].nCreateTime = 1;
-            pwallet->mapKeyMetadata[vchAddress].keyFlags |= CKeyMetadata::KEY_ORIGIN_IMPORTED;
+            CKeyMetadata& metadata = pwallet->mapKeyMetadata[vchAddress];
+            metadata.nCreateTime = 1;
+            metadata.SetKeyOrigin(metadata.GetKeyOrigin() | CKeyMetadata::KEY_ORIGIN_IMPORTED);
 
             if (!pwallet->AddKeyPubKey(key, pubkey)) {
                 throw JSONRPCError(RPC_WALLET_ERROR, "Error adding key to wallet");

src/wallet/rpcwallet.cpp
@@ -1626,7 +1626,7 @@ static UniValue ListReceived(CWallet * const pwallet, const UniValue& params, bo
         CKeyID keyID = GetKeyForDestination(*pwallet, address);
         uint8_t keyFlags = 0;
         if (!keyID.IsNull()) {
-            keyFlags = pwallet->mapKeyMetadata[keyID].keyFlags;
+            keyFlags = pwallet->mapKeyMetadata[keyID].GetKeyOrigin();
         }
 
         std::string keyOrigin;