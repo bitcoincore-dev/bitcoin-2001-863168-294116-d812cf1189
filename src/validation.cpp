daf3a07bfbd3933378c3c9e0b076d9b3c38877a1
Anthony Towns <aj@erisian.com.au>
2019-01-16 13:08:39 +1000

    Remove redundant state.Invalid() call after CheckTransaction()
    CheckTransaction() already sets the CValidationState if an error is found, so
    just log a message rather than overwrite that state.

--- src/validation.cpp
+++ src/validation.cpp
@@ -3109,10 +3109,12 @@ bool CheckBlock(const CBlock& block, CValidationState& state, const Consensus::P
             return state.DoS(100, false, REJECT_INVALID, "bad-cb-multiple", false, "more than one coinbase");
 
     // Check transactions
-    for (const auto& tx : block.vtx)
-        if (!CheckTransaction(*tx, state, true))
-            return state.Invalid(false, state.GetRejectCode(), state.GetRejectReason(),
-                                 strprintf("Transaction check failed (tx hash %s) %s", tx->GetHash().ToString(), state.GetDebugMessage()));
+    for (const auto& tx : block.vtx) {
+        if (!CheckTransaction(*tx, state, true)) {
+            LogPrintf("Transaction check failed (tx hash %s) %s\n", tx->GetHash().ToString(), state.GetDebugMessage());
+            return false;
+        }
+    }
 
     unsigned int nSigOps = 0;
     for (const auto& tx : block.vtx)
