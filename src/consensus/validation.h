daff2befb3620a4e0ddfca64f86c3f8ee88db33b
Anthony Towns <aj@erisian.com.au>
2019-01-18 12:58:41 +1000

    [refactor] Drop redundant nDoS, corruptionPossible, SetCorruptionPossible

--- src/consensus/validation.h
+++ src/consensus/validation.h
@@ -75,25 +75,21 @@ private:
         MODE_ERROR,   //!< run-time error
     } mode;
     ValidationInvalidReason reason;
-    int nDoS;
     std::string strRejectReason;
     unsigned int chRejectCode;
-    bool corruptionPossible;
     std::string strDebugMessage;
 public:
-    CValidationState() : mode(MODE_VALID), reason(ValidationInvalidReason::NONE), nDoS(0), chRejectCode(0), corruptionPossible(false) {}
+    CValidationState() : mode(MODE_VALID), reason(ValidationInvalidReason::NONE), chRejectCode(0) {}
     bool DoS(int level, ValidationInvalidReason reasonIn, bool ret = false,
              unsigned int chRejectCodeIn=0, const std::string &strRejectReasonIn="",
-             bool corruptionIn=false,
+             bool corruptionPossibleIn=false,
              const std::string &strDebugMessageIn="") {
         reason = reasonIn;
         chRejectCode = chRejectCodeIn;
         strRejectReason = strRejectReasonIn;
-        corruptionPossible = corruptionIn;
         strDebugMessage = strDebugMessageIn;
-        nDoS = level;
-        assert(nDoS == GetDoSForReason());
-        assert(corruptionPossible == (reason == ValidationInvalidReason::BLOCK_MUTATED || reason == ValidationInvalidReason::TX_WITNESS_MUTATED));
+        assert(level == GetDoS());
+        assert(corruptionPossibleIn == CorruptionPossible());
         if (mode == MODE_ERROR)
             return ret;
         mode = MODE_INVALID;
@@ -120,15 +116,9 @@ public:
         return mode == MODE_ERROR;
     }
     bool CorruptionPossible() const {
-        assert(corruptionPossible == (reason == ValidationInvalidReason::BLOCK_MUTATED || reason == ValidationInvalidReason::TX_WITNESS_MUTATED));
-        return corruptionPossible;
+        return reason == ValidationInvalidReason::BLOCK_MUTATED || reason == ValidationInvalidReason::TX_WITNESS_MUTATED;
     }
-    void SetCorruptionPossible() {
-        corruptionPossible = true;
-        assert(corruptionPossible == (reason == ValidationInvalidReason::BLOCK_MUTATED || reason == ValidationInvalidReason::TX_WITNESS_MUTATED));
-    }
-    int GetDoS(void) const { return nDoS; }
-    int GetDoSForReason() const {
+    int GetDoS() const {
         switch (reason) {
         case ValidationInvalidReason::NONE:
             return 0;
