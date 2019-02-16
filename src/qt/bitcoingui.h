821859240318fb4eb2917f7f4359070242f6b67b
Luke Dashjr <luke-jr+git@utopios.org>
2019-02-16 18:46:55 +0000

    GUI: Add a new tab for pairing

--- src/qt/bitcoingui.h
+++ src/qt/bitcoingui.h
@@ -133,6 +133,7 @@ private:
     QMenuBar* appMenuBar = nullptr;
     QToolBar* appToolBar = nullptr;
     QAction* overviewAction = nullptr;
+    QAction* m_action_pairing = nullptr;
     QAction* historyAction = nullptr;
     QAction* quitAction = nullptr;
     QAction* sendCoinsAction = nullptr;
@@ -273,6 +274,8 @@ public Q_SLOTS:
 #ifdef ENABLE_WALLET
     /** Switch to overview (home) page */
     void gotoOverviewPage();
+    /** Switch to pairing page */
+    void gotoPairingPage();
     /** Switch to history (transactions) page */
     void gotoHistoryPage();
     /** Switch to receive coins page */
