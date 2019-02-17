14b01f0468f26d504255b65d9d35c329bdd00adf
Luke Dashjr <luke-jr+git@utopios.org>
2019-02-17 00:19:22 +0000

    GUI: Add getTorInfo to ClientModel

--- src/interfaces/node.cpp
+++ src/interfaces/node.cpp
@@ -87,6 +87,15 @@ public:
         }
     }
     bool getProxy(Network net, proxyType& proxy_info) override { return GetProxy(net, proxy_info); }
+    std::vector<CNetAddr> getNetLocalAddresses() override
+    {
+        std::vector<CNetAddr> ret;
+        LOCK(cs_mapLocalHost);
+        for (const std::pair<const CNetAddr, LocalServiceInfo> &item : mapLocalHost) {
+            ret.emplace_back(item.first);
+        }
+        return ret;
+    }
     size_t getNodeCount(CConnman::NumConnections flags) override
     {
         return m_context->connman ? m_context->connman->GetNodeCount(flags) : 0;
