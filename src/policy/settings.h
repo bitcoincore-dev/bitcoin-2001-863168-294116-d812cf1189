1352a8e9fd8509a431641bced2d4c309e27301ae
Russell Yanofsky <russ@yanofsky.org>
2019-03-22 00:42:17 -0400

    Remove access to node globals from wallet-linked code
    Remove last few instances of accesses to node global variables from wallet
    code. Also remove accesses to node globals from code in policy/policy.cpp that
    isn't actually called by wallet code, but does get linked into wallet code.
    
    This is the last change needed to allow bitcoin-wallet tool to be linked
    without depending on libbitcoin_server.a, to ensure wallet code doesn't access
    node global state and avoid bugs like
    https://github.com/bitcoin/bitcoin/pull/15557#discussion_r267735431

--- src/policy/settings.h
+++ src/policy/settings.h
@@ -6,11 +6,31 @@
 #ifndef BITCOIN_POLICY_SETTINGS_H
 #define BITCOIN_POLICY_SETTINGS_H
 
+#include <policy/policy.h>
+
 class CFeeRate;
+class CTransaction;
 
 // Policy settings which are configurable at runtime.
+
+extern bool fIsBareMultisigStd;
 extern CFeeRate incrementalRelayFee;
 extern CFeeRate dustRelayFee;
 extern unsigned int nBytesPerSigOp;
+
+static inline bool IsStandardTx(const CTransaction& tx, std::string& reason)
+{
+    return IsStandardTx(tx, ::fIsBareMultisigStd, ::dustRelayFee, reason);
+}
+
+static inline int64_t GetVirtualTransactionSize(int64_t weight, int64_t sigop_cost)
+{
+    return GetVirtualTransactionSize(weight, sigop_cost, ::nBytesPerSigOp);
+}
+
+static inline int64_t GetVirtualTransactionSize(const CTransaction& tx, int64_t sigop_cost)
+{
+    return GetVirtualTransactionSize(tx, sigop_cost, ::nBytesPerSigOp);
+}
 
 #endif // BITCOIN_POLICY_SETTINGS_H
