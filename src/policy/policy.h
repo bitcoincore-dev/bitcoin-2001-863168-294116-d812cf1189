2b6991132cd0e73b9b2dbe25dda9cdab328fac0e
Russell Yanofsky <russ@yanofsky.org>
2019-03-22 00:42:17 -0400

    Remove access to node globals from wallet-linked code
    Remove last few instances of accesses to node global variables from wallet
    code. Also remove accesses to node globals from code in policy/policy.cpp that
    isn't actually called by wallet code, but does get linked into wallet code.
    
    This is the last change needed to allow bitcoin-wallet tool to be linked
    without depending on libbitcoin_server.a, to ensure wallet code doesn't access
    node global state and avoid bugs like
    https://github.com/bitcoin/bitcoin/pull/15557#discussion_r267735431

--- src/policy/policy.h
+++ src/policy/policy.h
@@ -84,7 +84,7 @@ bool IsStandard(const CScript& scriptPubKey, txnouttype& whichType);
      * Check for standard transaction types
      * @return True if all outputs (scriptPubKeys) use only standard transaction forms
      */
-bool IsStandardTx(const CTransaction& tx, std::string& reason);
+bool IsStandardTx(const CTransaction& tx, bool permit_bare_multisig, const CFeeRate& dust_relay_fee, std::string& reason);
     /**
      * Check for standard transaction types
      * @param[in] mapInputs    Map of previous transactions that have outputs we're spending
@@ -99,8 +99,8 @@ bool AreInputsStandard(const CTransaction& tx, const CCoinsViewCache& mapInputs)
 bool IsWitnessStandard(const CTransaction& tx, const CCoinsViewCache& mapInputs);
 
 /** Compute the virtual transaction size (weight reinterpreted as bytes). */
-int64_t GetVirtualTransactionSize(int64_t nWeight, int64_t nSigOpCost);
-int64_t GetVirtualTransactionSize(const CTransaction& tx, int64_t nSigOpCost = 0);
-int64_t GetVirtualTransactionInputSize(const CTxIn& tx, int64_t nSigOpCost = 0);
+int64_t GetVirtualTransactionSize(int64_t nWeight, int64_t nSigOpCost, unsigned int bytes_per_sigop);
+int64_t GetVirtualTransactionSize(const CTransaction& tx, int64_t nSigOpCost = 0, unsigned int bytes_per_sigop = 0);
+int64_t GetVirtualTransactionInputSize(const CTxIn& tx, int64_t nSigOpCost = 0, unsigned int bytes_per_sigop = 0);
 
 #endif // BITCOIN_POLICY_POLICY_H
