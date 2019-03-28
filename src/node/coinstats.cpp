f83aa524e8e3a8f77a001939581beeb39de46783
James O'Beirne <james.obeirne@gmail.com>
2019-03-28 12:09:06 -0400

    coinstats: add coins_count
    Also changes existing CCoinsStats attributes to be C++11 initialized.

--- src/node/coinstats.cpp
+++ src/node/coinstats.cpp
@@ -51,6 +51,7 @@ bool GetUTXOStats(CCoinsView *view, CCoinsStats &stats)
     }
     ss << stats.hashBlock;
     uint256 prevkey;
+    unsigned int coins_count{0};
     std::map<uint32_t, Coin> outputs;
     while (pcursor->Valid()) {
         boost::this_thread::interruption_point();
@@ -63,6 +64,7 @@ bool GetUTXOStats(CCoinsView *view, CCoinsStats &stats)
             }
             prevkey = key.hash;
             outputs[key.n] = std::move(coin);
+            coins_count += 1;
         } else {
             return error("%s: unable to read value", __func__);
         }
@@ -73,5 +75,6 @@ bool GetUTXOStats(CCoinsView *view, CCoinsStats &stats)
     }
     stats.hashSerialized = ss.GetHash();
     stats.nDiskSize = view->EstimateSize();
+    stats.coins_count = coins_count;
     return true;
 }
