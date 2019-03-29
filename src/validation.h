ac18241a5a4b12228d0dc31c1d573163c82b7af4
James O'Beirne <james.obeirne@gmail.com>
2019-03-29 14:29:12 -0400

    trivial: add CChainState::ToString()

--- src/validation.h
+++ src/validation.h
@@ -631,6 +631,8 @@ public:
      */
     std::set<CBlockIndex*, CBlockIndexWorkComparator> setBlockIndexCandidates;
 
+    std::string ToString();
+
     CCoinsViewCache& CoinsTip()
     {
         assert(m_coins_views->m_cacheview);
