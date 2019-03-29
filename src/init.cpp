df4009c84435bd3676526e375839b6517285eaf1
James O'Beirne <james.obeirne@gmail.com>
2019-03-29 17:39:27 -0400

    validation: fix pruning + misc.

--- src/init.cpp
+++ src/init.cpp
@@ -1613,7 +1613,8 @@ bool AppInitMain(InitInterfaces& interfaces)
                         }
 
                         if (!CVerifyDB().VerifyDB(
-                                chainparams, &chainstate->CoinsTip(),
+                                *chainstate,
+                                chainparams, chainstate->CoinsTip(),
                                 gArgs.GetArg("-checklevel", DEFAULT_CHECKLEVEL),
                                 gArgs.GetArg("-checkblocks", DEFAULT_CHECKBLOCKS))) {
                             strLoadError = _("Corrupted block database detected");
