19c0432c0db214f8bc98f2116d5e541342704923
James O'Beirne <james.obeirne@gmail.com>
2019-03-29 16:33:02 -0400

    add utxo snapshot detection and add to init

--- src/init.cpp
+++ src/init.cpp
@@ -259,11 +259,12 @@ void Shutdown(NodeContext& node)
         LOCK(cs_main);
         for (CChainState* chainstate : node.chainman->GetAll()) {
             if (chainstate->CanFlushToDisk()) {
+                LogPrintf("[snapshot] resetting coinsviews for %s\n", chainstate->ToString());
                 chainstate->ForceFlushStateToDisk();
                 chainstate->ResetCoinsViews();
             }
         }
-        pblocktree.reset();
+        ::pblocktree.reset();
     }
     for (const auto& client : node.chain_clients) {
         client->stop();
@@ -1348,9 +1349,22 @@ bool AppInitMain(NodeContext& node, interfaces::BlockAndHeaderTipInfo* tip_info)
             const int64_t load_block_index_start_time = GetTimeMillis();
             try {
                 LOCK(cs_main);
-                chainman.InitializeChainstate(*Assert(node.mempool));
                 chainman.m_total_coinstip_cache = nCoinCacheUsage;
                 chainman.m_total_coinsdb_cache = nCoinDBCache;
+
+                // Load a chain created from a UTXO snapshot, if any exist.
+                chainman.DetectSnapshotChainstate(*Assert(node.mempool));
+
+                // Conservative value that will ultimately be changed by
+                // a call to `chainman.MaybeRebalanceCaches()`.
+                double init_cache_fraction = 0.2;
+
+                // If we're not using a snapshot or we haven't fully validated it yet,
+                // create a validation chainstate.
+                if (!chainman.IsSnapshotValidated()) {
+                    LogPrintf("Loading validation chainstate\n");
+                    chainman.InitializeChainstate(*Assert(node.mempool));
+                }
 
                 UnloadBlockIndex(node.mempool.get(), chainman);
 
@@ -1367,6 +1381,17 @@ bool AppInitMain(NodeContext& node, interfaces::BlockAndHeaderTipInfo* tip_info)
                 }
 
                 if (ShutdownRequested()) break;
+
+                for (CChainState* chainstate : chainman.GetAll()) {
+                    // Initialize CoinsDB before loading the block index in case we
+                    // have to consult a cached nChainTx value in the snapshot
+                    // chainstate storage.
+                    // (See nChainTx usage in BlockManager::LoadBlockIndex()).
+                    chainstate->InitCoinsDB(
+                        /* cache_size_bytes */ nCoinDBCache * init_cache_fraction,
+                        /* in_memory */ false,
+                        /* should_wipe */ fReset || fReindexChainState);
+                }
 
                 // LoadBlockIndex will load fHavePruned if we've ever removed a
                 // block file from disk.
@@ -1407,10 +1432,7 @@ bool AppInitMain(NodeContext& node, interfaces::BlockAndHeaderTipInfo* tip_info)
                 bool failed_chainstate_init = false;
 
                 for (CChainState* chainstate : chainman.GetAll()) {
-                    chainstate->InitCoinsDB(
-                        /* cache_size_bytes */ nCoinDBCache,
-                        /* in_memory */ false,
-                        /* should_wipe */ fReset || fReindexChainState);
+                    LogPrintf("Initializing chainstate %s\n", chainstate->ToString());
 
                     chainstate->CoinsErrorCatcher().AddReadErrCallback([]() {
                         uiInterface.ThreadSafeMessageBox(
@@ -1451,6 +1473,12 @@ bool AppInitMain(NodeContext& node, interfaces::BlockAndHeaderTipInfo* tip_info)
                 if (failed_chainstate_init) {
                     break; // out of the chainstate activation do-while
                 }
+
+                // Now that chainstates are loaded and we're able to flush to
+                // disk, rebalance the coins caches to desired levels based
+                // on the condition of each chainstate.
+                chainman.MaybeRebalanceCaches();
+
             } catch (const std::exception& e) {
                 LogPrintf("%s\n", e.what());
                 strLoadError = _("Error opening block database");
