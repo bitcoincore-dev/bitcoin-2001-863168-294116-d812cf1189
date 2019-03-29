b2e1609cc8ea79c30d91e1a945d701b2429702be
James O'Beirne <james.obeirne@gmail.com>
2019-03-29 17:39:27 -0400

    validation: fix pruning + misc.

--- src/init.cpp
+++ src/init.cpp
@@ -1610,6 +1610,7 @@ bool AppInitMain(InitInterfaces& interfaces)
                         }
 
                         if (!CVerifyDB().VerifyDB(
+                                chainstate,
                                 chainparams, chainstate->CoinsCache(),
                                 gArgs.GetArg("-checklevel", DEFAULT_CHECKLEVEL),
                                 gArgs.GetArg("-checkblocks", DEFAULT_CHECKBLOCKS))) {
