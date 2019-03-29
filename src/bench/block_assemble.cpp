63ed23221d89709efa04f34f1e7abceac4ba97df
James O'Beirne <james.obeirne@gmail.com>
2019-03-29 15:22:06 -0400

    validation: introduce ChainstateManager, use in init
    ChainstateManager is responsible for creating and managing multiple
    chainstates, and will provide a high-level interface for accessing the
    appropriate chainstate based upon a certain use.
    
    This commit also retires `g_chainstate`.
    
    In future commits, we will add logic for loading UTXO snapshots into
    ChainstateManager.

--- src/bench/block_assemble.cpp
+++ src/bench/block_assemble.cpp
@@ -75,8 +75,8 @@ static void AssembleBlock(benchmark::State& state)
     {
         LOCK(cs_main);
         ::pblocktree.reset(new CBlockTreeDB(1 << 20, true));
-        g_chainstate.reset(new CChainState(
-            /* cache_size_bytes */ 1 << 23, /* in_memory */ true, /* should_wipe */ false));
+        g_chainman.InitializeChainstate(
+            /* cache_size_bytes */ 1 << 23, /* in_memory */ true, /* should_wipe */ false);
         UnloadBlockIndex();
     }
     {
