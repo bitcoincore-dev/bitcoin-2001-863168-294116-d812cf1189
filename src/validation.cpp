be2ec665f2e6d294df37decad975d6d217bdc86a
James O'Beirne <james.obeirne@gmail.com>
2019-03-29 15:58:45 -0400

    validation: introduce ChainstateManager::GetChainstateForNewBlock
    and use it in ProcessNewBlock().
    
    This is needed for an upcoming change to `net_processing`.

--- src/validation.cpp
+++ src/validation.cpp
@@ -3792,6 +3792,12 @@ bool ProcessNewBlock(const CChainParams& chainparams, const std::shared_ptr<cons
 {
     AssertLockNotHeld(cs_main);
 
+    CChainState* chainstate;
+    {
+        LOCK(cs_main);
+        chainstate = &g_chainman.GetChainstateForNewBlock(pblock->GetHash());
+    }
+
     {
         CBlockIndex *pindex = nullptr;
         if (fNewBlock) *fNewBlock = false;
@@ -3806,7 +3812,7 @@ bool ProcessNewBlock(const CChainParams& chainparams, const std::shared_ptr<cons
         bool ret = CheckBlock(*pblock, state, chainparams.GetConsensus());
         if (ret) {
             // Store to disk
-            ret = ::ChainstateActive().AcceptBlock(pblock, state, chainparams, &pindex, fForceProcessing, nullptr, fNewBlock);
+            ret = chainstate->AcceptBlock(pblock, state, chainparams, &pindex, fForceProcessing, nullptr, fNewBlock);
         }
         if (!ret) {
             GetMainSignals().BlockChecked(*pblock, state);
@@ -3814,10 +3820,12 @@ bool ProcessNewBlock(const CChainParams& chainparams, const std::shared_ptr<cons
         }
     }
 
-    NotifyHeaderTip();
+    if (chainstate == &::ChainstateActive()) {
+        NotifyHeaderTip();
+    }
 
     BlockValidationState state; // Only used to report errors, not invalidity - ignore it
-    if (!::ChainstateActive().ActivateBestChain(state, chainparams, pblock))
+    if (!chainstate->ActivateBestChain(state, chainparams, pblock))
         return error("%s: ActivateBestChain failed (%s)", __func__, FormatStateMessage(state));
 
     return true;
@@ -5454,6 +5462,16 @@ bool ChainstateManager::PopulateAndValidateSnapshot(
     LogPrintf("[snapshot] validated snapshot (%.2f MB)\n",
         coins_cache.DynamicMemoryUsage() / (1000 * 1000));
     return true;
+}
+
+CChainState& ChainstateManager::GetChainstateForNewBlock(const uint256& blockhash)
+{
+    if (m_snapshot_chainstate &&
+            !m_snapshot_chainstate->m_chain.Contains(LookupBlockIndex(blockhash))) {
+        return *m_snapshot_chainstate.get();
+    }
+    assert(m_ibd_chainstate);
+    return *m_ibd_chainstate.get();
 }
 
 CChain& ChainstateManager::ActiveChain() const
