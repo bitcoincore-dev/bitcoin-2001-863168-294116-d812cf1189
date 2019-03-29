4398d08883eae68e82ccb009f111b3068897a2f9
James O'Beirne <james.obeirne@gmail.com>
2019-03-29 14:29:12 -0400

    trivial: add CChainState::ToString()

--- src/validation.h
+++ src/validation.h
@@ -618,6 +618,8 @@ public:
      */
     std::set<CBlockIndex*, CBlockIndexWorkComparator> setBlockIndexCandidates;
 
+    std::string ToString();
+
     //! @returns A reference to the in-memory cache of the UTXO set.
     CCoinsViewCache& CoinsTip() EXCLUSIVE_LOCKS_REQUIRED(cs_main)
     {
