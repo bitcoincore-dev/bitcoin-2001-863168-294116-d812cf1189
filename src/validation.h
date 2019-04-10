351899e269540237aa25676038dd814ae0f0327a
James O'Beirne <james.obeirne@gmail.com>
2019-04-10 15:42:34 -0400

    refactoring: break pindexBestInvalid out of BlockManager
    It's only used internally in validation.cpp, so no reason for it
    to be exposed or managed by BlockManager.

--- src/validation.h
+++ src/validation.h
@@ -507,8 +507,6 @@ public:
      */
     std::multimap<CBlockIndex*, CBlockIndex*> m_blocks_unlinked;
 
-    CBlockIndex *m_pindex_best_invalid = nullptr;
-
     bool LoadBlockIndex(
         const Consensus::Params& consensus_params,
         CBlockTreeDB& blocktree) EXCLUSIVE_LOCKS_REQUIRED(cs_main);
@@ -516,7 +514,6 @@ public:
     void Unload() EXCLUSIVE_LOCKS_REQUIRED(cs_main) {
         m_failed_blocks.clear();
         m_blocks_unlinked.clear();
-        m_pindex_best_invalid = nullptr;
 
         for (const BlockMap::value_type& entry : m_block_index) {
             delete entry.second;
