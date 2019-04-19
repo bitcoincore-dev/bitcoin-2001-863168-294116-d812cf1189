903c4a507e5c4858eb78b99de67dbec3e51caf65
Luke Dashjr <luke-jr+git@utopios.org>
2019-04-19 02:01:53 +0000

    Partially Revert "GUI: Remove SendToSelf TransactionRecord type"
    This reverts commit 065121a6edfba67ac9203a9ebaeeec4c0322afb6.
    
    (To minimise diff in Knots)

--- src/qt/transactionrecord.h
+++ src/qt/transactionrecord.h
@@ -81,6 +81,7 @@ public:
         SendToOther,
         RecvWithAddress,
         RecvFromOther,
+        SendToSelf
     };
 
     /** Number of confirmation recommended for accepting a transaction */
