b52ae60d6e997afe3e1eabc08fffd2dee2dac17a
Luke Dashjr <luke-jr+git@utopios.org>
2019-04-19 02:01:53 +0000

    Partially Revert "GUI: Remove SendToSelf TransactionRecord type"
    This reverts commit 065121a6edfba67ac9203a9ebaeeec4c0322afb6.
    
    (To minimise diff in Knots)

--- src/qt/transactiontablemodel.cpp
+++ src/qt/transactiontablemodel.cpp
@@ -346,6 +346,8 @@ QString TransactionTableModel::formatTxType(const TransactionRecord *wtx) const
     case TransactionRecord::SendToAddress:
     case TransactionRecord::SendToOther:
         return tr("Sent to");
+    case TransactionRecord::SendToSelf:
+        return tr("Payment to yourself");
     case TransactionRecord::Generated:
         return tr("Mined");
     default:
@@ -388,6 +390,7 @@ QString TransactionTableModel::formatTxToAddress(const TransactionRecord *wtx, b
         return lookupAddress(wtx->address, tooltip) + watchAddress;
     case TransactionRecord::SendToOther:
         return QString::fromStdString(wtx->address) + watchAddress;
+    case TransactionRecord::SendToSelf:
     default:
         return tr("(n/a)") + watchAddress;
     }
@@ -406,6 +409,8 @@ QVariant TransactionTableModel::addressColor(const TransactionRecord *wtx) const
         if(label.isEmpty())
             return COLOR_BAREADDRESS;
         } break;
+    case TransactionRecord::SendToSelf:
+        return COLOR_BAREADDRESS;
     default:
         break;
     }
