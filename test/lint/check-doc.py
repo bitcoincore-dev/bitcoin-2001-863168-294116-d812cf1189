cfece4df3d9764ca5381142047688f48553944ef
MarcoFalke <falke.marco@gmail.com>
2019-04-28 10:33:22 -0400

    lint: Check that all wallet args are hidden

--- test/lint/check-doc.py
+++ test/lint/check-doc.py
@@ -20,6 +20,8 @@ REGEX_ARG = '(?:ForceSet|SoftSet|Get|Is)(?:Bool)?Args?(?:Set)?\("(-[^"]+)"'
 REGEX_DOC = 'AddArg\("(-[^"=]+?)(?:=|")'
 CMD_ROOT_DIR = '`git rev-parse --show-toplevel`/{}'.format(FOLDER_GREP)
 CMD_GREP_ARGS = r"git grep --perl-regexp '{}' -- {} ':(exclude){}'".format(REGEX_ARG, CMD_ROOT_DIR, FOLDER_TEST)
+CMD_GREP_WALLET_ARGS = r"git grep --function-context 'void WalletInit::AddWalletOptions' -- {} | grep AddArg".format(CMD_ROOT_DIR)
+CMD_GREP_WALLET_HIDDEN_ARGS = r"git grep --function-context 'void DummyWalletInit::AddWalletOptions' -- {}".format(CMD_ROOT_DIR)
 CMD_GREP_DOCS = r"git grep --perl-regexp '{}' {}".format(REGEX_DOC, CMD_ROOT_DIR)
 # list unsupported, deprecated and duplicate args as they need no documentation
 SET_DOC_OPTIONAL = set(['-h', '-help', '-dbcrashratio', '-forcecompactdb'])
@@ -45,7 +47,23 @@ def main():
     print("Args unknown     : {}".format(len(args_unknown)))
     print(args_unknown)
 
-    sys.exit(len(args_need_doc))
+    assert 0 == len(args_need_doc), "Please document the following arguments: {}".format(args_need_doc)
+
+
+    # missing hidden wallet args
+    if sys.version_info >= (3, 6):
+        wallet_args = check_output(CMD_GREP_WALLET_ARGS, shell=True, universal_newlines=True, encoding='utf8')
+        wallet_hidden_args = check_output(CMD_GREP_WALLET_HIDDEN_ARGS, shell=True, universal_newlines=True, encoding='utf8')
+    else:
+        wallet_args = check_output(CMD_GREP_WALLET_ARGS, shell=True).decode('utf8').strip()
+        wallet_hidden_args = check_output(CMD_GREP_WALLET_HIDDEN_ARGS, shell=True).decode('utf8').strip()
+
+    wallet_args = set(re.findall(re.compile(REGEX_DOC), wallet_args))
+    wallet_hidden_args = set(re.findall(re.compile(r'"([^"=]+)(?:=[^"]*)?"'), wallet_hidden_args))
+
+    hidden_missing = wallet_args.difference(wallet_hidden_args)
+    if hidden_missing:
+        assert 0, "Please add {} to the hidden args in DummyWalletInit::AddWalletOptions".format(hidden_missing)
 
 
 if __name__ == "__main__":
