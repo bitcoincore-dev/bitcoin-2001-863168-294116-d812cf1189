quantumbitcoin/bitcoin

Qt/Options: Replace prune setting with rwconf-based version

src/qt/optionsmodel.cpp
@@ -91,14 +91,18 @@ void OptionsModel::Init(bool resetSettings)
     // by command-line and show this in the UI.
 
     // Main
-    if (!settings.contains("bPrune"))
-        settings.setValue("bPrune", false);
-    if (!settings.contains("nPruneSize"))
-        settings.setValue("nPruneSize", 2);
-    // Convert prune size to MB:
-    const uint64_t nPruneSizeMB = settings.value("nPruneSize").toInt() * 1000;
-    if (!m_node.softSetArg("-prune", settings.value("bPrune").toBool() ? std::to_string(nPruneSizeMB) : "0")) {
-      addOverriddenOption("-prune");
+    if (!gArgs.IsArgSet("-prune")) {
+        if (settings.contains("bPrune")) {
+            if (settings.value("bPrune").toBool()) {
+                if (!settings.contains("nPruneSize"))
+                    settings.setValue("nPruneSize", 2);
+                // Convert prune size to MB:
+                const uint64_t nPruneSizeMB = settings.value("nPruneSize").toInt() * 1000;
+                gArgs.ForceSetArg("-prune", nPruneSizeMB);
+            } else {
+                gArgs.ForceSetArg("-prune", "0");
+            }
+        }
     }
 
     if (!settings.contains("nDatabaseCache"))
@@ -209,7 +213,7 @@ void OptionsModel::Reset()
     // Set strDataDir
     settings.setValue("strDataDir", dataDir);
 
-    // Set prune option iff it was initialised using Intro
+    // Set prune option iff it was configured in rwconf
     if (gArgs.RWConfigHasPruneOption()) {
         gArgs.ModifyRWConfigFile("prune", gArgs.GetArg("prune", ""));
     }
@@ -320,10 +324,8 @@ QVariant OptionsModel::data(const QModelIndex & index, int role) const
             return settings.value("language");
         case CoinControlFeatures:
             return fCoinControlFeatures;
-        case Prune:
-            return settings.value("bPrune");
-        case PruneSize:
-            return settings.value("nPruneSize");
+        case PruneMB:
+            return qlonglong(gArgs.GetArg("-prune", 0));
         case DatabaseCache:
             return settings.value("nDatabaseCache");
         case ThreadsScriptVerif:
@@ -479,18 +481,17 @@ bool OptionsModel::setData(const QModelIndex & index, const QVariant & value, in
             settings.setValue("fCoinControlFeatures", fCoinControlFeatures);
             Q_EMIT coinControlFeaturesChanged(fCoinControlFeatures);
             break;
-        case Prune:
-            if (settings.value("bPrune") != value) {
-                settings.setValue("bPrune", value);
+        case PruneMB:
+        {
+            const qlonglong llvalue = value.toLongLong();
+            if (gArgs.GetArg("-prune", 0) != llvalue) {
+                gArgs.ModifyRWConfigFile("prune", value.toString().toStdString());
+                settings.setValue("bPrune", (llvalue > 1));
+                settings.setValue("nPruneSize", llvalue / 1000);
                 setRestartRequired(true);
             }
             break;
-        case PruneSize:
-            if (settings.value("nPruneSize") != value) {
-                settings.setValue("nPruneSize", value);
-                setRestartRequired(true);
-            }
-            break;
+        }
         case DatabaseCache:
             if (settings.value("nDatabaseCache") != value) {
                 settings.setValue("nDatabaseCache", value);

bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

src/qt/optionsmodel.cpp
@@ -94,14 +94,18 @@ void OptionsModel::Init(bool resetSettings)
     // by command-line and show this in the UI.
 
     // Main
-    if (!settings.contains("bPrune"))
-        settings.setValue("bPrune", false);
-    if (!settings.contains("nPruneSize"))
-        settings.setValue("nPruneSize", 2);
-    // Convert prune size from GB to MiB:
-    const uint64_t nPruneSizeMiB = (settings.value("nPruneSize").toInt() * GB_BYTES) >> 20;
-    if (!m_node.softSetArg("-prune", settings.value("bPrune").toBool() ? std::to_string(nPruneSizeMiB) : "0")) {
-        addOverriddenOption("-prune");
+    if (!gArgs.IsArgSet("-prune")) {
+        if (settings.contains("bPrune")) {
+            if (settings.value("bPrune").toBool()) {
+                if (!settings.contains("nPruneSize"))
+                    settings.setValue("nPruneSize", 2);
+                // Convert prune size from GB to MiB:
+                const uint64_t nPruneSizeMiB = (settings.value("nPruneSize").toInt() * GB_BYTES) >> 20;
+                gArgs.ForceSetArg("-prune", nPruneSizeMiB);
+            } else {
+                gArgs.ForceSetArg("-prune", "0");
+            }
+        }
     }
 
     if (!settings.contains("nDatabaseCache"))
@@ -212,7 +216,7 @@ void OptionsModel::Reset()
     // Set strDataDir
     settings.setValue("strDataDir", dataDir);
 
-    // Set prune option iff it was initialised using Intro
+    // Set prune option iff it was configured in rwconf
     if (gArgs.RWConfigHasPruneOption()) {
         gArgs.ModifyRWConfigFile("prune", gArgs.GetArg("prune", ""));
     }
@@ -323,10 +327,8 @@ QVariant OptionsModel::data(const QModelIndex & index, int role) const
             return settings.value("language");
         case CoinControlFeatures:
             return fCoinControlFeatures;
-        case Prune:
-            return settings.value("bPrune");
-        case PruneSize:
-            return settings.value("nPruneSize");
+        case PruneMiB:
+            return qlonglong(gArgs.GetArg("-prune", 0));
         case DatabaseCache:
             return settings.value("nDatabaseCache");
         case ThreadsScriptVerif:
@@ -483,18 +485,17 @@ bool OptionsModel::setData(const QModelIndex & index, const QVariant & value, in
             settings.setValue("fCoinControlFeatures", fCoinControlFeatures);
             Q_EMIT coinControlFeaturesChanged(fCoinControlFeatures);
             break;
-        case Prune:
-            if (settings.value("bPrune") != value) {
-                settings.setValue("bPrune", value);
+        case PruneMiB:
+        {
+            const qlonglong llvalue = value.toLongLong();
+            if (gArgs.GetArg("-prune", 0) != llvalue) {
+                gArgs.ModifyRWConfigFile("prune", value.toString().toStdString());
+                settings.setValue("bPrune", (llvalue > 1));
+                settings.setValue("nPruneSize", ((llvalue * 1024 * 1024) + GB_BYTES - 1) / GB_BYTES);
                 setRestartRequired(true);
             }
             break;
-        case PruneSize:
-            if (settings.value("nPruneSize") != value) {
-                settings.setValue("nPruneSize", value);
-                setRestartRequired(true);
-            }
-            break;
+        }
         case DatabaseCache:
             if (settings.value("nDatabaseCache") != value) {
                 settings.setValue("nDatabaseCache", value);