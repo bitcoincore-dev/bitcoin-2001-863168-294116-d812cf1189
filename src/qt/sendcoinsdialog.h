776947e6cac31489405c99bf4bac3ed30ac6deaf
Luke Dashjr <luke-jr+git@utopios.org>
2019-05-08 21:34:13 +0000

    GUI: Add a warning prompt when sending to an already-used address

--- src/qt/sendcoinsdialog.h
+++ src/qt/sendcoinsdialog.h
@@ -121,6 +121,7 @@ Q_SIGNALS:
 
 
 #define SEND_CONFIRM_DELAY   3
+#define ADDRESS_REUSE_OVERRIDE_DELAY   10
 
 class SendConfirmationDialog : public QMessageBox
 {
@@ -128,6 +129,7 @@ class SendConfirmationDialog : public QMessageBox
 
 public:
     bool m_delete_on_close{false};
+    QString confirmButtonText{tr("Send")};
     QMessageBox::StandardButton m_yes_button{QMessageBox::Yes};
     QMessageBox::StandardButton m_cancel_button{QMessageBox::Cancel};
 
@@ -145,7 +147,6 @@ private:
     QAbstractButton *m_psbt_button;
     QTimer countDownTimer;
     int secDelay;
-    QString confirmButtonText{tr("Send")};
     bool m_enable_save;
     bool m_enable_send;
     QString m_psbt_button_text{tr("Create Unsigned")};
