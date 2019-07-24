9177224669f2a98f49c1b705bed83e88c057e56d
James O'Beirne <james.obeirne@gmail.com>
2019-07-24 13:23:48 -0400

    refactor: have CCoins* data managed under CChainState
    This change encapsulates UTXO set data within CChainState instances, removing
    global data `pcoinsTip` and `pcoinsviewdb`. This is necessary if we want to
    maintain multiple chainstates with their own rendering of the UTXO set.
    
    We introduce a class CoinsViews which consolidates the construction of a
    CCoins* hierarchy.
    
    This commit could be broken into smaller pieces, but it would require more
    ephemeral diffs to, e.g., temporarily change CCoinsViewDB's constructor
    invocations.
    
    Other changes:
    
    - The initialization order in init.cpp is changed slightly: the coinsdb (leveldb)
      creation is moved to be earlier than block index and genesis block loading,
      though this shouldn't have any noticeable effect.
    
    - A parameter has been added to the CCoinsViewDB constructor that allows the
      name of the corresponding leveldb directory to be specified.
    
    Thanks to Russell Yanofsky for helpful feedback.

--- src/validation.cpp
+++ src/validation.cpp
@@ -82,11 +82,17 @@ namespace {
 BlockManager g_blockman;
 } // anon namespace
 
-static CChainState g_chainstate(g_blockman);
+std::unique_ptr<CChainState> g_chainstate;
 
-CChainState& ChainstateActive() { return g_chainstate; }
+CChainState& ChainstateActive() {
+    assert(g_chainstate);
+    return *g_chainstate;
+}
 
-CChain& ChainActive() { return g_chainstate.m_chain; }
+CChain& ChainActive() {
+    assert(g_chainstate);
+    return g_chainstate->m_chain;
+}
 
 /**
  * Mutex to guard access to validation specific variables, such as reading
@@ -173,8 +179,6 @@ CBlockIndex* FindForkInGlobalIndex(const CChain& chain, const CBlockLocator& loc
     return chain.Genesis();
 }
 
-std::unique_ptr<CCoinsViewDB> pcoinsdbview;
-std::unique_ptr<CCoinsViewCache> pcoinsTip;
 std::unique_ptr<CBlockTreeDB> pblocktree;
 
 // See definition for documentation
@@ -1038,6 +1042,27 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
     nSubsidy >>= halvings;
     return nSubsidy;
 }
+
+CoinsViews::CoinsViews(
+    std::string ldb_name,
+    size_t cache_size_bytes,
+    bool in_memory,
+    bool should_wipe) : m_dbview(MakeUnique<CCoinsViewDB>(
+                            GetDataDir() / ldb_name, cache_size_bytes, in_memory, should_wipe)),
+                        m_catcherview(MakeUnique<CCoinsViewErrorCatcher>(m_dbview.get())),
+                        m_cacheview(MakeUnique<CCoinsViewCache>(m_catcherview.get())) {}
+
+CChainState::CChainState(
+    /* parameters forwarded to CoinsViews */
+    size_t cache_size_bytes,
+    bool in_memory,
+    bool should_wipe,
+    std::string leveldb_name
+    // NOTE: for now m_blockman is set to a global, but this will be changed
+    // in a future commit.
+    ) : m_blockman(g_blockman),
+        m_coins_views(MakeUnique<CoinsViews>(
+            leveldb_name, cache_size_bytes, in_memory, should_wipe)) {}
 
 // Note that though this is marked const, we may end up modifying `m_cached_finished_ibd`, which
 // is a performance-related implementation detail. This function must be marked
