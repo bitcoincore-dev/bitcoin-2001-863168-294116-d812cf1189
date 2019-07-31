a884bacaff43e249d7836aeed684f12e5f0e78fd
Andrew Toth <andrewstoth@gmail.com>
2019-07-31 12:38:14 -0400

    validation: flush state after initial sync

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -910,6 +910,11 @@ bool GetNodeStateStats(NodeId nodeid, CNodeStateStats &stats) {
     return true;
 }
 
+int GetNumberOfPeersWithValidatedDownloads()
+{
+    return nPeersWithValidatedDownloads;
+}
+
 //////////////////////////////////////////////////////////////////////////////
 //
 // mapOrphanTransactions
