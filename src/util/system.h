e581ca4e3510e0ce6f374d54e58e310363831c18
Russell Yanofsky <russ@yanofsky.org>
2019-08-04 09:21:54 -0400

    Add more granular ArgsManager flags
    Make ArgsManager flags more granular to simplify error checking code added in
    next commit.
    
    The granularity also makes error checking a little more flexible, so negation
    being allowed isn't tied to empty string values being allowed, and it's
    possible to do things like allow string options that can't be negated but can
    be empty.
    
    This commit does not change behavior because the flags aren't currently
    currently implemented or referenced outside of tests.

--- src/util/system.h
+++ src/util/system.h
@@ -129,12 +129,23 @@ class ArgsManager
 {
 public:
     enum Flags {
-        NONE = 0x00,
-        // Boolean options can accept negation syntax -noOPTION or -noOPTION=1
-        ALLOW_BOOL = 0x01,
-        ALLOW_INT = 0x02,
-        ALLOW_STRING = 0x04,
-        ALLOW_ANY = ALLOW_BOOL | ALLOW_INT | ALLOW_STRING,
+        NONE = 0x0, //<! Indicates flag lookup failed.
+
+        /* Low level validation flags. In most cases these should be avoided,
+         * and standard TYPE_* flag combinations below should be preferred. */
+        ALLOW_ANY      = 0x01, //!< disable validation
+        ALLOW_NEGATED  = 0x02, //!< allow -nofoo and -nofoo=1
+        ALLOW_EMPTY    = 0x04, //!< allow -foo and -foo=""
+        ALLOW_NONEMPTY = 0x08, //!< allow -foo=bar
+        ALLOW_INT      = 0x10, //!< allow -foo=123
+        ALLOW_BOOL     = 0x20, //!< allow -foo=
+
+        /* Standard value types. */
+        TYPE_STRING = ALLOW_NONEMPTY,
+        TYPE_OPTIONAL_STRING = ALLOW_NONEMPTY | ALLOW_NEGATED | ALLOW_EMPTY,
+        TYPE_INT = ALLOW_INT | ALLOW_NEGATED,
+        TYPE_BOOL = ALLOW_BOOL | ALLOW_NEGATED | ALLOW_EMPTY,
+
         DEBUG_ONLY = 0x100,
         /* Some options would cause cross-contamination if values for
          * mainnet were used while running on regtest/testnet (or vice-versa).
