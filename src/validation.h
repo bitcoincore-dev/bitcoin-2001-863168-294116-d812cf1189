9e404d38046c099819bc5ee82903733cda18ea4f
James O'Beirne <james.obeirne@gmail.com>
2019-09-11 14:24:29 -0400

    p2p: don't advertise until we finish all IBDs
    Until we have completed IBD on all chainstates, don't answer getheaders
    requests from our peers or advertise ourselves. This preserves the
    existing behavior of not performing these actions until IBD of the
    whole chain completes.

--- src/validation.h
+++ src/validation.h
@@ -1187,6 +1187,9 @@ public:
     //! @sa node/chainstate:LoadChainstate()
     bool ValidatedSnapshotCleanup() EXCLUSIVE_LOCKS_REQUIRED(::cs_main);
 
+    //! Returns true if any chainstate in use is in initial block download.
+    bool IsAnyChainInIBD() const EXCLUSIVE_LOCKS_REQUIRED(::cs_main);
+
     ~ChainstateManager();
 };
 
