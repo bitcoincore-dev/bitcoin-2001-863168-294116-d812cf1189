8672ad8506e71c4fbc6b5c4a470e25c1c4813482
Russell Yanofsky <russ@yanofsky.org>
2019-09-17 19:05:26 -0400

    Avoid using g_rpc_node global in wallet code
    Wallet code should use interfaces::Chain and not directly access to node state.
    
    Add a g_rpc_chain replacement global for wallet code to use, and move
    g_rpc_node definition to a libbitcoin_server source file so there are link
    errors if wallet code tries to access it.

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -2290,3 +2290,5 @@ void RegisterBlockchainRPCCommands(CRPCTable &t)
     for (unsigned int vcidx = 0; vcidx < ARRAYLEN(commands); vcidx++)
         t.appendCommand(commands[vcidx].name, &commands[vcidx]);
 }
+
+Node* g_rpc_node = nullptr;
