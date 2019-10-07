045f7de4f6d7d5e0aa927b726d277dccaee7fb5e
Andrew Chow <achow101-github@achow101.com>
2019-10-07 14:11:34 -0400

    Refactor: Move encryption code between KeyMan and Wallet
    Let wallet class handle locked/unlocked status and master key, and let keyman
    handle encrypting its data and determining whether there is encrypted data.
    
    There should be no change in behavior, but state is tracked differently.
    The fUseCrypto atomic bool is eliminated and replaced with equivalent
    HasEncryptionKeys and HasEncryptedData checks.

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -528,8 +528,7 @@ bool CWallet::EncryptWallet(const SecureString& strWalletPassphrase)
     {
         LOCK(cs_wallet);
         mapMasterKeys[++nMasterKeyMaxID] = kMasterKey;
-        assert(!encrypted_batch);
-        encrypted_batch = new WalletBatch(*database);
+        WalletBatch* encrypted_batch = new WalletBatch(*database);
         if (!encrypted_batch->TxnBegin()) {
             delete encrypted_batch;
             encrypted_batch = nullptr;
@@ -538,7 +537,7 @@ bool CWallet::EncryptWallet(const SecureString& strWalletPassphrase)
         encrypted_batch->WriteMasterKey(nMasterKeyMaxID, kMasterKey);
 
         if (auto spk_man = m_spk_man.get()) {
-            if (!spk_man->EncryptKeys(_vMasterKey)) {
+            if (!spk_man->Encrypt(_vMasterKey, encrypted_batch)) {
                 encrypted_batch->TxnAbort();
                 delete encrypted_batch;
                 encrypted_batch = nullptr;
@@ -3980,15 +3979,18 @@ std::vector<OutputGroup> CWallet::GroupOutputs(const std::vector<COutput>& outpu
     return groups;
 }
 
-bool CWallet::SetCrypted()
+bool CWallet::IsCrypted() const
 {
-    LOCK(cs_KeyStore);
-    if (fUseCrypto)
+    if (HasEncryptionKeys()) {
         return true;
-    if (!mapKeys.empty())
-        return false;
-    fUseCrypto = true;
-    return true;
+    }
+
+    if (m_spk_man) {
+        if (m_spk_man->HasEncryptedData()) {
+            return true;
+        }
+    }
+    return false;
 }
 
 bool CWallet::IsLocked() const
@@ -4002,7 +4004,7 @@ bool CWallet::IsLocked() const
 
 bool CWallet::Lock()
 {
-    if (!SetCrypted())
+    if (!IsCrypted())
         return false;
 
     {
@@ -4010,6 +4012,21 @@ bool CWallet::Lock()
         vMasterKey.clear();
     }
 
+    NotifyStatusChanged(this);
+    return true;
+}
+
+bool CWallet::Unlock(const CKeyingMaterial& vMasterKeyIn, bool accept_no_keys)
+{
+    {
+        LOCK(cs_KeyStore);
+        if (m_spk_man) {
+            if (!m_spk_man->CheckDecryptionKey(vMasterKeyIn, accept_no_keys)) {
+                return false;
+            }
+        }
+        vMasterKey = vMasterKeyIn;
+    }
     NotifyStatusChanged(this);
     return true;
 }
@@ -4027,4 +4044,14 @@ const SigningProvider* CWallet::GetSigningProvider() const
 LegacyScriptPubKeyMan* CWallet::GetLegacyScriptPubKeyMan() const
 {
     return m_spk_man.get();
+}
+
+const CKeyingMaterial& CWallet::GetEncryptionKey() const
+{
+    return vMasterKey;
+}
+
+bool CWallet::HasEncryptionKeys() const
+{
+    return !mapMasterKeys.empty();
 }
