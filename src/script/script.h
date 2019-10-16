03f407f827ce970f043226c3234c195f958d843d
Jeremy Rubin <j@rubin.io>
2019-10-16 15:04:09 -0700

    Make Bare OP_CHECKTEMPLATEVERIFY basic transactions standard

--- src/script/script.h
+++ src/script/script.h
@@ -533,6 +533,7 @@ public:
      */
     unsigned int GetSigOpCount(const CScript& scriptSig) const;
 
+    bool IsPayToBareDefaultCheckTemplateVerifyHash() const;
     bool IsPayToScriptHash() const;
     bool IsPayToWitnessScriptHash() const;
     bool IsWitnessProgram(int& version, std::vector<unsigned char>& program) const;
