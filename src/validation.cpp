70b70f8e95b6bdfb4a4a09aa5746ef2a6327b60b
Luke Dashjr <luke-jr+git@utopios.org>
2019-10-30 15:38:30 +0000

    MOVEONLY: policy: Move RBF variables to policy/settings

--- src/validation.cpp
+++ src/validation.cpp
@@ -122,7 +122,6 @@ bool fCheckpointsEnabled = DEFAULT_CHECKPOINTS_ENABLED;
 size_t nCoinCacheUsage = 5000 * 300;
 uint64_t nPruneTarget = 0;
 int64_t nMaxTipAge = DEFAULT_MAX_TIP_AGE;
-bool fEnableReplacement = DEFAULT_ENABLE_REPLACEMENT;
 
 uint256 hashAssumeValid;
 arith_uint256 nMinimumChainWork;
