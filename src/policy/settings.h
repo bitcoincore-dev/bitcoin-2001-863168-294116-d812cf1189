ccf9e191c06b3a8a876ccf89431399d6e9eeb35e
Luke Dashjr <luke-jr+git@utopios.org>
2019-10-30 15:38:30 +0000

    MOVEONLY: policy: Move RBF variables to policy/settings

--- src/policy/settings.h
+++ src/policy/settings.h
@@ -16,6 +16,7 @@ extern CFeeRate incrementalRelayFee;
 extern CFeeRate dustRelayFee;
 extern unsigned int nBytesPerSigOp;
 extern bool fIsBareMultisigStd;
+extern bool fEnableReplacement;
 
 static inline bool IsStandardTx(const CTransaction& tx, std::string& reason, const ignore_rejects_type& ignore_rejects=empty_ignore_rejects)
 {
