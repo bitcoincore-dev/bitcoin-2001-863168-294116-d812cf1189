e6b97f04a84283e24a0b52a0e0475edce386e764
Luke Dashjr <luke-jr+git@utopios.org>
2019-10-30 11:23:57 +0000

    Restore -mempoolreplacement option to allow disabling opt-in RBF
    This partially reverts commit 8053e5cdade87550f0381d51feab81dedfec6c46.
    
    Diff-minimised

--- src/validation.cpp
+++ src/validation.cpp
@@ -122,6 +122,7 @@ bool fCheckpointsEnabled = DEFAULT_CHECKPOINTS_ENABLED;
 size_t nCoinCacheUsage = 5000 * 300;
 uint64_t nPruneTarget = 0;
 int64_t nMaxTipAge = DEFAULT_MAX_TIP_AGE;
+bool fEnableReplacement = DEFAULT_ENABLE_REPLACEMENT;
 
 uint256 hashAssumeValid;
 arith_uint256 nMinimumChainWork;
@@ -611,6 +612,8 @@ bool MemPoolAccept::PreChecks(ATMPArgs& args, Workspace& ws)
                 // unconfirmed ancestors anyway; doing otherwise is hopelessly
                 // insecure.
                 bool fReplacementOptOut = true;
+                if (fEnableReplacement) {
+
                 for (const CTxIn &_txin : ptxConflicting->vin)
                 {
                     if (_txin.nSequence <= MAX_BIP125_RBF_SEQUENCE)
@@ -619,6 +622,8 @@ bool MemPoolAccept::PreChecks(ATMPArgs& args, Workspace& ws)
                         break;
                     }
                 }
+
+                }  // fEnableReplacement
                 if (fReplacementOptOut) {
                     return state.Invalid(ValidationInvalidReason::TX_MEMPOOL_POLICY, false, REJECT_DUPLICATE, "txn-mempool-conflict");
                 }
