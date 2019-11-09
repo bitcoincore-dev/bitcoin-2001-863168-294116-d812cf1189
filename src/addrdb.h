5297afde0a900f5eab4265f0ec2dd87bc94737e2
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2019-11-09 23:02:09 +0200

    p2p: Add ReadAnchors()

--- src/addrdb.h
+++ src/addrdb.h
@@ -11,9 +11,9 @@
 #include <serialize.h>
 
 #include <string>
-#include <map>
+#include <vector>
 
-class CSubNet;
+class CAddress;
 class CAddrMan;
 class CDataStream;
 
@@ -94,5 +94,8 @@ public:
     bool Write(const banmap_t& banSet);
     bool Read(banmap_t& banSet);
 };
+
+/** Read the anchor IP address database (anchors.dat) */
+std::vector<CAddress> ReadAnchors(const fs::path& anchors_db_path);
 
 #endif // BITCOIN_ADDRDB_H
