148f0de1f040f7446380fb36060b16af91e7b7c9
Matt Corallo <git@bluematt.me>
2019-12-01 19:30:08 -0500

    Fix a few bugs in REST-headers handling that got copied to filter (partial)
    Github-Pull: #17631
    Rebased-From: 84c127d8f171d1f6873bb0689b504ab9abd8e25e (partial)

--- src/rest.cpp
+++ src/rest.cpp
@@ -334,7 +334,7 @@ static bool rest_filter_header(const util::Ref& context, HTTPRequest* req, const
 
     long count = strtol(uriParts[1].c_str(), nullptr, 10);
     if (count < 1 || count > 2000) {
-        return RESTERR(req, HTTP_BAD_REQUEST, "Header count out of range: " + uriParts[1]);
+        return RESTERR(req, HTTP_BAD_REQUEST, "Header count out of acceptable range (1-2000): " + uriParts[1]);
     }
 
     std::vector<const CBlockIndex *> headers;
@@ -406,7 +406,7 @@ static bool rest_filter_header(const util::Ref& context, HTTPRequest* req, const
         return true;
     }
     default: {
-        return RESTERR(req, HTTP_NOT_FOUND, "output format not found (available: .bin, .hex)");
+        return RESTERR(req, HTTP_NOT_FOUND, "output format not found (available: " + AvailableDataFormatsString() + ")");
     }
     }
 
