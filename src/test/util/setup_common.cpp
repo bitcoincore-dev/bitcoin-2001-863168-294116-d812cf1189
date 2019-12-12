5e4fbe17bceb3540c698eb9810fdfbe86a8d297c
James O'Beirne <james.obeirne@gmail.com>
2019-12-12 10:48:28 -0500

    use ChainstateManager to initialize chainstate
    This allows us to easily initialize multiple chainstates on startup in future
    commits. It retires the g_chainstate global in lieu of g_chainman.

--- src/test/util/setup_common.cpp
+++ src/test/util/setup_common.cpp
@@ -109,7 +109,7 @@ TestingSetup::TestingSetup(const std::string& chainName) : BasicTestingSetup(cha
     GetMainSignals().RegisterBackgroundSignalScheduler(scheduler);
 
     pblocktree.reset(new CBlockTreeDB(1 << 20, true));
-    g_chainstate = MakeUnique<CChainState>();
+    g_chainman.InitializeChainstate();
     ::ChainstateActive().InitCoinsDB(
         /* cache_size_bytes */ 1 << 23, /* in_memory */ true, /* should_wipe */ false);
     assert(!::ChainstateActive().CanFlushToDisk());
@@ -148,7 +148,7 @@ TestingSetup::~TestingSetup()
     m_node.banman.reset();
     m_node.mempool = nullptr;
     UnloadBlockIndex();
-    g_chainstate.reset();
+    g_chainman.Reset();
     pblocktree.reset();
 }
 
