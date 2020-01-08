1e825976a209f3234ff5049a9effb20a8429b44b
Luke Dashjr <luke-jr+git@utopios.org>
2020-01-08 00:00:23 +0000

    QA: p2p_dos_header_tree: Use node-to-test / outgoing connection to check disconnect due to checkpoint violation
    Also disable minimumchainwork test to avoid being disconnected for that
    
    For relax_invblk_punishment

--- test/functional/p2p_dos_header_tree.py
+++ test/functional/p2p_dos_header_tree.py
@@ -22,6 +22,10 @@ class RejectLowDifficultyHeadersTest(BitcoinTestFramework):
         self.setup_clean_chain = True
         self.chain = 'testnet3'  # Use testnet chain because it has an early checkpoint
         self.num_nodes = 2
+        self.extra_args = [
+            ["-minimumchainwork=0"],
+            ["-minimumchainwork=0"]
+        ]
 
     def add_options(self, parser):
         parser.add_argument(
@@ -46,7 +50,7 @@ class RejectLowDifficultyHeadersTest(BitcoinTestFramework):
         self.headers_fork = [FromHex(CBlockHeader(), h) for h in self.headers_fork]
 
         self.log.info("Feed all non-fork headers, including and up to the first checkpoint")
-        self.nodes[0].add_p2p_connection(P2PInterface())
+        self.nodes[0].add_p2p_connection(P2PInterface(), node_outgoing=True)
         self.nodes[0].p2p.send_message(msg_headers(self.headers))
         self.nodes[0].p2p.sync_with_ping()
         assert {
@@ -63,8 +67,8 @@ class RejectLowDifficultyHeadersTest(BitcoinTestFramework):
 
         self.log.info("Feed all fork headers (succeeds without checkpoint)")
         # On node 0 it succeeds because checkpoints are disabled
-        self.restart_node(0, extra_args=['-nocheckpoints'])
-        self.nodes[0].add_p2p_connection(P2PInterface())
+        self.restart_node(0, extra_args=['-nocheckpoints', "-minimumchainwork=0"])
+        self.nodes[0].add_p2p_connection(P2PInterface(), node_outgoing=True)
         self.nodes[0].p2p.send_message(msg_headers(self.headers_fork))
         self.nodes[0].p2p.sync_with_ping()
         assert {
@@ -75,7 +79,7 @@ class RejectLowDifficultyHeadersTest(BitcoinTestFramework):
         } in self.nodes[0].getchaintips()
 
         # On node 1 it succeeds because no checkpoint has been reached yet by a chain tip
-        self.nodes[1].add_p2p_connection(P2PInterface())
+        self.nodes[1].add_p2p_connection(P2PInterface(), node_outgoing=True)
         self.nodes[1].p2p.send_message(msg_headers(self.headers_fork))
         self.nodes[1].p2p.sync_with_ping()
         assert {
