cf431d3c8be150cc0db6f217a97fa00468c11514
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-01-19 18:53:31 +0200

    qt: Move conns from WalletFrame to BitcoinGUI
    This commit does not change behavior.

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -632,7 +632,8 @@ void BitcoinGUI::setWalletController(WalletController* wallet_controller)
 void BitcoinGUI::addWallet(WalletModel* walletModel)
 {
     if (!walletFrame) return;
-    if (!walletFrame->addWallet(walletModel)) return;
+    WalletView* walletView = new WalletView(platformStyle, walletFrame);
+    if (!walletFrame->addWallet(walletModel, walletView)) return;
     const QString display_name = walletModel->getDisplayName();
     setWalletActionsEnabled(true);
     rpcConsole->addWallet(walletModel);
@@ -641,6 +642,14 @@ void BitcoinGUI::addWallet(WalletModel* walletModel)
         m_wallet_selector_label_action->setVisible(true);
         m_wallet_selector_action->setVisible(true);
     }
+
+    connect(walletView, &WalletView::outOfSyncWarningClicked, walletFrame, &WalletFrame::outOfSyncWarningClicked);
+    connect(walletView, &WalletView::transactionClicked, this, &BitcoinGUI::gotoHistoryPage);
+    connect(walletView, &WalletView::coinsSent, this, &BitcoinGUI::gotoHistoryPage);
+    connect(walletView, &WalletView::message, this, &BitcoinGUI::message);
+    connect(walletView, &WalletView::encryptionStatusChanged, this, &BitcoinGUI::updateWalletStatus);
+    connect(walletView, &WalletView::incomingTransaction, this, &BitcoinGUI::incomingTransaction);
+    connect(walletView, &WalletView::hdEnabledStatusChanged, this, &BitcoinGUI::updateWalletStatus);
 }
 
 void BitcoinGUI::removeWallet(WalletModel* walletModel)
