e0e797d91dbd5e535391f5b3b2981666ccbec0e8
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-01-19 19:23:46 +0200

    qt: Make path to showModalOverlay slot shorter
    This commit does not change behavior.

--- src/qt/walletview.h
+++ src/qt/walletview.h
@@ -109,9 +109,6 @@ public Q_SLOTS:
     /** Show progress dialog e.g. for rescan */
     void showProgress(const QString &title, int nProgress);
 
-    /** User has requested more information about the out of sync state */
-    void requestedSyncWarningInfo();
-
 Q_SIGNALS:
     void transactionClicked();
     void coinsSent();
