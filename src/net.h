b5d47c1c6a08b545caec44f61c92932e9c717809
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-02-06 19:00:26 +0200

    refactor: Move port mapping code to own files
    This commit does not change behavior.

--- src/net.h
+++ src/net.h
@@ -32,11 +32,6 @@
 #include <memory>
 #include <condition_variable>
 
-#ifndef WIN32
-#include <arpa/inet.h>
-#endif
-
-
 class CScheduler;
 class CNode;
 class BanMan;
@@ -67,12 +62,6 @@ static const int MAX_BLOCK_RELAY_ONLY_CONNECTIONS = 2;
 static const int MAX_FEELER_CONNECTIONS = 1;
 /** -listen default */
 static const bool DEFAULT_LISTEN = true;
-/** -upnp default */
-#ifdef USE_UPNP
-static const bool DEFAULT_UPNP = USE_UPNP;
-#else
-static const bool DEFAULT_UPNP = false;
-#endif
 /** The maximum number of peer connections to maintain. */
 static const unsigned int DEFAULT_MAX_PEER_CONNECTIONS = 125;
 /** The default for -maxuploadtarget. 0 = Unlimited */
@@ -563,9 +552,6 @@ private:
     friend struct ConnmanTestMsg;
 };
 void Discover();
-void StartMapPort();
-void InterruptMapPort();
-void StopMapPort();
 uint16_t GetListenPort();
 
 struct CombinerAll
