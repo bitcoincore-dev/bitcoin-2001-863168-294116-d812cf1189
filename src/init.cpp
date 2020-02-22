557a148768f0ae321ecfa9c8564aeeb385764da9
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-02-23 00:10:48 +0200

    net: Add flags for port mapping protocols

--- src/init.cpp
+++ src/init.cpp
@@ -1881,9 +1881,7 @@ bool AppInitMain(const util::Ref& context, NodeContext& node)
     Discover();
 
     // Map ports with UPnP
-    if (gArgs.GetBoolArg("-upnp", DEFAULT_UPNP)) {
-        StartMapPort();
-    }
+    StartMapPort(gArgs.GetBoolArg("-upnp", DEFAULT_UPNP));
 
     CConnman::Options connOptions;
     connOptions.nLocalServices = nLocalServices;
