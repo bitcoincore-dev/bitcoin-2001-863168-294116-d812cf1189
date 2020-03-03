5725c9c605523d01a3883981c040674da1d7c9d6
Karl-Johan Alm <karljohan-alm@garage.co.jp>
2020-03-03 14:09:02 +0900

    wallet: error if an explicit fee rate was given but the needed fee rate differed
    This avoids cases where a user requests a fee rate below the minimum and is silently overruled by the wallet.
    
    Github-Pull: #11413
    Rebased-From: 110cd63b1fe81cdceec6864c03637e58ab97292c

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3004,6 +3004,12 @@ bool CWallet::CreateTransaction(interfaces::Chain::Lock& locked_chain, const std
 
             // Get the fee rate to use effective values in coin selection
             CFeeRate nFeeRateNeeded = GetMinimumFeeRate(*this, coin_control, &feeCalc);
+            // Do not, ever, assume that it's fine to change the fee rate if the user has explicitly
+            // provided one
+            if (coin_control.m_feerate && nFeeRateNeeded > *coin_control.m_feerate) {
+                strFailReason = strprintf("Fee rate is too low for the current mempool. Increase it to %s or modify -maxmempool", nFeeRateNeeded.ToString());
+                return false;
+            }
 
             nFeeRet = 0;
             bool pick_new_inputs = true;
