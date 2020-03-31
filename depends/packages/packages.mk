720b6f037cfcb0f26e5c3adc02186bb4ec968bbe
Luke Dashjr <luke-jr+git@utopios.org>
2020-03-31 17:07:25 +0000

    Add QtWinExtras optional dependency
    Co-authored-by: Chun Kuan Lee <ken2812221@gmail.com>

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -8,7 +8,7 @@ qrencode_mingw32_packages = qrencode
 qt_linux_packages:=qt expat libxcb xcb_proto libXau xproto freetype fontconfig libxkbcommon libxcb_util libxcb_util_render libxcb_util_keysyms libxcb_util_image libxcb_util_wm
 qt_android_packages=qt
 qt_darwin_packages=qt
-qt_mingw32_packages=qt
+qt_mingw32_packages=qt qtwinextras
 
 bdb_packages=bdb
 sqlite_packages=sqlite
