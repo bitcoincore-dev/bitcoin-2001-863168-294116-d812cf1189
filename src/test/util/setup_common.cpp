e7fa05b4b492aeac691a1661d3437222bd1cc753
Luke Dashjr <luke-jr+git@utopios.org>
2020-03-31 04:10:10 +0000

    Save the number of parallel script check threads in a global variable
    Restore the global nScriptCheckThreads

--- src/test/util/setup_common.cpp
+++ src/test/util/setup_common.cpp
@@ -165,6 +165,7 @@ ChainTestingSetup::ChainTestingSetup(const std::string& chainName, const std::ve
     m_node.chainman->m_blockman.m_block_tree_db = std::make_unique<CBlockTreeDB>(m_cache_sizes.block_tree_db, true);
 
     // Start script-checking threads. Set g_parallel_script_checks to true so they are used.
+    nScriptCheckThreads = 3;
     constexpr int script_check_threads = 2;
     StartScriptCheckWorkerThreads(script_check_threads);
     g_parallel_script_checks = true;
