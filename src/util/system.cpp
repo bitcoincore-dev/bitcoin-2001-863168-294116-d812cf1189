f3b58819db0302e8e93d01138492846d1c73a553
Luke Dashjr <luke-jr+git@utopios.org>
2020-04-11 00:51:23 +0000

    util/settings: Support ArgsManager::ReadConfigStream into other targets

--- src/util/system.cpp
+++ src/util/system.cpp
@@ -799,7 +799,7 @@ bool IsConfSupported(KeyInfo& key, std::string& error) {
     return true;
 }
 
-bool ArgsManager::ReadConfigStream(std::istream& stream, const std::string& filepath, std::string& error, bool ignore_invalid_keys)
+bool ArgsManager::ReadConfigStream(std::istream& stream, const std::string& filepath, std::string& error, bool ignore_invalid_keys, std::map<std::string, std::vector<util::SettingsValue>>* settings_target)
 {
     LOCK(cs_args);
     std::vector<std::pair<std::string, std::string>> options;
@@ -815,6 +815,9 @@ bool ArgsManager::ReadConfigStream(std::istream& stream, const std::string& file
             if (!value) {
                 return false;
             }
+            if (settings_target) {
+                (*settings_target)[key.name].push_back(*value);
+            } else
             m_settings.ro_config[key.section][key.name].push_back(*value);
         } else {
             if (ignore_invalid_keys) {
