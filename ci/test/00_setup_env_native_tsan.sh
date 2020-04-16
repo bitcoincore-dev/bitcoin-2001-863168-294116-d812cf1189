4a8e07913855da676a0eb13a4dc13d4b9cb77cfb
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-04-16 22:05:41 +0300

    build: Check libevent minimum version

--- ci/test/00_setup_env_native_tsan.sh
+++ ci/test/00_setup_env_native_tsan.sh
@@ -7,7 +7,6 @@
 export LC_ALL=C.UTF-8
 
 export CONTAINER_NAME=ci_native_tsan
-export DOCKER_NAME_TAG=ubuntu:16.04
 export PACKAGES="clang llvm python3-zmq qtbase5-dev qttools5-dev-tools libevent-dev bsdmainutils libboost-system-dev libboost-filesystem-dev libboost-test-dev libboost-thread-dev libdb5.3++-dev libminiupnpc-dev libzmq3-dev libqrencode-dev"
 export NO_DEPENDS=1
 export GOAL="install"
