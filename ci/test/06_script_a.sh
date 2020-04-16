4f29a488f19a5eb7ba21f4a9cce5a5fe9a4d8d3d
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-04-16 17:09:40 +0300

    ci: Limit cache size regardless of NO_DEPENDS

--- ci/test/06_script_a.sh
+++ ci/test/06_script_a.sh
@@ -8,9 +8,7 @@ export LC_ALL=C.UTF-8
 
 BITCOIN_CONFIG_ALL="--disable-dependency-tracking --prefix=$DEPENDS_DIR/$HOST --bindir=$BASE_OUTDIR/bin --libdir=$BASE_OUTDIR/lib"
 DOCKER_EXEC "command -v ccache > /dev/null && ccache --zero-stats"
-if [ -z "$NO_DEPENDS" ]; then
-  DOCKER_EXEC ccache --max-size=$CCACHE_SIZE
-fi
+DOCKER_EXEC "command -v ccache > /dev/null && ccache --max-size=$CCACHE_SIZE"
 
 BEGIN_FOLD autogen
 if [ -n "$CONFIG_SHELL" ]; then
