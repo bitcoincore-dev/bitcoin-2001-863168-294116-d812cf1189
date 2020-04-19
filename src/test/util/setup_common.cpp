44ca3a097db660010166646ba70794eba5bddcb4
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-04-20 00:32:13 +0300

    Add local thread pool to CCheckQueue

--- src/test/util/setup_common.cpp
+++ src/test/util/setup_common.cpp
@@ -152,10 +152,7 @@ TestingSetup::TestingSetup(const std::string& chainName, const std::vector<const
 
     // Start script-checking threads. Set g_parallel_script_checks to true so they are used.
     constexpr int script_check_threads = 2;
-    for (int i = 0; i < script_check_threads; ++i) {
-        threadGroup.create_thread([i]() { return ThreadScriptCheck(i); });
-    }
-    g_parallel_script_checks = true;
+    CreateScriptCheckWorkerThreads(script_check_threads);
 
     m_node.mempool = &::mempool;
     m_node.mempool->setSanityCheck(1.0);
