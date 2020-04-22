ba61343f43f8d91c160d698b3db11fc9d34e81eb
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-04-22 03:17:19 +0300

    refactor: Drop g_parallel_script_checks global

--- src/init.cpp
+++ src/init.cpp
@@ -1281,9 +1281,6 @@ bool AppInitMain(NodeContext& node)
     script_threads = std::min(script_threads, MAX_SCRIPTCHECK_THREADS);
 
     LogPrintf("Script verification uses %d additional threads\n", script_threads);
-    if (script_threads >= 1) {
-        g_parallel_script_checks = true;
-    }
 
     assert(!node.scheduler);
     node.scheduler = MakeUnique<CScheduler>();
