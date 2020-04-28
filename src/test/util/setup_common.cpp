3294ae652edc8d37adb0221722e2f1f34facadea
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-04-28 21:05:00 +0300

    refactor: Make CCheckQueue RAII-styled
    Also this commits gets rid of scriptcheckqueue global variable.

--- src/test/util/setup_common.cpp
+++ src/test/util/setup_common.cpp
@@ -137,8 +137,11 @@ TestingSetup::TestingSetup(const std::string& chainName, const std::vector<const
 
     pblocktree.reset(new CBlockTreeDB(1 << 20, true));
 
+    // Start script-checking threads. Set g_parallel_script_checks to true so they are used.
+    constexpr int script_check_threads = 2;
+    g_parallel_script_checks = true;
     m_node.chainman = &::g_chainman;
-    m_node.chainman->InitializeChainstate();
+    m_node.chainman->InitializeChainstate(uint256(), script_check_threads);
     ::ChainstateActive().InitCoinsDB(
         /* cache_size_bytes */ 1 << 23, /* in_memory */ true, /* should_wipe */ false);
     assert(!::ChainstateActive().CanFlushToDisk());
@@ -152,11 +155,6 @@ TestingSetup::TestingSetup(const std::string& chainName, const std::vector<const
     if (!ActivateBestChain(state, chainparams)) {
         throw std::runtime_error(strprintf("ActivateBestChain failed. (%s)", state.ToString()));
     }
-
-    // Start script-checking threads. Set g_parallel_script_checks to true so they are used.
-    constexpr int script_check_threads = 2;
-    StartScriptCheckWorkerThreads(script_check_threads);
-    g_parallel_script_checks = true;
 
     m_node.mempool = &::mempool;
     m_node.mempool->setSanityCheck(1.0);
@@ -175,7 +173,6 @@ TestingSetup::~TestingSetup()
     if (m_node.scheduler) m_node.scheduler->stop();
     threadGroup.interrupt_all();
     threadGroup.join_all();
-    StopScriptCheckWorkerThreads();
     GetMainSignals().FlushBackgroundCallbacks();
     GetMainSignals().UnregisterBackgroundSignalScheduler();
     m_node.connman.reset();
