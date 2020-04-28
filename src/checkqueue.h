6ed21ecfad58e19c6e1cd5867ec2c38a73aa2931
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-04-28 21:05:00 +0300

    refactor: Make CCheckQueue RAII-styled
    Also this commits gets rid of scriptcheckqueue global variable.

--- src/checkqueue.h
+++ src/checkqueue.h
@@ -84,6 +84,9 @@ private:
                         m_master_cv.notify_one();
                 } else {
                     // first iteration
+                    if (!fMaster) {
+                        util::ThreadRename(strprintf("scriptch.%i", nTotal));
+                    }
                     nTotal++;
                 }
                 // logically, the do loop starts here
@@ -133,26 +136,12 @@ public:
     Mutex m_control_mutex;
 
     //! Create a new check queue
-    explicit CCheckQueue(unsigned int nBatchSizeIn)
-        : nBatchSize(nBatchSizeIn)
+    explicit CCheckQueue(unsigned int nBatchSizeIn, const int threads_num = 0)
+        : nBatchSize(nBatchSizeIn), m_worker_threads(threads_num)
     {
-    }
-
-    //! Create a pool of new worker threads.
-    void StartWorkerThreads(const int threads_num)
-    {
-        {
-            LOCK(m_mutex);
-            nIdle = 0;
-            nTotal = 0;
-            fAllOk = true;
-        }
-        assert(m_worker_threads.empty());
-        for (int n = 0; n < threads_num; ++n) {
-            m_worker_threads.emplace_back([this, n]() {
-                util::ThreadRename(strprintf("scriptch.%i", n));
-                Loop(false /* worker thread */);
-            });
+        auto worker = [this]() { Loop(false /* worker thread */); };
+        for (std::thread& t : m_worker_threads) {
+            std::thread(worker).swap(t);
         }
     }
 
@@ -177,23 +166,14 @@ public:
             m_worker_cv.notify_all();
     }
 
-    //! Stop all of the worker threads.
-    void StopWorkerThreads()
+    ~CCheckQueue()
     {
         WITH_LOCK(m_mutex, m_request_stop = true);
         m_worker_cv.notify_all();
         for (std::thread& t : m_worker_threads) {
             t.join();
         }
-        m_worker_threads.clear();
-        WITH_LOCK(m_mutex, m_request_stop = false);
     }
-
-    ~CCheckQueue()
-    {
-        assert(m_worker_threads.empty());
-    }
-
 };
 
 /**
