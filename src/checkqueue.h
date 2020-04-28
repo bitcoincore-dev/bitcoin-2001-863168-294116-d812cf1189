dd3ce66f2824b93b93ad73c01b8c098dd57623ad
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-04-28 21:05:00 +0300

    refactor: Make CCheckQueue RAII-styled
    Also this commits gets rid of scriptcheckqueue global variable.

--- src/checkqueue.h
+++ src/checkqueue.h
@@ -10,6 +10,7 @@
 #include <util/threadnames.h>
 
 #include <algorithm>
+#include <atomic>
 #include <vector>
 
 template <typename T>
@@ -62,7 +63,7 @@ private:
     const unsigned int nBatchSize;
 
     std::vector<std::thread> m_worker_threads;
-    bool m_request_stop GUARDED_BY(m_mutex){false};
+    std::atomic<bool> m_request_stop{false};
 
     /** Internal function that does bulk of the verification work. */
     bool Loop(bool fMaster)
@@ -84,6 +85,9 @@ private:
                         m_master_cv.notify_one();
                 } else {
                     // first iteration
+                    if (!fMaster) {
+                        util::ThreadRename(strprintf("scriptch.%i", nTotal));
+                    }
                     nTotal++;
                 }
                 // logically, the do loop starts here
@@ -133,26 +137,12 @@ public:
     Mutex m_control_mutex;
 
     //! Create a new check queue
-    explicit CCheckQueue(unsigned int nBatchSizeIn)
-        : nBatchSize(nBatchSizeIn)
+    explicit CCheckQueue(unsigned int nBatchSizeIn, const int threads_num = 0)
+        : nBatchSize(nBatchSizeIn), m_worker_threads(threads_num)
     {
-    }
-
-    //! Create a pool of new worker threads.
-    void StartWorkerThreads(const int threads_num)
-    {
-        {
-            LOCK(m_mutex);
-            nIdle = 0;
-            nTotal = 0;
-            fAllOk = true;
-        }
-        assert(m_worker_threads.empty());
-        for (int n = 0; n < threads_num; ++n) {
-            m_worker_threads.emplace_back([this, n]() {
-                util::ThreadRename(strprintf("scriptch.%i", n));
-                Loop(false /* worker thread */);
-            });
+        auto worker = [this]() { Loop(false /* worker thread */); };
+        for (std::thread& t : m_worker_threads) {
+            std::thread(worker).swap(t);
         }
     }
 
@@ -177,23 +167,14 @@ public:
             m_worker_cv.notify_all();
     }
 
-    //! Stop all of the worker threads.
-    void StopWorkerThreads()
+    ~CCheckQueue()
     {
-        WITH_LOCK(m_mutex, m_request_stop = true);
+        m_request_stop = true;
         m_worker_cv.notify_all();
         for (std::thread& t : m_worker_threads) {
             t.join();
         }
-        m_worker_threads.clear();
-        WITH_LOCK(m_mutex, m_request_stop = false);
     }
-
-    ~CCheckQueue()
-    {
-        assert(m_worker_threads.empty());
-    }
-
 };
 
 /**
