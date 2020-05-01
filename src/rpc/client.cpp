05d8ff88773ac15c6a94307fe9618c26e5e77688
pierrenn <git@pnn.sh>
2020-05-01 14:07:21 +0900

    rpc: allow dumptxoutset to dump human-readable data
    Github-Pull: #18689
    Rebased-From: cd20cb886deb0ef91ab89c66bbfb511e89eb77ee

--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -75,6 +75,8 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "sendmany", 6 , "conf_target" },
     { "sendmany", 8, "verbose" },
     { "deriveaddresses", 1, "range" },
+    { "dumptxoutset", 1, "format" },
+    { "dumptxoutset", 2, "show_header" },
     { "scantxoutset", 1, "scanobjects" },
     { "addmultisigaddress", 0, "nrequired" },
     { "addmultisigaddress", 1, "keys" },
