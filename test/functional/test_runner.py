e5261506056ab4713bb53850b0966cb4c849904e
Harris <brakmic@gmail.com>
2020-05-04 20:33:52 +0200

    RPC: Add getrpcwhitelist method

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -136,6 +136,7 @@ BASE_SCRIPTS = [
     'rpc_psbt.py',
     'rpc_users.py',
     'rpc_whitelist.py',
+    'rpc_getrpcwhitelist.py',
     'feature_proxy.py',
     'rpc_signrawtransaction.py',
     'wallet_groups.py',
