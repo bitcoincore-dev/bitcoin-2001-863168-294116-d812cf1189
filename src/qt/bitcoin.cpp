be25c4b9279815e18a029de3e89d0f8cac099ab5
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-06-01 15:56:11 +0300

    qt: Use QVariant instead of int for BitcoinUnit in QSettings
    This change improves type safety.

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -81,6 +81,8 @@ static void RegisterMetaTypes()
 
     qRegisterMetaType<std::function<void()>>("std::function<void()>");
     qRegisterMetaType<QMessageBox::Icon>("QMessageBox::Icon");
+
+    qRegisterMetaTypeStreamOperators<BitcoinUnit>("BitcoinUnit");
 }
 
 static QString GetLangTerritory()
