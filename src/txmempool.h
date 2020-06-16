e54905aea0c60b209f17bb7e55dfbb2257b19aa3
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-06-16 17:32:26 +0300

    refactor: Prevent double lock in CTxMemPool::infoAll()

--- src/txmempool.h
+++ src/txmempool.h
@@ -752,7 +752,7 @@ public:
     }
 
     TxMempoolInfo info(const uint256& hash) const EXCLUSIVE_LOCKS_REQUIRED(!cs);
-    std::vector<TxMempoolInfo> infoAll() const;
+    std::vector<TxMempoolInfo> infoAll() const EXCLUSIVE_LOCKS_REQUIRED(cs);
 
     size_t DynamicMemoryUsage() const;
 
