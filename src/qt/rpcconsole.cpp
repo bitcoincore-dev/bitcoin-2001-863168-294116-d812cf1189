ab070cf39a6756723a4efc17e6e98de9d47425d5
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-07-01 20:21:29 +0300

    qt: Rework RPCConsole::clearSelectedNode()
    Only successfully disconnected peers are deselected from the selection.

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -647,7 +647,7 @@ void RPCConsole::setClientModel(ClientModel *model, int bestblock_height, int64_
         connect(unbanAction, &QAction::triggered, this, &RPCConsole::unbanSelectedNode);
 
         // ban table signal handling - clear peer details when clicking a peer in the ban table
-        connect(ui->banlistWidget, &QTableView::clicked, this, &RPCConsole::clearSelectedNode);
+        connect(ui->banlistWidget, &QTableView::clicked, ui->peerWidget->selectionModel(), &QItemSelectionModel::clearSelection);
         // ban table signal handling - ensure ban table is shown or hidden (if empty)
         connect(model->getBanTableModel(), &BanTableModel::layoutChanged, this, &RPCConsole::showOrHideBanTableIfRequired);
         showOrHideBanTableIfRequired();
@@ -1124,8 +1124,9 @@ void RPCConsole::disconnectSelectedNode()
         // Get currently selected peer address
         NodeId id = nodes.at(i).data().toLongLong();
         // Find the node, disconnect it and clear the selected node
-        if(m_node.disconnectById(id))
-            clearSelectedNode();
+        if (m_node.disconnectById(id)) {
+            clearSelectedNode(nodes.at(i));
+        }
     }
 }
 
@@ -1149,10 +1150,11 @@ void RPCConsole::banSelectedNode(int bantime)
         const CNodeCombinedStats *stats = clientModel->getPeerTableModel()->getNodeStats(detailNodeRow);
         if (stats) {
             m_node.ban(stats->nodeStats.addr, bantime);
-            m_node.disconnectByAddress(stats->nodeStats.addr);
+            if (m_node.disconnectByAddress(stats->nodeStats.addr)) {
+                clearSelectedNode(nodes.at(i));
+            }
         }
     }
-    clearSelectedNode();
     clientModel->getBanTableModel()->refresh();
 }
 
@@ -1177,11 +1179,10 @@ void RPCConsole::unbanSelectedNode()
     }
 }
 
-void RPCConsole::clearSelectedNode()
+void RPCConsole::clearSelectedNode(const QModelIndex& peer)
 {
-    ui->peerWidget->selectionModel()->clearSelection();
-    cachedNodeids.clear();
-    updateDetailWidget();
+    auto selection = ui->peerWidget->selectionModel();
+    selection->select(peer, QItemSelectionModel::Deselect | QItemSelectionModel::Rows);
 }
 
 void RPCConsole::showOrHideBanTableIfRequired()
