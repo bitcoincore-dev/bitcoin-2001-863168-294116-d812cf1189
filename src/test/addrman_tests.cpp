bitcoin/bitcoin

refactor: Prevent double lock in CAddrMan::Attempt()

src/test/addrman_tests.cpp
@@ -79,7 +79,7 @@ public:
 
          bool count_failure = false;
          int64_t nLastTry = GetAdjustedTime()-61;
-         Attempt(addr, count_failure, nLastTry);
+         Attempt_(addr, count_failure, nLastTry);
      }
 
     void Clear()

bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

src/test/addrman_tests.cpp
@@ -79,7 +79,7 @@ public:
 
          bool count_failure = false;
          int64_t nLastTry = GetAdjustedTime()-61;
-         Attempt(addr, count_failure, nLastTry);
+         Attempt_cs(addr, count_failure, nLastTry);
      }
 
     void Clear()