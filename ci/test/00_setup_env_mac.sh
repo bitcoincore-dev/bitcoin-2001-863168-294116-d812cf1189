187c9e987653a8ce641e402b8b157b0c80d9a11d
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-08-23 15:12:03 +0300

    build, doc: Correct and complete zlib info and usage
    Use correct zlib1g-dev header instead of libz-dev.
    Make explicit dependency of system zlib for building depends.

--- ci/test/00_setup_env_mac.sh
+++ ci/test/00_setup_env_mac.sh
@@ -8,7 +8,7 @@ export LC_ALL=C.UTF-8
 
 export CONTAINER_NAME=ci_macos_cross
 export HOST=x86_64-apple-darwin16
-export PACKAGES="cmake imagemagick libcap-dev librsvg2-bin libz-dev libbz2-dev libtiff-tools python3-dev python3-setuptools"
+export PACKAGES="cmake imagemagick libcap-dev librsvg2-bin zlib1g-dev libbz2-dev libtiff-tools python3-dev python3-setuptools"
 export XCODE_VERSION=11.3.1
 export XCODE_BUILD_ID=11C505
 export RUN_UNIT_TESTS=false
