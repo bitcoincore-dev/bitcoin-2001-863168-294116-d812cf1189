f7b93e0e2ff93d161d3744635a739f5d6a28106e
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-09-12 18:03:06 +0300

    p2p: Add CConnman::GetCurrentBlockRelayOnlyConns()

--- src/net.h
+++ src/net.h
@@ -312,6 +312,7 @@ public:
      * call the function without a parameter to avoid using the cache.
      */
     std::vector<CAddress> GetAddresses(Network requestor_network, size_t max_addresses, size_t max_pct);
+    std::vector<CAddress> GetCurrentBlockRelayOnlyConns() const;
 
     // This allows temporarily exceeding m_max_outbound_full_relay, with the goal of finding
     // a peer that is better than all our current peers.
