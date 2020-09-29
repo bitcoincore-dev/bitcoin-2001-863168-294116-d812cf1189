7965f8d9f024f16a222d00524c8f4f9fc16f9dc4
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-09-29 18:03:43 +0300

    net: Extend -bind config option with optional network type

--- test/lint/lint-circular-dependencies.sh
+++ test/lint/lint-circular-dependencies.sh
@@ -21,6 +21,7 @@ EXPECTED_CIRCULAR_DEPENDENCIES=(
     "wallet/fees -> wallet/wallet -> wallet/fees"
     "wallet/wallet -> wallet/walletdb -> wallet/wallet"
     "policy/fees -> txmempool -> validation -> policy/fees"
+    "net -> torcontrol -> net"
 )
 
 EXIT_CODE=0
