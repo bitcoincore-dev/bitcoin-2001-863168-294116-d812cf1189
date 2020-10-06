f8c6ca641c672f81d9fee333e081dd167eb18f36
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-10-06 19:47:26 +0300

    wallet, log, qa: Unify wallet directory lock error message
    This change removes redundant LogPrintf call, and unifies error message
    wording with the similar datadir lock error message.

--- test/functional/wallet_multiwallet.py
+++ test/functional/wallet_multiwallet.py
@@ -155,11 +155,12 @@ class MultiWalletTest(BitcoinTestFramework):
         w5_info = w5.getwalletinfo()
         assert_equal(w5_info['immature_balance'], 50)
 
-        competing_wallet_dir = os.path.join(self.options.tmpdir, 'competing_walletdir')
-        os.mkdir(competing_wallet_dir)
-        self.restart_node(0, ['-walletdir=' + competing_wallet_dir])
-        exp_stderr = r"Error: Error initializing wallet database environment \"\S+competing_walletdir\S*\"!"
-        self.nodes[1].assert_start_raises_init_error(['-walletdir=' + competing_wallet_dir], exp_stderr, match=ErrorMatch.PARTIAL_REGEX)
+        if not self.options.descriptors:
+            competing_wallet_dir = os.path.join(self.options.tmpdir, 'competing_walletdir')
+            os.mkdir(competing_wallet_dir)
+            self.restart_node(0, ['-walletdir=' + competing_wallet_dir])
+            exp_stderr = "Error: Cannot obtain a lock on wallet directory {0}. {1} is probably already running.".format(competing_wallet_dir, self.config['environment']['PACKAGE_NAME'])
+            self.nodes[1].assert_start_raises_init_error(['-walletdir=' + competing_wallet_dir], exp_stderr)
 
         self.restart_node(0, extra_args)
 
@@ -361,7 +362,8 @@ class MultiWalletTest(BitcoinTestFramework):
         if self.options.descriptors:
             assert_raises_rpc_error(-4, "Unable to obtain an exclusive lock", self.nodes[1].loadwallet, wallet)
         else:
-            assert_raises_rpc_error(-4, "Error initializing wallet database environment", self.nodes[1].loadwallet, wallet)
+            expected_rpc_error = "Cannot obtain a lock on wallet directory {0}. {1} is probably already running.".format(wallet, self.config['environment']['PACKAGE_NAME'])
+            assert_raises_rpc_error(-4, expected_rpc_error, self.nodes[1].loadwallet, wallet)
         self.nodes[0].unloadwallet(wallet)
         self.nodes[1].loadwallet(wallet)
 
