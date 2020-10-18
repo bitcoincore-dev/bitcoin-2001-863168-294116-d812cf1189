0179abea02576b4e4291d36d14a2915d3d3c477d
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-10-19 00:36:22 +0300

    ci: Enable -Werror for Travis builds, and document exceptions

--- ci/test/00_setup_env_native_nowallet.sh
+++ ci/test/00_setup_env_native_nowallet.sh
@@ -12,3 +12,7 @@ export PACKAGES="python3-zmq clang-3.8 llvm-3.8"  # Use clang-3.8 to test C++11 
 export DEP_OPTS="NO_WALLET=1"
 export GOAL="install"
 export BITCOIN_CONFIG="--enable-glibc-back-compat --enable-reduce-exports CC=clang-3.8 CXX=clang++-3.8 --with-boost-process"
+
+# clang < 6.0 emits the -Wmissing-braces warning when initializing an std::array with only one pair of braces:
+#  - https://bugs.llvm.org/show_bug.cgi?id=21629
+export NO_WERROR=1
