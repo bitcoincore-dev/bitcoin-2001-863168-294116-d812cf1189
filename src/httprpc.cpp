4ebd67ccc17151cf9caa2aff9877e66d9ba5f200
João Barbosa <joao.paulo.barbosa@gmail.com>
2020-11-16 23:53:39 +0000

    rpc: Support -rpcauthfile argument
    This argument is similar to -rpcauth but takes the value from the
    specified file content

--- src/httprpc.cpp
+++ src/httprpc.cpp
@@ -14,6 +14,7 @@
 #include <walletinitinterface.h>
 
 #include <algorithm>
+#include <fstream>
 #include <iterator>
 #include <map>
 #include <memory>
@@ -250,7 +251,7 @@ static bool InitRPCAuthentication()
         LogPrintf("Config options rpcuser and rpcpassword will soon be deprecated. Locally-run instances may remove rpcuser to use cookie-based auth, or may be replaced with rpcauth. Please see share/rpcauth for rpcauth auth generation.\n");
         strRPCUserColonPass = gArgs.GetArg("-rpcuser", "") + ":" + gArgs.GetArg("-rpcpassword", "");
     }
-    if (gArgs.GetArg("-rpcauth", "") != "") {
+    if (gArgs.GetArg("-rpcauth", "") != "" || gArgs.GetArg("-rpcauthfile", "") != "") {
         LogPrintf("Using rpcauth authentication.\n");
         for (const std::string& rpcauth : gArgs.GetArgs("-rpcauth")) {
             std::vector<std::string> fields{SplitString(rpcauth, ':')};
@@ -262,6 +263,20 @@ static bool InitRPCAuthentication()
             } else {
                 LogPrintf("Invalid -rpcauth argument.\n");
                 return false;
+            }
+        }
+        for (std::string path : gArgs.GetArgs("-rpcauthfile")) {
+            std::ifstream file;
+            file.open(path);
+            if (!file.is_open()) continue;
+            std::string rpcauth;
+            std::getline(file, rpcauth);
+            std::vector<std::string> fields{SplitString(rpcauth, ':')};
+            const std::vector<std::string> salt_hmac{SplitString(fields.back(), '$')};
+            if (fields.size() == 2 && salt_hmac.size() == 2) {
+                fields.pop_back();
+                fields.insert(fields.end(), salt_hmac.begin(), salt_hmac.end());
+                g_rpcauth.push_back(fields);
             }
         }
     }
