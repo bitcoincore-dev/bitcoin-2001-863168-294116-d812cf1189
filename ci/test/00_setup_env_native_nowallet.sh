e5f3d517ccf6d9b47a2631235c1b3a8f6feb4385
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-11-26 19:01:19 +0200

    ci: Enable -Werror, and document exceptions

--- ci/test/00_setup_env_native_nowallet.sh
+++ ci/test/00_setup_env_native_nowallet.sh
@@ -12,3 +12,7 @@ export PACKAGES="python3-zmq clang-5.0 llvm-5.0"  # Use clang-5 to test C++17 co
 export DEP_OPTS="NO_WALLET=1"
 export GOAL="install"
 export BITCOIN_CONFIG="--enable-glibc-back-compat --enable-reduce-exports CC=clang-5.0 CXX=clang++-5.0 --with-boost-process"
+
+# clang < 6.0 emits the -Wmissing-braces warning when initializing an std::array with only one pair of braces:
+#  - https://bugs.llvm.org/show_bug.cgi?id=21629
+export NO_WERROR=1
--- ci/test/06_script_a.sh
+++ ci/test/06_script_a.sh
@@ -7,6 +7,9 @@
 export LC_ALL=C.UTF-8
 
 BITCOIN_CONFIG_ALL="--enable-suppress-external-warnings --disable-dependency-tracking --prefix=$DEPENDS_DIR/$HOST --bindir=$BASE_OUTDIR/bin --libdir=$BASE_OUTDIR/lib"
+if [ -z "$NO_WERROR" ]; then
+  BITCOIN_CONFIG_ALL="${BITCOIN_CONFIG_ALL} --enable-werror"
+fi
 DOCKER_EXEC "ccache --zero-stats --max-size=$CCACHE_SIZE"
 
 BEGIN_FOLD autogen
