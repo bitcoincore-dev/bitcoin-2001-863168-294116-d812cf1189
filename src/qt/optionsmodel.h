77edf1bd0149bd2625dff563f3bd4032a12a2233
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2021-01-21 22:27:28 +0200

    qt: Use QVariant instead of int for BitcoinUnit in QSettings
    This change improves type safety.

--- src/qt/optionsmodel.h
+++ src/qt/optionsmodel.h
@@ -6,12 +6,13 @@
 #define BITCOIN_QT_OPTIONSMODEL_H
 
 #include <amount.h>
-#include <cstdint>
+#include <qt/bitcoinunits.h>
 #include <qt/guiconstants.h>
 
 #include <QAbstractListModel>
 
 #include <assert.h>
+#include <cstdint>
 
 namespace interfaces {
 class Node;
@@ -82,7 +83,7 @@ public:
     bool getShowTrayIcon() const { return m_show_tray_icon; }
     bool getMinimizeToTray() const { return fMinimizeToTray; }
     bool getMinimizeOnClose() const { return fMinimizeOnClose; }
-    int getDisplayUnit() const { return nDisplayUnit; }
+    int getDisplayUnit() const { return static_cast<int>(m_display_unit); }
     QString getThirdPartyTxUrls() const { return strThirdPartyTxUrls; }
     bool getCoinControlFeatures() const { return fCoinControlFeatures; }
     const QString& getOverriddenByCommandLine() { return strOverriddenByCommandLine; }
@@ -105,7 +106,7 @@ private:
     bool fMinimizeToTray;
     bool fMinimizeOnClose;
     QString language;
-    int nDisplayUnit;
+    BitcoinUnit m_display_unit;
     QString strThirdPartyTxUrls;
     bool fCoinControlFeatures;
     /* settings that were overridden by command-line */
