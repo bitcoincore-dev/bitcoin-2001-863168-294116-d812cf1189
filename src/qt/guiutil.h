fff736a7f1ebcf8cb5950ca6b19a29c5a9f95011
Jon Atack <jon@atack.com>
2021-01-30 18:53:40 +0100

    gui: display plain "Inbound" in peer details
    Github-Pull: gui#203
    Rebased-From: 506e6585a54818d0613a067f91c0bac2f308a48c

--- src/qt/guiutil.h
+++ src/qt/guiutil.h
@@ -226,7 +226,7 @@ namespace GUIUtil
     QString boostPathToQString(const fs::path &path);
 
     /** Convert enum ConnectionType to QString */
-    QString ConnectionTypeToQString(ConnectionType conn_type, bool relay_txes);
+    QString ConnectionTypeToQString(ConnectionType conn_type);
 
     /* Convert seconds into a QString with days, hours, mins, secs */
     QString formatDurationStr(int secs);
