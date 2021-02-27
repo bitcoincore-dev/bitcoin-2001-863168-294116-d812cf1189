d916ceb6b7e99d2078c3018d522e0d7ca33277b8
Luke Dashjr <luke-jr+git@utopios.org>
2021-02-27 16:45:22 +0000

    interfaces/Wallet: Add the ability to choose a backup format
    Also outputs a bilingual_str error (unused for now)

--- src/interfaces/wallet.h
+++ src/interfaces/wallet.h
@@ -41,6 +41,10 @@ struct WalletContext;
 using isminefilter = std::underlying_type<isminetype>::type;
 } // namespace wallet
 
+enum class WalletBackupFormat {
+    Raw,   // Literal db copy
+};
+
 namespace interfaces {
 
 class Handler;
@@ -82,7 +86,7 @@ public:
     virtual void abortRescan() = 0;
 
     //! Back up wallet.
-    virtual bool backupWallet(const std::string& filename) = 0;
+    virtual bool backupWallet(const std::string& filename, const WalletBackupFormat format, bilingual_str& error) = 0;
 
     //! Get wallet name.
     virtual std::string getWalletName() = 0;
