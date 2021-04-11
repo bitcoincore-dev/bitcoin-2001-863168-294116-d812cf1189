712d9b4089eba9b103e67ceed0f65f3861a57cc5
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2021-04-11 15:42:00 +0300

    build: Makes rcc output always deterministic
    The Qt Resource Compiler (rcc) has a command-line option
    `--format-version` which has the default value 2.
    
    The only difference from `--format-version 1` is adding a last modified
    timestamp to the output file. That, in turn, forces us to use
    `QT_RCC_SOURCE_DATE_OVERRIDE=1` to get deterministic builds.
    
    This change makes rcc output always deterministic by using
    `--format-version 1` option that makes usage of the
    `QT_RCC_SOURCE_DATE_OVERRIDE` needless. Also it improves interaction
    with ccache.
    
    Co-authored-by: fanquake <fanquake@gmail.com>

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -174,7 +174,6 @@ $(package)_config_opts_x86_64_android += -android-arch x86_64
 $(package)_config_opts_i686_android += -android-arch i686
 
 $(package)_build_env  = QT_RCC_TEST=1
-$(package)_build_env += QT_RCC_SOURCE_DATE_OVERRIDE=1
 endef
 
 define $(package)_fetch_cmds
