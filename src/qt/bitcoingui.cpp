0ad634ff878bde4d073bb2b2919b492ccfc40e04
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2021-05-16 19:48:58 +0300

    qt, refactor: Keep OptionsDialog in the main event loop

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -952,10 +952,11 @@ void BitcoinGUI::openOptionsDialogWithTab(OptionsDialog::Tab tab)
     if (!clientModel || !clientModel->getOptionsModel())
         return;
 
-    OptionsDialog dlg(this, enableWallet);
-    dlg.setCurrentTab(tab);
-    dlg.setModel(clientModel->getOptionsModel());
-    dlg.exec();
+    auto dlg = new OptionsDialog(this, enableWallet);
+    dlg->setCurrentTab(tab);
+    dlg->setModel(clientModel->getOptionsModel());
+    dlg->setAttribute(Qt::WA_DeleteOnClose);
+    dlg->open();
 }
 
 void BitcoinGUI::setNumBlocks(int count, const QDateTime& blockDate, double nVerificationProgress, bool header, SynchronizationState sync_state)
