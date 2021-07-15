f493c9b4b19eb99ba7e63cf86816a7fed8115ad2
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2021-07-15 14:56:19 +0300

    build: Use specific cross-compiler instead of multilib one for i386

--- depends/hosts/linux.mk
+++ depends/hosts/linux.mk
@@ -10,13 +10,6 @@ linux_debug_CXXFLAGS=$(linux_debug_CFLAGS)
 linux_debug_CPPFLAGS=-D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_LIBCPP_DEBUG=1
 
 ifeq (86,$(findstring 86,$(build_arch)))
-i686_linux_CC=gcc -m32
-i686_linux_CXX=g++ -m32
-i686_linux_AR=ar
-i686_linux_RANLIB=ranlib
-i686_linux_NM=nm
-i686_linux_STRIP=strip
-
 x86_64_linux_CC=gcc -m64
 x86_64_linux_CXX=g++ -m64
 x86_64_linux_AR=ar
