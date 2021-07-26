5f1ed426d576efd0864eb1dc0cd188a52c698e6a
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2021-07-26 20:22:47 +0300

    scripted-diff: Make (package)_X variables simply expanded ones
    -BEGIN VERIFY SCRIPT-
    FILES=$(git ls-files depends/packages)
    sed -i 's/package=/package := /' -- $FILES
    sed -i -e 's/$(package)\(\w\+\)=/$(package)\1 := /' -- $FILES
    -END VERIFY SCRIPT-

--- depends/packages/fontconfig.mk
+++ depends/packages/fontconfig.mk
@@ -1,13 +1,13 @@
-package=fontconfig
-$(package)_version=2.12.1
-$(package)_download_path=https://www.freedesktop.org/software/fontconfig/release/
-$(package)_file_name=$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=b449a3e10c47e1d1c7a6ec6e2016cca73d3bd68fbbd4f0ae5cc6b573f7d6c7f3
-$(package)_dependencies=freetype expat
-$(package)_patches=remove_char_width_usage.patch gperf_header_regen.patch
+package := fontconfig
+$(package)_version := 2.12.1
+$(package)_download_path := https://www.freedesktop.org/software/fontconfig/release/
+$(package)_file_name := $(package)-$($(package)_version).tar.bz2
+$(package)_sha256_hash := b449a3e10c47e1d1c7a6ec6e2016cca73d3bd68fbbd4f0ae5cc6b573f7d6c7f3
+$(package)_dependencies := freetype expat
+$(package)_patches := remove_char_width_usage.patch gperf_header_regen.patch
 
 define $(package)_set_vars
-  $(package)_config_opts=--disable-docs --disable-static --disable-libxml2 --disable-iconv
+  $(package)_config_opts := --disable-docs --disable-static --disable-libxml2 --disable-iconv
   $(package)_config_opts += --disable-dependency-tracking --enable-option-checking
 endef
 
