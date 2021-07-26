4e70c20acdefc9e596297b322e9f2897f9dc4654
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2021-07-26 20:22:47 +0300

    scripted-diff: Make (package)_X variables simply expanded ones
    -BEGIN VERIFY SCRIPT-
    FILES=$(git ls-files depends/packages)
    sed -i 's/package=/package := /' -- $FILES
    sed -i -e 's/$(package)\(\w\+\)=/$(package)\1 := /' -- $FILES
    -END VERIFY SCRIPT-

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -1,32 +1,32 @@
-package=boost
-$(package)_version=$(native_b2_version)
-$(package)_download_path=$(native_b2_download_path)
-$(package)_file_name=$(native_b2_file_name)
-$(package)_sha256_hash=$(native_b2_sha256_hash)
-$(package)_dependencies=native_b2
+package := boost
+$(package)_version := $(native_b2_version)
+$(package)_download_path := $(native_b2_download_path)
+$(package)_file_name := $(native_b2_file_name)
+$(package)_sha256_hash := $(native_b2_sha256_hash)
+$(package)_dependencies := native_b2
 
 define $(package)_set_vars
-$(package)_config_opts_release=variant=release
-$(package)_config_opts_debug=variant=debug
-$(package)_config_opts=--layout=tagged --build-type=complete --user-config=user-config.jam
+$(package)_config_opts_release := variant=release
+$(package)_config_opts_debug := variant=debug
+$(package)_config_opts := --layout=tagged --build-type=complete --user-config=user-config.jam
 $(package)_config_opts+=threading=multi link=static -sNO_COMPRESSION=1
-$(package)_config_opts_linux=target-os=linux threadapi=pthread runtime-link=shared
-$(package)_config_opts_darwin=target-os=darwin runtime-link=shared
-$(package)_config_opts_mingw32=target-os=windows binary-format=pe threadapi=win32 runtime-link=static
-$(package)_config_opts_x86_64=architecture=x86 address-model=64
-$(package)_config_opts_i686=architecture=x86 address-model=32
-$(package)_config_opts_aarch64=address-model=64
-$(package)_config_opts_armv7a=address-model=32
+$(package)_config_opts_linux := target-os=linux threadapi=pthread runtime-link=shared
+$(package)_config_opts_darwin := target-os=darwin runtime-link=shared
+$(package)_config_opts_mingw32 := target-os=windows binary-format=pe threadapi=win32 runtime-link=static
+$(package)_config_opts_x86_64 := architecture=x86 address-model=64
+$(package)_config_opts_i686 := architecture=x86 address-model=32
+$(package)_config_opts_aarch64 := address-model=64
+$(package)_config_opts_armv7a := address-model=32
 ifneq (,$(findstring clang,$($(package)_cxx)))
 $(package)_toolset_$(host_os)=clang
 else
 $(package)_toolset_$(host_os)=gcc
 endif
-$(package)_config_libraries=filesystem,system,test
-$(package)_cxxflags=-std=c++17 -fvisibility=hidden
-$(package)_cxxflags_linux=-fPIC
-$(package)_cxxflags_android=-fPIC
-$(package)_cxxflags_x86_64_darwin=-fcf-protection=full
+$(package)_config_libraries := filesystem,system,test
+$(package)_cxxflags := -std=c++17 -fvisibility=hidden
+$(package)_cxxflags_linux := -fPIC
+$(package)_cxxflags_android := -fPIC
+$(package)_cxxflags_x86_64_darwin := -fcf-protection=full
 endef
 
 define $(package)_preprocess_cmds
