ae66248dfd8b17614c3d88d81e9baa2c19bf6adf
Carl Dong <contact@carldong.me>
2021-08-18 13:54:08 -0400

    init/chainstate: Reduce coupling of LogPrintf
    ...by moving the try/catch out of LoadChainstate
    
    I strongly recommend reviewing with the following git-diff flags:
      --color-moved=dimmed_zebra --color-moved-ws=allow-indentation-change

--- src/init.cpp
+++ src/init.cpp
@@ -1400,28 +1400,40 @@ bool AppInitMain(NodeContext& node, interfaces::BlockAndHeaderTipInfo* tip_info)
 
         uiInterface.InitMessage(_("Loading block index…").translated);
         const int64_t load_block_index_start_time = GetTimeMillis();
-        auto rv = LoadChainstate(fReset,
-                                 chainman,
-                                 Assert(node.mempool.get()),
-                                 fPruneMode,
-                                 chainparams,
-                                 fReindexChainState,
-                                 nBlockTreeDBCache,
-                                 nCoinDBCache,
-                                 nCoinCacheUsage,
-                                 []() {
-                                     uiInterface.ThreadSafeMessageBox(
-                                         _("Error reading from database, shutting down."),
-                                         "", CClientUIInterface::MSG_ERROR);
-                                 });
+        std::optional<ChainstateLoadingError> rv;
+        try {
+            rv = LoadChainstate(fReset,
+                                chainman,
+                                Assert(node.mempool.get()),
+                                fPruneMode,
+                                chainparams,
+                                fReindexChainState,
+                                nBlockTreeDBCache,
+                                nCoinDBCache,
+                                nCoinCacheUsage,
+                                []() {
+                                    uiInterface.ThreadSafeMessageBox(
+                                        _("Error reading from database, shutting down."),
+                                        "", CClientUIInterface::MSG_ERROR);
+                                });
+        } catch (const std::exception& e) {
+            LogPrintf("%s\n", e.what());
+            rv = ChainstateLoadingError::ERROR_GENERIC_BLOCKDB_OPEN_FAILED;
+        }
 
-        uiInterface.InitMessage(_("Verifying blocks…").translated);
-        auto rv2 = VerifyLoadedChainstate(chainman,
-                                          fReset,
-                                          fReindexChainState,
-                                          chainparams,
-                                          args.GetIntArg("-checkblocks", DEFAULT_CHECKBLOCKS),
-                                          args.GetIntArg("-checklevel", DEFAULT_CHECKLEVEL));
+        std::optional<ChainstateLoadVerifyError> rv2;
+        try {
+            uiInterface.InitMessage(_("Verifying blocks…").translated);
+            rv2 = VerifyLoadedChainstate(chainman,
+                                         fReset,
+                                         fReindexChainState,
+                                         chainparams,
+                                         args.GetIntArg("-checkblocks", DEFAULT_CHECKBLOCKS),
+                                         args.GetIntArg("-checklevel", DEFAULT_CHECKLEVEL));
+        } catch (const std::exception& e) {
+            LogPrintf("%s\n", e.what());
+            rv2 = ChainstateLoadVerifyError::ERROR_GENERIC_FAILURE;
+        }
 
         if (rv.has_value()) {
             switch (rv.value()) {
