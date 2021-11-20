0b56d1ae3b8717bff39be3e8770689e102d9cf0a
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2021-11-20 12:34:47 +0200

    build: Add libglvnd package

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -2,7 +2,7 @@ packages:=boost libevent
 
 qrencode_packages = qrencode
 
-qt_linux_packages := qt expat libxcb xcb_proto libXau xproto freetype fontconfig libxkbcommon libffi wayland
+qt_linux_packages := qt expat libxcb xcb_proto libXau xproto freetype fontconfig libxkbcommon libffi wayland libglvnd
 qt_linux_native_packages := native_expat native_wayland_scanner
 
 qt_android_packages=qt
