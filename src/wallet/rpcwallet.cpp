88f2ec32befcbac1ddaebdaa6eb8895d2257d064
Luke Dashjr <luke-jr+git@utopios.org>
2021-12-07 21:27:38 +0000

    RPC/Wallet: Actually document meaning of "in_mempool" value

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -1388,7 +1388,7 @@ static const std::vector<RPCResult> TransactionDescriptionString()
            {RPCResult::Type::BOOL, "generated", /* optional */ true, "Only present if the transaction's only input is a coinbase one."},
            {RPCResult::Type::BOOL, "trusted", /* optional */ true, "Whether we consider the transaction to be trusted and safe to spend from.\n"
                 "Only present when the transaction has 0 confirmations (or negative confirmations, if conflicted)."},
-           {RPCResult::Type::BOOL, "in_mempool", /* optional */ true, "Only present on unconfirmed transactions."},
+           {RPCResult::Type::BOOL, "in_mempool", /* optional */ true, "True if the transaction is in this node's memory pool. Only present on unconfirmed transactions."},
            {RPCResult::Type::STR_HEX, "blockhash", /* optional */ true, "The block hash containing the transaction."},
            {RPCResult::Type::NUM, "blockheight", /* optional */ true, "The block height containing the transaction."},
            {RPCResult::Type::NUM, "blockindex", /* optional */ true, "The index of the transaction in the block that includes it."},
