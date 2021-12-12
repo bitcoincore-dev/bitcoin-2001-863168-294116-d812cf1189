480b2175fe65bfcc6706e7e1abe1455517108afb
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2021-12-12 14:05:42 +0200

    build, qt: No need to set inapplicable QPA backend for Android

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -169,7 +169,6 @@ $(package)_config_opts_android += -android-ndk $(ANDROID_NDK)
 $(package)_config_opts_android += -android-ndk-platform android-$(ANDROID_API_LEVEL)
 $(package)_config_opts_android += -device-option CROSS_COMPILE="$(host)-"
 $(package)_config_opts_android += -egl
-$(package)_config_opts_android += -qpa xcb
 $(package)_config_opts_android += -no-dbus
 $(package)_config_opts_android += -opengl es2
 $(package)_config_opts_android += -qt-freetype
