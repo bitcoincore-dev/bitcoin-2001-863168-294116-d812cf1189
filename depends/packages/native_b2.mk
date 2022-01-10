e9eb3b57fdda1c2d8c2ad9fba9468f79308bdeca
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-01-10 14:54:44 +0200

    scripted-diff: Make (package)_X variables simply expanded ones
    -BEGIN VERIFY SCRIPT-
    FILES=$(git ls-files depends/packages)
    sed -i 's/package=/package := /' -- $FILES
    sed -i -e 's/$(package)\(\w\+\)=/$(package)\1 := /' -- $FILES
    -END VERIFY SCRIPT-

--- depends/packages/native_b2.mk
+++ depends/packages/native_b2.mk
@@ -1,10 +1,10 @@
-package=native_b2
-$(package)_version=1.71.0
-$(package)_download_path=https://boostorg.jfrog.io/artifactory/main/release/$($(package)_version)/source/
-$(package)_file_name=boost_$(subst .,_,$($(package)_version)).tar.bz2
-$(package)_sha256_hash=d73a8da01e8bf8c7eda40b4c84915071a8c8a0df4a6734537ddde4a8580524ee
+package := native_b2
+$(package)_version := 1.71.0
+$(package)_download_path := https://boostorg.jfrog.io/artifactory/main/release/$($(package)_version)/source/
+$(package)_file_name := boost_$(subst .,_,$($(package)_version)).tar.bz2
+$(package)_sha256_hash := d73a8da01e8bf8c7eda40b4c84915071a8c8a0df4a6734537ddde4a8580524ee
 
-$(package)_build_subdir=tools/build/src/engine
+$(package)_build_subdir := tools/build/src/engine
 ifneq (,$(findstring clang,$($(package)_cxx)))
 $(package)_toolset_$(host_os)=clang
 else
