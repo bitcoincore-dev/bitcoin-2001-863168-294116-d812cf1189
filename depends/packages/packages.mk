4cba82dcb232ab2f46703d94511904c13c9445be
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-01-10 14:54:57 +0200

    build: Make packages related variables simply expanded ones

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,35 +1,36 @@
-packages:=boost libevent
+packages := boost libevent
 
-qrencode_linux_packages = qrencode
-qrencode_android_packages = qrencode
-qrencode_darwin_packages = qrencode
-qrencode_mingw32_packages = qrencode
+qrencode_linux_packages := qrencode
+qrencode_android_packages := qrencode
+qrencode_darwin_packages := qrencode
+qrencode_mingw32_packages := qrencode
 
-qt_linux_packages:=qt expat libxcb xcb_proto libXau xproto freetype fontconfig libxkbcommon libxcb_util libxcb_util_render libxcb_util_keysyms libxcb_util_image libxcb_util_wm
-qt_android_packages=qt
-qt_darwin_packages=qt
-qt_mingw32_packages=qt
+qt_linux_packages := qt expat libxcb xcb_proto libXau xproto freetype fontconfig libxkbcommon libxcb_util libxcb_util_render libxcb_util_keysyms libxcb_util_image libxcb_util_wm
+qt_android_packages := qt
 
-bdb_packages=bdb
-sqlite_packages=sqlite
+qt_darwin_packages := qt
+qt_mingw32_packages := qt
 
-zmq_packages=zeromq
+bdb_packages := bdb
+sqlite_packages := sqlite
 
-upnp_packages=miniupnpc
-natpmp_packages=libnatpmp
+zmq_packages := zeromq
 
-multiprocess_packages = libmultiprocess capnp
-multiprocess_native_packages = native_libmultiprocess native_capnp
+upnp_packages := miniupnpc
+natpmp_packages := libnatpmp
 
-usdt_linux_packages=systemtap
+multiprocess_packages := libmultiprocess capnp
+multiprocess_native_packages := native_libmultiprocess native_capnp
 
-darwin_native_packages = native_ds_store native_mac_alias
+usdt_linux_packages := systemtap
+
+darwin_native_packages := native_ds_store native_mac_alias
 
 ifneq ($(build_os),darwin)
 darwin_native_packages += native_cctools native_libtapi
 
 ifeq ($(strip $(FORCE_USE_SYSTEM_CLANG)),)
-darwin_native_packages+= native_clang
+darwin_native_packages += native_clang
 endif
 
 endif
