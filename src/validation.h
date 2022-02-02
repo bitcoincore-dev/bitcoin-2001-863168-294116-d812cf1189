358091004a0041e330bc958f965293b4ddb026f0
James O'Beirne <james.obeirne@pm.me>
2022-02-02 14:40:47 -0500

    validation: add CChainState::m_stop_use and ChainMan::isUsable
    and remove m_snapshot_validated. This state can now be inferred by the
    number of isUsable chainstates.
    
    m_stop_use is used to signal that a chainstate should no longer be used
    by validation logic; it is used as a sentinel when background validation
    completes or if the snapshot chainstate is found to be invalid.
    
    isUsable is a convenience method that incorporates m_stop_use.

--- src/validation.h
+++ src/validation.h
@@ -493,6 +493,12 @@ protected:
     //! Manages the UTXO set, which is a reflection of the contents of `m_chain`.
     std::unique_ptr<CoinsViews> m_coins_views;
 
+    //! This toggle exists for use when doing background validation for UTXO
+    //! snapshots. It is set once the background validation chain reaches the
+    //! same height as the base of the snapshot, and signals that we should no
+    //! longer connect blocks to the background chainstate.
+    bool m_stop_use{false};
+
 public:
     //! Reference to a BlockManager instance which itself is shared across all
     //! CChainState instances.
@@ -846,10 +852,6 @@ private:
     //! that call.
     CChainState* m_active_chainstate GUARDED_BY(::cs_main) {nullptr};
 
-    //! If true, the assumed-valid chainstate has been fully validated
-    //! by the background validation chainstate.
-    bool m_snapshot_validated{false};
-
     CBlockIndex* m_best_invalid;
     friend bool node::BlockManager::LoadBlockIndex(const Consensus::Params&);
 
@@ -873,6 +875,15 @@ private:
     // Returns nullptr if no snapshot has been loaded.
     CBlockIndex* GetSnapshotBaseBlock() EXCLUSIVE_LOCKS_REQUIRED(::cs_main);
     std::optional<int> GetSnapshotBaseHeight() EXCLUSIVE_LOCKS_REQUIRED(::cs_main);
+
+    //! Return true if a chainstate is considered usable.
+    //!
+    //! This is false when a background validation chainstate has completed its
+    //! validation of an assumed-valid chainstate, or when a snapshot
+    //! chainstate has been found to be invalid.
+    bool IsUsable(const CChainState* const cs) const {
+        return cs && !cs->m_stop_use;
+    }
 
 public:
     std::thread m_load_block;
@@ -959,7 +970,12 @@ public:
     std::optional<uint256> SnapshotBlockhash() const;
 
     //! Is there a snapshot in use and has it been fully validated?
-    bool IsSnapshotValidated() const { return m_snapshot_validated; }
+    bool IsSnapshotValidated() EXCLUSIVE_LOCKS_REQUIRED(::cs_main)
+    {
+        return m_ibd_chainstate &&
+               m_snapshot_chainstate &&
+               !this->IsUsable(m_ibd_chainstate.get());
+    }
 
     /**
      * Process an incoming block. This only returns after the best known valid
