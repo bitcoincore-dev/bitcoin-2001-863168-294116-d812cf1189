bitcoin/bitcoin

RPC/blockchain: getmempoolinfo: Enable specifying with_fee_histogram as a boolean to use a sensible default set of fee rate levels

src/rpc/blockchain.h
@@ -44,6 +44,15 @@ UniValue blockToJSON(const CBlock& block, const CBlockIndex* tip, const CBlockIn
 
 typedef std::vector<CAmount> MempoolHistogramFeeRates;
 
+/* TODO: define log scale formular for dynamically creating the
+ * feelimits but with the property of not constantly changing
+ * (and thus screw up client implementations) */
+static const MempoolHistogramFeeRates MempoolInfoToJSON_const_limits{
+    1, 2, 3, 4, 5, 6, 7, 8, 10,
+    12, 14, 17, 20, 25, 30, 40, 50, 60, 70, 80, 100,
+    120, 140, 170, 200, 250, 300, 400, 500, 600, 700, 800, 1000,
+    1200, 1400, 1700, 2000, 2500, 3000, 4000, 5000, 6000, 7000, 8000, 10000};
+
 /** Mempool information to JSON */
 UniValue MempoolInfoToJSON(const CTxMemPool& pool, const std::optional<MempoolHistogramFeeRates> feeLimits);
 

bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

src/rpc/blockchain.h
@@ -45,6 +45,15 @@ UniValue blockToJSON(const CBlock& block, const CBlockIndex* tip, const CBlockIn
 
 typedef std::vector<CAmount> MempoolHistogramFeeRates;
 
+/* TODO: define log scale formular for dynamically creating the
+ * feelimits but with the property of not constantly changing
+ * (and thus screw up client implementations) */
+static const MempoolHistogramFeeRates MempoolInfoToJSON_const_histogram_floors{
+    1, 2, 3, 4, 5, 6, 7, 8, 10,
+    12, 14, 17, 20, 25, 30, 40, 50, 60, 70, 80, 100,
+    120, 140, 170, 200, 250, 300, 400, 500, 600, 700, 800, 1000,
+    1200, 1400, 1700, 2000, 2500, 3000, 4000, 5000, 6000, 7000, 8000, 10000};
+
 /** Mempool information to JSON */
 UniValue MempoolInfoToJSON(const CTxMemPool& pool, const std::optional<MempoolHistogramFeeRates>& histogram_floors);
 