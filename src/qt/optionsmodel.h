2ca4d51b16dc2ecb3768006d2f298ff2245b8b5e
Luke Dashjr <luke-jr+git@utopios.org>
2022-05-21 05:15:50 +0000

    Qt/Options: Configure blockmaxsize, blockprioritysize, and blockmaxweight using rwconf

--- src/qt/optionsmodel.h
+++ src/qt/optionsmodel.h
@@ -99,6 +99,9 @@ public:
         limitdescendantsize,
         rejectbaremultisig,     // bool
         datacarriersize,
+        blockmaxsize,
+        blockprioritysize,
+        blockmaxweight,
         OptionIDRowCount,
     };
 
