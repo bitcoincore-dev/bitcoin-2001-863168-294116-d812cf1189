87a08a69b6da5099fd49ad1da571f46ac6a2e758
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-05-31 12:50:19 +0200

    ci, android: Update Command-line Tools from 2.1 up to 7.0

--- ci/test/00_setup_env_android.sh
+++ ci/test/00_setup_env_android.sh
@@ -14,7 +14,7 @@ export DOCKER_NAME_TAG="ubuntu:focal"
 export RUN_UNIT_TESTS=false
 export RUN_FUNCTIONAL_TESTS=false
 
-export ANDROID_TOOLS_URL=https://dl.google.com/android/repository/commandlinetools-linux-6609375_latest.zip
+export ANDROID_TOOLS_URL=https://dl.google.com/android/repository/commandlinetools-linux-8512546_latest.zip
 export ANDROID_NDK_HOME="${ANDROID_HOME}/ndk/${ANDROID_NDK_VERSION}"
 export DEP_OPTS="ANDROID_SDK=${ANDROID_HOME} ANDROID_NDK=${ANDROID_NDK_HOME} ANDROID_API_LEVEL=${ANDROID_API_LEVEL} ANDROID_TOOLCHAIN_BIN=${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-x86_64/bin/"
 
