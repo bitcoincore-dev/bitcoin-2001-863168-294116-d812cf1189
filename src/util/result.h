e2b184ec86000df85472923bd1b0e2863cb7cc49
Ryan Ofsky <ryan@ofsky.org>
2022-07-18 15:48:04 -0400

    refactor: Add util::Result multiple error and warning messages
    Add util::Result support for returning warning messages and multiple errors,
    not just a single error string. This provides a way for functions to report
    errors and warnings in a standard way, and simplifies interfaces.
    
    The functionality is unit tested here, and put to use in followup PR
    https://github.com/bitcoin/bitcoin/pull/25722

--- src/util/result.h
+++ src/util/result.h
@@ -18,11 +18,11 @@
 
 namespace util {
 
-//! The `Result` class provides a standard way for functions to return an error
-//! string in addition to optional result values.
+//! The `Result` class provides a standard way for functions to return error and
+//! warning strings in addition to optional result values.
 //!
 //! The easiest way to understand `Result<T>` is to think of it as a substitute
-//! for `std::optional<T>`, that just contains an error string in
+//! for `std::optional<T>`, that just contains error and warning strings in
 //! addition to the optional return value. For example:
 //!
 //!    util::Result<int> AddNumbers(int a, int b)
@@ -61,20 +61,44 @@ namespace util {
 template <typename T, typename F = void>
 class Result;
 
-//! Wrapper type to pass error strings to Result constructors.
+//! Wrapper types to pass error and warning strings to Result constructors.
 struct Error {
     bilingual_str message;
 };
+struct Warning {
+    bilingual_str message;
+};
+
+//! Wrapper type to move error and warning strings from an existing Result object to a new Result constructor.
+template <typename Result>
+struct MoveMessages {
+    MoveMessages(Result&& result) : m_result(result) {}
+    Result& m_result;
+};
+
+//! Template deduction guide for MoveMessages class.
+template <class Result>
+MoveMessages(Result&& result) -> MoveMessages<Result>;
 
 namespace detail {
+//! Empty string list
+const std::vector<bilingual_str> EMPTY_LIST{};
+
+//! Helper function to join messages in space separated string.
+bilingual_str JoinMessages(const std::vector<bilingual_str>& errors, const std::vector<bilingual_str>& warnings);
+
+//! Helper function to move messages from one vector to another.
+void MoveMessages(std::vector<bilingual_str>& src, std::vector<bilingual_str>& dest);
+
 //! Substitute for std::monostate that doesn't depend on std::variant.
 struct MonoState{};
 
-//! Error information only allocated on failure.
+//! Error information only allocated if there are errors or warnings.
 template <typename F>
 struct ErrorInfo {
-    std::conditional_t<std::is_same_v<F, void>, MonoState, F> failure;
-    bilingual_str error;
+    std::optional<std::conditional_t<std::is_same_v<F, void>, MonoState, F>> failure{};
+    std::vector<bilingual_str> errors{};
+    std::vector<bilingual_str> warnings{};
 };
 
 //! Result base class which is inherited by Result<T, F>.
@@ -91,6 +115,12 @@ class ResultBase<void, F>
 protected:
     std::unique_ptr<ErrorInfo<F>> m_info;
 
+    ErrorInfo<F>& Info() LIFETIMEBOUND
+    {
+        if (!m_info) m_info = std::make_unique<ErrorInfo<F>>();
+        return *m_info;
+    }
+
     //! Value setter methods which do nothing because this ResultBase class is
     //! specialized for type T=void, so there is no result value for it to hold.
     //! The other ResultBase specialization below for non-void T overrides these
@@ -106,7 +136,7 @@ protected:
     static void ConstructResult(Result& result, Args&&... args)
     {
         if constexpr (Failure) {
-            result.m_info.reset(new detail::ErrorInfo<F>{.failure{std::forward<Args>(args)...}, .error{}});
+            result.Info().failure.emplace(std::forward<Args>(args)...);
         } else {
             result.ConstructValue(std::forward<Args>(args)...);
         }
@@ -116,12 +146,28 @@ protected:
     template <bool Failure, typename Result, typename... Args>
     static void ConstructResult(Result& result, util::Error error, Args&&... args)
     {
+        result.AddError(std::move(error.message));
         ConstructResult</*Failure=*/true>(result, std::forward<Args>(args)...);
-        result.m_info->error = std::move(error.message);
     }
 
-    //! Helper function to move success or failure values and any error
-    //! message from one result object to another. The two result
+    //! ConstructResult() overload peeling off a util::Warning constructor argument.
+    template <bool Failure, typename Result, typename... Args>
+    static void ConstructResult(Result& result, util::Warning warning, Args&&... args)
+    {
+        result.AddWarning(std::move(warning.message));
+        ConstructResult<Failure>(result, std::forward<Args>(args)...);
+    }
+
+    //! ConstructResult() overload peeling off a util::MoveMessages constructor argument.
+    template <bool Failure, typename Result, typename R, typename... Args>
+    static void ConstructResult(Result& result, util::MoveMessages<R> messages, Args&&... args)
+    {
+        result.MoveMessages(std::move(messages.m_result));
+        ConstructResult<Failure>(result, std::forward<Args>(args)...);
+    }
+
+    //! Helper function to move success or failure values and any error and
+    //! warning messages from one result object to another. The two result
     //! objects must have compatible success and failure types.
     template <bool Constructed, typename DstResult, typename SrcResult>
     static void MoveResult(DstResult& dst, SrcResult&& src)
@@ -130,20 +176,21 @@ protected:
             if (dst) {
                 dst.DestroyValue();
             } else {
-                dst.m_info.reset();
+                dst.m_info->failure.reset();
             }
         }
+        dst.MoveMessages(src);
         if (src) {
             dst.MoveValue(std::move(src));
         } else {
-            dst.m_info.reset(new detail::ErrorInfo<F>{std::move(*src.m_info)});
+            dst.Info().failure = std::move(src.m_info->failure);
         }
     }
 
-    //! Disallow operator= and require explicit Result::Set() calls to avoid
-    //! confusion in the future when the Result class gains support for richer
-    //! error reporting, and callers should have ability to set a new result
-    //! value without clearing existing error messages.
+    //! Disallow potentially dangerous assignment operators which might erase
+    //! error and warning messages. The Result::Set() method can be used instead
+    //! of operator= to assign result values while keeping any existing errors
+    //! and warnings.
     template <typename Result>
     ResultBase& operator=(Result&&) = delete;
 
@@ -152,10 +199,35 @@ protected:
 
 public:
     //! Success check.
-    explicit operator bool() const { return !m_info; }
+    explicit operator bool() const { return !m_info || !m_info->failure; }
 
     //! Error retrieval.
-    const auto& GetFailure() const LIFETIMEBOUND { assert(!*this); return m_info->failure; }
+    const auto& GetFailure() const LIFETIMEBOUND { assert(!*this); return *m_info->failure; }
+    const std::vector<bilingual_str>& GetErrors() const LIFETIMEBOUND { return m_info ? m_info->errors : EMPTY_LIST; }
+    const std::vector<bilingual_str>& GetWarnings() const LIFETIMEBOUND { return m_info ? m_info->warnings : EMPTY_LIST; }
+
+    //! Add error and warning messages.
+    void AddError(bilingual_str error)
+    {
+        if (!error.empty()) this->Info().errors.emplace_back(std::move(error));
+    }
+    void AddWarning(bilingual_str warning)
+    {
+        if (!warning.empty()) this->Info().warnings.emplace_back(std::move(warning));
+    }
+
+    //! Move error and warning messages from another result to this one.
+    template <typename Result>
+    void MoveMessages(Result&& other)
+    {
+        if (other.m_info) {
+            // Check that errors and warnings are empty before calling
+            // MoveMessages to avoid allocating memory in this->Info() in the
+            // typical case when there are no errors or warnings.
+            if (!other.m_info->errors.empty()) detail::MoveMessages(other.m_info->errors, this->Info().errors);
+            if (!other.m_info->warnings.empty()) detail::MoveMessages(other.m_info->warnings, this->Info().warnings);
+        }
+    }
 };
 
 //! Result base class for T value type. Holds value and provides accessor methods.
@@ -210,9 +282,10 @@ protected:
     using Base = detail::ResultBase<T, F>;
 
 public:
-    //! Construct a Result object setting a success or failure value. An
-    //! optional util::Error argument is processed first to set an error
-    //! message. Then, any remaining arguments are passed to the type `T`
+    //! Construct a Result object setting a success or failure value and
+    //! optional warning and error messages. Initial util::Error, util::Warning,
+    //! and util::MoveMessages arguments are processed first to add warning and
+    //! error messages. Then, any remaining arguments are passed to the type `T`
     //! constructor and used to construct a success value in the success case.
     //! In the failure case, any remaining arguments are passed to the type `F`
     //! constructor and used to construct a failure value.
@@ -223,29 +296,53 @@ public:
     }
 
     //! Move-construct a Result object from another Result object, moving the
-    //! success or failure value and any error message.
+    //! success or failure value any any error or warning messages.
     template <typename OT, typename OF>
     Result(Result<OT, OF>&& other)
     {
         Base::template MoveResult</*Constructed=*/false>(*this, std::move(other));
     }
 
-    //! Move success or failure values and any error message from another Result
-    //! object to this object.
+    //! Move success or failure values from another Result object to this
+    //! object. Also move any error and warning messages from the other Result
+    //! object to this one. If this Result object has an existing success or
+    //! failure value, it is cleared and replaced by the other value. If this
+    //! Result object has any error or warning messages, they are kept and
+    //! appended to.
     Result& Set(Result&& other) LIFETIMEBOUND
     {
         Base::template MoveResult</*Constructed=*/true>(*this, std::move(other));
         return *this;
     }
 
-    inline friend bilingual_str _ErrorString(const Result& result)
+    //! Operator moving warning and error messages from a source result object
+    //! to a destination result object. Only moves message strings, does not
+    //! change success or failure values of either Result object.
+    //!
+    //! This is useful for combining error and warning messages from multiple
+    //! result objects into a single object, e.g.:
+    //!
+    //!    util::Result<void> result;
+    //!    auto r1 = DoSomething() >> result;
+    //!    auto r2 = DoSomethingElse() >> result;
+    //!    ...
+    //!    return result;
+    //!
+    template <typename O>
+    Result&& operator>>(O&& other LIFETIMEBOUND) &&
     {
-        return result ? bilingual_str{} : result.m_info->error;
+        other.MoveMessages(*this);
+        return std::move(*this);
     }
 };
 
+//! Join error and warning messages in a space separated string. This is
+//! intended for simple applications where there's probably only one error or
+//! warning message to report, but multiple messages should not be lost if they
+//! are present. More complicated applications should use GetErrors() and
+//! GetWarning() methods directly.
 template <typename T, typename F>
-bilingual_str ErrorString(const Result<T, F>& result) { return _ErrorString(result); }
+bilingual_str ErrorString(const Result<T, F>& result) { return detail::JoinMessages(result.GetErrors(), result.GetWarnings()); }
 } // namespace util
 
 #endif // BITCOIN_UTIL_RESULT_H
