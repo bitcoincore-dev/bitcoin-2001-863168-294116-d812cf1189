Lajandra/bitcoin

indexes, refactor: Remove index Init method
Add new interfaces::Chain::attachChain method and start moving index
sync logic out of indexing code to the node code in interfaces::Chain.
In this commit, move code that registers for index validationinterface
notifications from index code to node code.  In later commits, move the
actual sync thread and add real blockConnected, blockDisconnected, and
chainStateFlushed implementations.

Also in this commit, move code that sets m_best_block_index and m_sync
that runs at the beginning of sync. This is done here so cs_main is
still locked while these are initialized.

This commit does not change behavior in any way.

src/node/interfaces.cpp
@@ -661,8 +661,7 @@ public:
     {
         ::uiInterface.ShowProgress(title, progress, resume_possible);
     }
-    bool checkBlocks(const CBlockIndex* locator_block) override {
-        LOCK(::cs_main);
+    bool checkBlocks(const CBlockIndex* locator_block) EXCLUSIVE_LOCKS_REQUIRED(::cs_main) {
         const CChain& active_chain = Assert(m_node.chainman)->ActiveChain();
         bool prune_violation = false;
         if (!locator_block) {
@@ -693,6 +692,18 @@ public:
             }
         }
         return !prune_violation;
+    }
+    std::unique_ptr<Handler> attachChain(std::shared_ptr<Notifications> notifications, const CBlockLocator& locator, const NotifyOptions& options) override
+    {
+        LOCK(cs_main);
+        const CChainState& active = Assert(m_node.chainman)->ActiveChainstate();
+        const CBlockIndex* locator_block = locator.IsNull() ? nullptr : active.FindForkInGlobalIndex(locator);
+        interfaces::BlockInfo block_info = node::MakeBlockInfo(locator_block);
+        block_info.chain_tip = locator_block == active.m_chain.Tip();
+        notifications->blockConnected(block_info);
+        if (!block_info.chain_tip && !checkBlocks(locator_block)) return nullptr;
+        auto handler = std::make_unique<NotificationsHandlerImpl>(notifications);
+        return handler;
     }
     std::unique_ptr<Handler> handleNotifications(std::shared_ptr<Notifications> notifications) override
     {

bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

src/node/interfaces.cpp
@@ -717,8 +717,7 @@ public:
     {
         ::uiInterface.ShowProgress(title, progress, resume_possible);
     }
-    bool checkBlocks(const CBlockIndex* start_block) override {
-        LOCK(::cs_main);
+    bool checkBlocks(const CBlockIndex* start_block) EXCLUSIVE_LOCKS_REQUIRED(::cs_main) {
         const CChain& active_chain = Assert(m_node.chainman)->ActiveChain();
         bool prune_violation = false;
         if (!start_block) {
@@ -749,6 +748,18 @@ public:
             }
         }
         return !prune_violation;
+    }
+    std::unique_ptr<Handler> attachChain(std::shared_ptr<Notifications> notifications, const CBlockLocator& locator, const NotifyOptions& options) override
+    {
+        LOCK(cs_main);
+        const CChainState& active = Assert(m_node.chainman)->ActiveChainstate();
+        const CBlockIndex* start_block = locator.IsNull() ? nullptr : active.FindForkInGlobalIndex(locator);
+        interfaces::BlockInfo block_info = kernel::MakeBlockInfo(start_block);
+        block_info.chain_tip = start_block == active.m_chain.Tip();
+        notifications->blockConnected(block_info);
+        if (!block_info.chain_tip && !checkBlocks(start_block)) return nullptr;
+        auto handler = std::make_unique<NotificationsHandlerImpl>(notifications);
+        return handler;
     }
     std::unique_ptr<Handler> handleNotifications(std::shared_ptr<Notifications> notifications) override
     {