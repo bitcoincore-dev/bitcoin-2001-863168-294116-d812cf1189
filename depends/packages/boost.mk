b08deecc0905317940b9a1296eb883fd01ace339
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-07-26 11:51:17 +0100

    build: Re-enable external signer on Windows

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -3,6 +3,11 @@ $(package)_version=1.80.0
 $(package)_download_path=https://boostorg.jfrog.io/artifactory/main/beta/$($(package)_version).beta1/source/
 $(package)_file_name=boost_$(subst .,_,$($(package)_version))_b1.tar.bz2
 $(package)_sha256_hash=b29191c1bd4504511f9b1155c6f79f06cc7044444780392ed7358f5778ac7d16
+$(package)_patches := mingw32_process.patch
+
+define $(package)_preprocess_cmds
+  patch -p1 -i $($(package)_patch_dir)/mingw32_process.patch
+endef
 
 define $(package)_stage_cmds
   mkdir -p $($(package)_staging_prefix_dir)/include && \
