Lajandra/bitcoin

indexes, refactor: Remove index prune_violation code
This commit is mostly move only. Suggest reviewing with --color-moved.

Just move prune-checking code that runs at the beginning of sync (the
code that checks for pruned blocks and triggers the "goes beyond pruned
data" error) from index to node.

This commit does not change behavior in any way.

src/interfaces/chain.h
@@ -19,6 +19,7 @@
 
 class ArgsManager;
 class CBlock;
+class CBlockIndex;
 class CBlockUndo;
 class CFeeRate;
 class CRPCCommand;
@@ -278,6 +279,9 @@ public:
         virtual void updatedBlockTip() {}
         virtual void chainStateFlushed(const CBlockLocator& locator) {}
     };
+
+    //! Check if all blocks needed to sync start block to current tip are present.
+    virtual bool hasDataFromTipDown(const CBlockIndex* start_block) = 0;
 
     //! Register handler for notifications.
     virtual std::unique_ptr<Handler> handleNotifications(std::shared_ptr<Notifications> notifications) = 0;

src/node/interfaces.cpp
@@ -729,6 +729,39 @@ public:
     {
         ::uiInterface.ShowProgress(title, progress, resume_possible);
     }
+    bool hasDataFromTipDown(const CBlockIndex* start_block) override {
+        LOCK(::cs_main);
+        const CChain& active_chain = Assert(m_node.chainman)->ActiveChain();
+        bool prune_violation = false;
+        if (!start_block) {
+            // make sure we have all block data back to the genesis
+            prune_violation = m_node.chainman->m_blockman.GetFirstStoredBlock(*active_chain.Tip()) != active_chain.Genesis();
+        }
+        // in case the index has a best block set and is not fully synced
+        // check if we have the required blocks to continue building the index
+        else {
+            const CBlockIndex* block_to_test = start_block;
+            if (!active_chain.Contains(block_to_test)) {
+                // if the bestblock is not part of the mainchain, find the fork
+                // and make sure we have all data down to the fork
+                block_to_test = active_chain.FindFork(block_to_test);
+            }
+            const CBlockIndex* block = active_chain.Tip();
+            prune_violation = true;
+            // check backwards from the tip if we have all block data until we reach the indexes bestblock
+            while (block_to_test && block && (block->nStatus & BLOCK_HAVE_DATA)) {
+                if (block_to_test == block) {
+                    prune_violation = false;
+                    break;
+                }
+                // block->pprev must exist at this point, since block_to_test is part of the chain
+                // and thus must be encountered when going backwards from the tip
+                assert(block->pprev);
+                block = block->pprev;
+            }
+        }
+        return !prune_violation;
+    }
     std::unique_ptr<Handler> handleNotifications(std::shared_ptr<Notifications> notifications) override
     {
         return std::make_unique<NotificationsHandlerImpl>(std::move(notifications));

bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

src/interfaces/chain.h
@@ -18,6 +18,7 @@
 
 class ArgsManager;
 class CBlock;
+class CBlockIndex;
 class CBlockUndo;
 class CFeeRate;
 class CRPCCommand;
@@ -267,6 +268,9 @@ public:
         virtual void updatedBlockTip() {}
         virtual void chainStateFlushed(const CBlockLocator& locator) {}
     };
+
+    //! Check if all blocks needed to sync start block to current tip are present.
+    virtual bool checkBlocks(const CBlockIndex* start_block) = 0;
 
     //! Register handler for notifications.
     virtual std::unique_ptr<Handler> handleNotifications(std::shared_ptr<Notifications> notifications) = 0;

src/node/interfaces.cpp
@@ -717,6 +717,39 @@ public:
     {
         ::uiInterface.ShowProgress(title, progress, resume_possible);
     }
+    bool checkBlocks(const CBlockIndex* start_block) override {
+        LOCK(::cs_main);
+        const CChain& active_chain = Assert(m_node.chainman)->ActiveChain();
+        bool prune_violation = false;
+        if (!start_block) {
+            // make sure we have all block data back to the genesis
+            prune_violation = m_node.chainman->m_blockman.GetFirstStoredBlock(*active_chain.Tip()) != active_chain.Genesis();
+        }
+        // in case the index has a best block set and is not fully synced
+        // check if we have the required blocks to continue building the index
+        else {
+            const CBlockIndex* block_to_test = start_block;
+            if (!active_chain.Contains(block_to_test)) {
+                // if the bestblock is not part of the mainchain, find the fork
+                // and make sure we have all data down to the fork
+                block_to_test = active_chain.FindFork(block_to_test);
+            }
+            const CBlockIndex* block = active_chain.Tip();
+            prune_violation = true;
+            // check backwards from the tip if we have all block data until we reach the indexes bestblock
+            while (block_to_test && block && (block->nStatus & BLOCK_HAVE_DATA)) {
+                if (block_to_test == block) {
+                    prune_violation = false;
+                    break;
+                }
+                // block->pprev must exist at this point, since block_to_test is part of the chain
+                // and thus must be encountered when going backwards from the tip
+                assert(block->pprev);
+                block = block->pprev;
+            }
+        }
+        return !prune_violation;
+    }
     std::unique_ptr<Handler> handleNotifications(std::shared_ptr<Notifications> notifications) override
     {
         return std::make_unique<NotificationsHandlerImpl>(std::move(notifications));
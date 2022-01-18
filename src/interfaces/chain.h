20f2973ae29c6f4ff89571ffd9fcd5fec65ac550
Ryan Ofsky <ryan@ofsky.org>
2022-01-18 01:06:46 -0500

    indexes, refactor: Remove index prune_violation code
    This commit is mostly move only. Suggest reviewing with --color-moved.
    
    Just move prune-checking code that runs at the beginning of sync (the
    code that checks for pruned blocks and triggers the "goes beyond pruned
    data" error) from index to node.
    
    This commit does not change behavior in any way.

--- src/interfaces/chain.h
+++ src/interfaces/chain.h
@@ -18,6 +18,7 @@
 
 class ArgsManager;
 class CBlock;
+class CBlockIndex;
 class CBlockUndo;
 class CFeeRate;
 class CRPCCommand;
@@ -267,6 +268,9 @@ public:
         virtual void updatedBlockTip() {}
         virtual void chainStateFlushed(const CBlockLocator& locator) {}
     };
+
+    //! Check if all blocks needed to sync start block to current tip are present.
+    virtual bool checkBlocks(const CBlockIndex* start_block) = 0;
 
     //! Register handler for notifications.
     virtual std::unique_ptr<Handler> handleNotifications(std::shared_ptr<Notifications> notifications) = 0;
