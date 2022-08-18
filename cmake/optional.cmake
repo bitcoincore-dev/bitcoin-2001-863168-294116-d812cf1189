523470a10b7b8502e2180670502c3090463c106a
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-08-18 10:12:27 +0100

    cmake: Add `libqrencode` optional package support

--- cmake/optional.cmake
+++ cmake/optional.cmake
@@ -187,3 +187,20 @@ else()
   set(WITH_SQLITE OFF)
   set(WITH_BDB OFF)
 endif()
+
+if(WITH_GUI AND WITH_QRENCODE)
+  # TODO: vcpkg fails to build libqrencode package due to
+  #       the build error in its libiconv dependency.
+  #       See: https://github.com/microsoft/vcpkg/issues/36924.
+  cross_pkg_check_modules(libqrencode libqrencode IMPORTED_TARGET)
+  if(libqrencode_FOUND)
+    set_target_properties(PkgConfig::libqrencode PROPERTIES
+      INTERFACE_COMPILE_DEFINITIONS USE_QRCODE
+    )
+    set(WITH_QRENCODE ON)
+  elseif(WITH_QRENCODE STREQUAL "AUTO")
+    set(WITH_QRENCODE OFF)
+  else()
+    message(FATAL_ERROR "libqrencode requested, but not found.")
+  endif()
+endif()
