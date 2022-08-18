1b1c709a41453cd8cd0b6c7dfc67445c27d93207
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2022-08-18 14:03:10 +0100

    cmake: Add cross-compiling support
    To configure CMake for cross-compiling, use
    `--toolchain depends/${HOST}/share/toolchain.cmake` command-line option.

--- cmake/optional.cmake
+++ cmake/optional.cmake
@@ -54,7 +54,7 @@ endif()
 
 if(ENABLE_WALLET)
   if(NOT WITH_SQLITE STREQUAL "no")
-    pkg_check_modules(sqlite sqlite3>=3.7.17 IMPORTED_TARGET)
+    cross_pkg_check_modules(sqlite sqlite3>=3.7.17 IMPORTED_TARGET)
     if(sqlite_FOUND)
       set(WITH_SQLITE yes)
       set(USE_SQLITE ON)
@@ -123,7 +123,7 @@ if(NOT WITH_ZMQ STREQUAL "no")
   if(MSVC)
     find_package(ZeroMQ CONFIG)
   else()
-    pkg_check_modules(libzmq libzmq>=4 IMPORTED_TARGET GLOBAL)
+    cross_pkg_check_modules(libzmq libzmq>=4 IMPORTED_TARGET GLOBAL)
     if(libzmq_FOUND)
       set_property(TARGET PkgConfig::libzmq APPEND PROPERTY
         INTERFACE_COMPILE_DEFINITIONS $<$<PLATFORM_ID:Windows>:ZMQ_STATIC>
@@ -166,7 +166,7 @@ if(NOT WITH_USDT STREQUAL "no")
 endif()
 
 if(NOT WITH_GUI STREQUAL "no" AND NOT WITH_QRENCODE STREQUAL "no")
-  pkg_check_modules(libqrencode libqrencode IMPORTED_TARGET)
+  cross_pkg_check_modules(libqrencode libqrencode IMPORTED_TARGET)
   if(libqrencode_FOUND)
     set_target_properties(PkgConfig::libqrencode PROPERTIES
       INTERFACE_COMPILE_DEFINITIONS USE_QRCODE
