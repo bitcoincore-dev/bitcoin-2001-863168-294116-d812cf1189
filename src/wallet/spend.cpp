f1b46f4017a52093a4ae41656b953ab3c1b7403d
Ryan Ofsky <ryan@ofsky.org>
2022-09-15 11:15:06 -0400

    refactor: Drop util::Result operator=
    In cases where different error conditions need to be handled differently, it is
    useful for `util::`Result` objects to be able to hold failure values and error
    messages simultaneously. Also it is useful for `util::Result` objects to be
    able to hold multiple error message strings and warnings, instead of just
    single error strings.
    
    In both of these cases, which are supported in upcoming commits, having an
    `operator=` method is potentially dangerous if it clears existing error and
    warnings strings while setting result values, or confusing if it doesn't clear
    them, so the safest thing is to disable `operator=` and provide an explicit
    `Set()` method so callers have a way of setting result values without having to
    clear message strings.

--- src/wallet/spend.cpp
+++ src/wallet/spend.cpp
@@ -559,11 +559,11 @@ util::Result<SelectionResult> ChooseSelectionResult(const CAmount& nTargetValue,
     // Vector of results. We will choose the best one based on waste.
     std::vector<SelectionResult> results;
     std::vector<util::Result<SelectionResult>> errors;
-    auto append_error = [&] (const util::Result<SelectionResult>& result) {
+    auto append_error = [&] (util::Result<SelectionResult>&& result) {
         // If any specific error message appears here, then something different from a simple "no selection found" happened.
         // Let's save it, so it can be retrieved to the user if no other selection algorithm succeeded.
         if (HasErrorMsg(result)) {
-            errors.emplace_back(result);
+            errors.emplace_back(std::move(result));
         }
     };
 
@@ -572,7 +572,7 @@ util::Result<SelectionResult> ChooseSelectionResult(const CAmount& nTargetValue,
 
     if (auto bnb_result{SelectCoinsBnB(groups.positive_group, nTargetValue, coin_selection_params.m_cost_of_change, max_inputs_weight)}) {
         results.push_back(*bnb_result);
-    } else append_error(bnb_result);
+    } else append_error(std::move(bnb_result));
 
     // As Knapsack and SRD can create change, also deduce change weight.
     max_inputs_weight -= (coin_selection_params.change_output_size * WITNESS_SCALE_FACTOR);
@@ -581,17 +581,17 @@ util::Result<SelectionResult> ChooseSelectionResult(const CAmount& nTargetValue,
     if (auto knapsack_result{KnapsackSolver(groups.mixed_group, nTargetValue, coin_selection_params.m_min_change_target, coin_selection_params.rng_fast, max_inputs_weight)}) {
         knapsack_result->ComputeAndSetWaste(coin_selection_params.min_viable_change, coin_selection_params.m_cost_of_change, coin_selection_params.m_change_fee);
         results.push_back(*knapsack_result);
-    } else append_error(knapsack_result);
+    } else append_error(std::move(knapsack_result));
 
     if (auto srd_result{SelectCoinsSRD(groups.positive_group, nTargetValue, coin_selection_params.m_change_fee, coin_selection_params.rng_fast, max_inputs_weight)}) {
         srd_result->ComputeAndSetWaste(coin_selection_params.min_viable_change, coin_selection_params.m_cost_of_change, coin_selection_params.m_change_fee);
         results.push_back(*srd_result);
-    } else append_error(srd_result);
+    } else append_error(std::move(srd_result));
 
     if (results.empty()) {
         // No solution found, retrieve the first explicit error (if any).
         // future: add 'severity level' to errors so the worst one can be retrieved instead of the first one.
-        return errors.empty() ? util::Error() : errors.front();
+        return errors.empty() ? util::Error() : std::move(errors.front());
     }
 
     // Choose the result with the least waste
