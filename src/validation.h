d420bae06babdab90ba3d0eb53ba19adea9a99a5
James O'Beirne <james.obeirne@pm.me>
2022-11-10 17:20:05 -0500

    index: cache indexer references on ChainstateManager
    This is done for use in later commits. Once snapshot validation
    completes, we must restart the indexers so that they continue
    indexation (sequentially) on the fully validated snapshot chainstate
    now that all the requisite block data is there.

--- src/validation.h
+++ src/validation.h
@@ -46,6 +46,7 @@
 #include <utility>
 #include <vector>
 
+class BaseIndex;
 class Chainstate;
 class CBlockTreeDB;
 class CTxMemPool;
@@ -919,6 +920,8 @@ public:
     using Options = kernel::ChainstateManagerOpts;
 
     explicit ChainstateManager(Options options, node::BlockManager::Options blockman_options);
+
+    std::vector<BaseIndex*> indexers{};
 
     const CChainParams& GetParams() const { return m_options.chainparams; }
     const Consensus::Params& GetConsensus() const { return m_options.chainparams.GetConsensus(); }
