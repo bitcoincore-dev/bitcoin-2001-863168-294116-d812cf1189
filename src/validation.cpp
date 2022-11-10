7eb60910ac0d5193d82ab30b8ab9cdcb5b9b4eb2
James O'Beirne <james.obeirne@pm.me>
2022-11-10 12:03:39 -0500

    validation: add ChainstateRole

--- src/validation.cpp
+++ src/validation.cpp
@@ -5723,6 +5723,16 @@ void Chainstate::InvalidateCoinsDBOnDisk()
     }
 }
 
+ChainstateRole Chainstate::GetRole() const
+{
+    if (!m_chainman.IsSnapshotActive()) {
+        return ChainstateRole::NORMAL;
+    }
+    return (this != &m_chainman.ActiveChainstate()) ?
+               ChainstateRole::BACKGROUND :
+               ChainstateRole::ASSUMEDVALID;
+}
+
 const CBlockIndex* ChainstateManager::GetSnapshotBaseBlock() const
 {
     const auto blockhash_op = this->SnapshotBlockhash();
