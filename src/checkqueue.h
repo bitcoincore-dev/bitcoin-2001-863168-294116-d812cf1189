487fa0f7750e3eadd4bd791ca7f57add81641d62
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2023-01-16 13:06:54 +0000

    refactor: Make move semantics explicit for callers

--- src/checkqueue.h
+++ src/checkqueue.h
@@ -165,7 +165,7 @@ public:
     }
 
     //! Add a batch of checks to the queue
-    void Add(std::vector<T>& vChecks) EXCLUSIVE_LOCKS_REQUIRED(!m_mutex)
+    void Add(std::vector<T>&& vChecks) EXCLUSIVE_LOCKS_REQUIRED(!m_mutex)
     {
         if (vChecks.empty()) {
             return;
@@ -236,10 +236,11 @@ public:
         return fRet;
     }
 
-    void Add(std::vector<T>& vChecks)
+    void Add(std::vector<T>&& vChecks)
     {
-        if (pqueue != nullptr)
-            pqueue->Add(vChecks);
+        if (pqueue != nullptr) {
+            pqueue->Add(std::move(vChecks));
+        }
     }
 
     ~CCheckQueueControl()
