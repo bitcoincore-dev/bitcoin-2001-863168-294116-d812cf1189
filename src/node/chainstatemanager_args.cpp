665dbcd1fb46dfd811283369a0ea98aa2c6e61e5
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2023-01-30 12:00:20 +0000

    Move global `scriptcheckqueue` into `ChainstateManager` class

--- src/node/chainstatemanager_args.cpp
+++ src/node/chainstatemanager_args.cpp
@@ -5,6 +5,7 @@
 #include <node/chainstatemanager_args.h>
 
 #include <arith_uint256.h>
+#include <logging.h>
 #include <tinyformat.h>
 #include <uint256.h>
 #include <util/strencodings.h>
@@ -12,6 +13,7 @@
 #include <util/translation.h>
 #include <validation.h>
 
+#include <algorithm>
 #include <chrono>
 #include <optional>
 #include <string>
@@ -33,6 +35,16 @@ std::optional<bilingual_str> ApplyArgsManOptions(const ArgsManager& args, Chains
     if (auto value{args.GetArg("-assumevalid")}) opts.assumed_valid_block = uint256S(*value);
 
     if (auto value{args.GetIntArg("-maxtipage")}) opts.max_tip_age = std::chrono::seconds{*value};
+
+    int par_value = args.GetIntArg("-par", DEFAULT_SCRIPTCHECK_THREADS);
+    if (par_value <= 0) {
+        // -par=0 means autodetect (number of cores - 1 script threads)
+        // -par=-n means "leave n cores free" (number of cores - n - 1 script threads)
+        par_value += GetNumCores();
+    }
+    // Subtract 1 because the main thread counts towards the par threads.
+    opts.worker_threads_num = std::clamp(par_value - 1, 0, MAX_SCRIPTCHECK_THREADS);
+    LogPrintf("Script verification uses %d additional threads\n", opts.worker_threads_num);
 
     return std::nullopt;
 }
--- src/validation.cpp
+++ src/validation.cpp
@@ -1926,16 +1926,9 @@ DisconnectResult Chainstate::DisconnectBlock(const CBlock& block, const CBlockIn
     return fClean ? DISCONNECT_OK : DISCONNECT_UNCLEAN;
 }
 
-static CCheckQueue<CScriptCheck> scriptcheckqueue(128);
-
-void StartScriptCheckWorkerThreads(int threads_num)
+void ChainstateManager::StopScriptCheckWorkerThreads()
 {
-    scriptcheckqueue.StartWorkerThreads(threads_num);
-}
-
-void StopScriptCheckWorkerThreads()
-{
-    scriptcheckqueue.StopWorkerThreads();
+    m_script_check_queue.StopWorkerThreads();
 }
 
 /**
@@ -2028,7 +2021,7 @@ bool Chainstate::ConnectBlock(const CBlock& block, BlockValidationState& state, 
 
     uint256 block_hash{block.GetHash()};
     assert(*pindex->phashBlock == block_hash);
-    const bool parallel_script_checks{scriptcheckqueue.HasThreads()};
+    const bool parallel_script_checks{m_chainman.GetCheckQueue().HasThreads()};
 
     const auto time_start{SteadyClock::now()};
     const CChainParams& params{m_chainman.GetParams()};
@@ -2217,7 +2210,7 @@ bool Chainstate::ConnectBlock(const CBlock& block, BlockValidationState& state, 
     // in multiple threads). Preallocate the vector size so a new allocation
     // doesn't invalidate pointers into the vector, and keep txsdata in scope
     // for as long as `control`.
-    CCheckQueueControl<CScriptCheck> control(fScriptChecks && parallel_script_checks ? &scriptcheckqueue : nullptr);
+    CCheckQueueControl<CScriptCheck> control(fScriptChecks && parallel_script_checks ? &m_chainman.GetCheckQueue() : nullptr);
     std::vector<PrecomputedTransactionData> txsdata(block.vtx.size());
 
     std::vector<int> prevheights;
@@ -5307,10 +5300,17 @@ static ChainstateManager::Options&& Flatten(ChainstateManager::Options&& opts)
     return std::move(opts);
 }
 
-ChainstateManager::ChainstateManager(Options options) : m_options{Flatten(std::move(options))} {}
+ChainstateManager::ChainstateManager(Options options)
+    : m_script_check_queue{/*nBatchSizeIn=*/128},
+      m_options{Flatten(std::move(options))}
+{
+    m_script_check_queue.StartWorkerThreads(m_options.worker_threads_num);
+}
 
 ChainstateManager::~ChainstateManager()
 {
+    StopScriptCheckWorkerThreads();
+
     LOCK(::cs_main);
 
     m_versionbitscache.Clear();
