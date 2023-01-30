f2f65ec5094a05d71e21080c14fbdb8e46e4381d
Anthony Towns <aj@erisian.com.au>
2023-01-30 16:47:11 +1000

    tests: drop versionbits specific tests
    After we replace versionbits with heretical deployments, these will not
    make sense.

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -236,7 +236,6 @@ BASE_SCRIPTS = [
     'mempool_package_onemore.py',
     'rpc_createmultisig.py',
     'mempool_package_limits.py',
-    'feature_versionbits_warning.py',
     'rpc_preciousblock.py',
     'wallet_importprunedfunds.py --legacy-wallet',
     'wallet_importprunedfunds.py --descriptors',
