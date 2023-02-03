6da37b0c84b412a9d79f4142933a6050cd854985
James O'Beirne <james.obeirne@pm.me>
2023-02-03 15:51:54 -0500

    consensus: implement OP_VAULT and OP_VAULT_RECOVER
    OP_VAULT adds a native way of doing vaults - that is, encumbering coins
    in such a way that they must pass through a delay period before being
    successfully spent, aside from a predetermined path (the recovery
    path) which they can be swept to at any time during the life of the
    vault.
    
    Thanks to feedback from
    
    - John Moffett: explicating recursive eval attack and providing a test
      case.
    
    Co-authored-by: Greg Sanders <gsanders87@gmail.com>
    Co-authored-by: Anthony Towns <aj@erisian.com.au>

--- src/policy/policy.h
+++ src/policy/policy.h
@@ -101,7 +101,9 @@ static constexpr unsigned int STANDARD_SCRIPT_VERIFY_FLAGS{MANDATORY_SCRIPT_VERI
                                                              SCRIPT_VERIFY_DISCOURAGE_UPGRADABLE_CHECK_TEMPLATE_VERIFY_HASH |
                                                              SCRIPT_VERIFY_DEFAULT_CHECK_TEMPLATE_VERIFY_HASH |
                                                              SCRIPT_VERIFY_ANYPREVOUT |
-                                                             SCRIPT_VERIFY_VAULT};
+                                                             SCRIPT_VERIFY_VAULT |
+                                                             SCRIPT_VERIFY_VAULT_REPLACEABLE_RECOVERY |
+                                                             SCRIPT_VERIFY_VAULT_UNAUTH_RECOVERY_STRUCTURE};
 
 /** For convenience, standard but not mandatory verify flags. */
 static constexpr unsigned int STANDARD_NOT_MANDATORY_VERIFY_FLAGS{STANDARD_SCRIPT_VERIFY_FLAGS & ~MANDATORY_SCRIPT_VERIFY_FLAGS};
