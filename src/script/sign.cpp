1ec21284d411cb34ec60d772129ac4b5aaaebb70
James O'Beirne <james.obeirne@pm.me>
2023-02-03 15:51:54 -0500

    consensus: implement OP_VAULT and OP_VAULT_RECOVER
    OP_VAULT adds a native way of doing vaults - that is, encumbering coins
    in such a way that they must pass through a delay period before being
    successfully spent, aside from a predetermined path (the recovery
    path) which they can be swept to at any time during the life of the
    vault.
    
    Thanks to feedback from
    
    - John Moffett: explicating recursive eval attack and providing a test
      case.
    
    Co-authored-by: Greg Sanders <gsanders87@gmail.com>
    Co-authored-by: Anthony Towns <aj@erisian.com.au>

--- src/script/sign.cpp
+++ src/script/sign.cpp
@@ -11,6 +11,7 @@
 #include <primitives/transaction.h>
 #include <script/keyorigin.h>
 #include <script/miniscript.h>
+#include <script/script.h>
 #include <script/signingprovider.h>
 #include <script/standard.h>
 #include <uint256.h>
@@ -364,23 +365,6 @@ static bool SignStep(const SigningProvider& provider, const BaseSignatureCreator
         return SignTaproot(provider, creator, WitnessV1Taproot(XOnlyPubKey{vSolutions[0]}), sigdata, ret);
     } // no default case, so the compiler can warn about missing cases
     assert(false);
-}
-
-static CScript PushAll(const std::vector<valtype>& values)
-{
-    CScript result;
-    for (const valtype& v : values) {
-        if (v.size() == 0) {
-            result << OP_0;
-        } else if (v.size() == 1 && v[0] >= 1 && v[0] <= 16) {
-            result << CScript::EncodeOP_N(v[0]);
-        } else if (v.size() == 1 && v[0] == 0x81) {
-            result << OP_1NEGATE;
-        } else {
-            result << v;
-        }
-    }
-    return result;
 }
 
 template<typename M, typename K, typename V>
