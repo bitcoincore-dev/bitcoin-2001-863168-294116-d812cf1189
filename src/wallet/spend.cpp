a891b559c95cc44e951424db3f5df14ba6d23147
Greg Sanders <gsanders87@gmail.com>
2023-03-16 14:58:41 -0400

    Fix fund transaction case at 0-value, 0-fee

--- src/wallet/spend.cpp
+++ src/wallet/spend.cpp
@@ -899,6 +899,12 @@ static util::Result<CreatedTransactionResult> CreateTransactionInternal(
     const CAmount not_input_fees = coin_selection_params.m_effective_feerate.GetFee(coin_selection_params.tx_noinputs_size);
     CAmount selection_target = recipients_sum + not_input_fees;
 
+    // This can only happen if feerate is 0, and requested destinations are value of 0 (e.g. OP_RETURN)
+    // and no pre-selected inputs. This will result in 0-input transaction, which is consensus-invalid anyways
+    if (selection_target == 0 && !coin_control.HasSelected()) {
+        return util::Error{_("Transaction requires one destination of non-0 value, a non-0 feerate, or a pre-selected input")};
+    }
+
     // Get available coins
     auto available_coins = AvailableCoins(wallet,
                                               &coin_control,
