instagibbs/bitcoin

[POLICY] Add OP_TRUE as a standard output type
This will be used in following commits
as the flag for ephemeral anchors, allowing
dust values, but also requiring
the output to be spend in the same relay package.

src/script/script.cpp
@@ -198,6 +198,11 @@ unsigned int CScript::GetSigOpCount(const CScript& scriptSig) const
     return subscript.GetSigOpCount(true);
 }
 
+bool CScript::IsTrue() const
+{
+    return (this->size() == 1 && (*this)[0] == OP_TRUE);
+}
+
 bool CScript::IsPayToBareDefaultCheckTemplateVerifyHash() const
 {
     // Extra-fast test for pay-to-bare-default-check-template-verify-hash CScripts:
@@ -205,6 +210,7 @@ bool CScript::IsPayToBareDefaultCheckTemplateVerifyHash() const
             (*this)[0] == 0x20 &&
             (*this)[33] == OP_CHECKTEMPLATEVERIFY);
 }
+
 bool CScript::IsPayToScriptHash() const
 {
     // Extra-fast test for pay-to-script-hash CScripts:

bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

src/script/script.cpp
@@ -198,6 +198,11 @@ unsigned int CScript::GetSigOpCount(const CScript& scriptSig) const
     return subscript.GetSigOpCount(true);
 }
 
+bool CScript::IsPayToAnchor() const
+{
+    return (this->size() == 1 && (*this)[0] == OP_TRUE);
+}
+
 bool CScript::IsPayToBareDefaultCheckTemplateVerifyHash() const
 {
     // Extra-fast test for pay-to-bare-default-check-template-verify-hash CScripts:
@@ -205,6 +210,7 @@ bool CScript::IsPayToBareDefaultCheckTemplateVerifyHash() const
             (*this)[0] == 0x20 &&
             (*this)[33] == OP_CHECKTEMPLATEVERIFY);
 }
+
 bool CScript::IsPayToScriptHash() const
 {
     // Extra-fast test for pay-to-script-hash CScripts: