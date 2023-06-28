malandro0/bitcoin

RPC/Wallet: Hacky fix for getbalance bugs

src/wallet/rpc/coins.cpp
@@ -165,8 +165,8 @@ RPCHelpMan getbalance()
                 "The available balance is what the wallet considers currently spendable, and is\n"
                 "thus affected by options which limit spendability such as -spendzeroconfchange.\n",
                 {
-                    {"dummy", RPCArg::Type::STR, RPCArg::Optional::OMITTED_NAMED_ARG, "Remains for backward compatibility. Must be excluded or set to \"*\"."},
-                    {"minconf", RPCArg::Type::NUM, RPCArg::Default{0}, "Only include transactions confirmed at least this many times."},
+                    {"dummy|account", RPCArg::Type::STR, RPCArg::Optional::OMITTED_NAMED_ARG, "Remains for backward compatibility. Must be excluded or set to \"*\"."},
+                    {"minconf", RPCArg::Type::NUM, RPCArg::Default{1}, "Only include transactions confirmed at least this many times. (Requires dummy=\"*\")"},
                     {"include_watchonly", RPCArg::Type::BOOL, RPCArg::DefaultHint{"true for watch-only wallets, otherwise false"}, "Also include balance in watch-only addresses (see 'importaddress')"},
                     {"avoid_reuse", RPCArg::Type::BOOL, RPCArg::Default{true}, "(only available if avoid_reuse wallet flag is set) Do not include balance in dirty outputs; addresses are considered dirty if they have previously been used in a transaction."},
                 },
@@ -198,6 +198,10 @@ RPCHelpMan getbalance()
     }
 
     int min_depth = 0;
+    if (!dummy_value.isNull()) {
+        // Default min_depth to 1 when dummy="*"
+        min_depth = 1;
+    }
     if (!request.params[1].isNull()) {
         min_depth = request.params[1].getInt<int>();
     }
@@ -208,6 +212,15 @@ RPCHelpMan getbalance()
 
     const auto bal = GetBalance(*pwallet, min_depth, avoid_reuse);
 
+    if (!dummy_value.isNull()) {
+        isminefilter filter = ISMINE_SPENDABLE;
+        if (include_watchonly) filter = filter | ISMINE_WATCH_ONLY;
+        return ValueFromAmount(pwallet->GetLegacyBalance(filter, min_depth));
+    }
+
+    if (!request.params[1].isNull()) {
+        throw JSONRPCError(RPC_INVALID_PARAMETER, "getbalance minconf option is only currently supported if dummy is set to \"*\"");
+    }
     return ValueFromAmount(bal.m_mine_trusted + (include_watchonly ? bal.m_watchonly_trusted : 0));
 },
     };

bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

src/wallet/rpc/coins.cpp
@@ -166,8 +166,8 @@ RPCHelpMan getbalance()
                 "The available balance is what the wallet considers currently spendable, and is\n"
                 "thus affected by options which limit spendability such as -spendzeroconfchange.\n",
                 {
-                    {"dummy", RPCArg::Type::STR, RPCArg::Optional::OMITTED, "Remains for backward compatibility. Must be excluded or set to \"*\"."},
-                    {"minconf", RPCArg::Type::NUM, RPCArg::Default{0}, "Only include transactions confirmed at least this many times."},
+                    {"dummy|account", RPCArg::Type::STR, RPCArg::Optional::OMITTED, "Remains for backward compatibility. Must be excluded or set to \"*\"."},
+                    {"minconf", RPCArg::Type::NUM, RPCArg::Default{1}, "Only include transactions confirmed at least this many times. (Requires dummy=\"*\")"},
                     {"include_watchonly", RPCArg::Type::BOOL, RPCArg::DefaultHint{"true for watch-only wallets, otherwise false"}, "Also include balance in watch-only addresses (see 'importaddress')"},
                     {"avoid_reuse", RPCArg::Type::BOOL, RPCArg::Default{true}, "(only available if avoid_reuse wallet flag is set) Do not include balance in dirty outputs; addresses are considered dirty if they have previously been used in a transaction."},
                 },
@@ -199,6 +199,10 @@ RPCHelpMan getbalance()
     }
 
     int min_depth = 0;
+    if (!dummy_value.isNull()) {
+        // Default min_depth to 1 when dummy="*"
+        min_depth = 1;
+    }
     if (!request.params[1].isNull()) {
         min_depth = request.params[1].getInt<int>();
     }
@@ -209,6 +213,15 @@ RPCHelpMan getbalance()
 
     const auto bal = GetBalance(*pwallet, min_depth, avoid_reuse);
 
+    if (!dummy_value.isNull()) {
+        isminefilter filter = ISMINE_SPENDABLE;
+        if (include_watchonly) filter = filter | ISMINE_WATCH_ONLY;
+        return ValueFromAmount(pwallet->GetLegacyBalance(filter, min_depth));
+    }
+
+    if (!request.params[1].isNull()) {
+        throw JSONRPCError(RPC_INVALID_PARAMETER, "getbalance minconf option is only currently supported if dummy is set to \"*\"");
+    }
     return ValueFromAmount(bal.m_mine_trusted + (include_watchonly ? bal.m_watchonly_trusted : 0));
 },
     };