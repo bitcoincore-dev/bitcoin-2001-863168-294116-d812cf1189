faff1840d4d6bb6c0ce49b44ea76aa53d58bde12
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2023-06-30 16:29:45 +0200

    Return XOR CAutoFile from BlockManager::Open*File()
    This is a refactor, because the XOR key is empty.

--- src/node/blockstorage.cpp
+++ src/node/blockstorage.cpp
@@ -800,13 +800,13 @@ FlatFileSeq BlockManager::UndoFileSeq() const
 
 CAutoFile BlockManager::OpenBlockFile(const FlatFilePos& pos, bool fReadOnly) const
 {
-    return CAutoFile{BlockFileSeq().Open(pos, fReadOnly), CLIENT_VERSION};
+    return CAutoFile{BlockFileSeq().Open(pos, fReadOnly), CLIENT_VERSION, m_xor_key};
 }
 
 /** Open an undo file (rev?????.dat) */
 CAutoFile BlockManager::OpenUndoFile(const FlatFilePos& pos, bool fReadOnly) const
 {
-    return CAutoFile{UndoFileSeq().Open(pos, fReadOnly), CLIENT_VERSION};
+    return CAutoFile{UndoFileSeq().Open(pos, fReadOnly), CLIENT_VERSION, m_xor_key};
 }
 
 fs::path BlockManager::GetBlockPosFilename(const FlatFilePos& pos) const
@@ -1103,6 +1103,12 @@ FlatFilePos BlockManager::SaveBlockToDisk(const CBlock& block, int nHeight, cons
     }
     return blockPos;
 }
+
+BlockManager::BlockManager(const util::SignalInterrupt& interrupt, Options opts)
+    : m_prune_mode{opts.prune_target > 0},
+      m_xor_key{},
+      m_opts{std::move(opts)},
+      m_interrupt{interrupt} {}
 
 class ImportingNow
 {
