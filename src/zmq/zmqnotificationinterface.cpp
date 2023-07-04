11269eeb1d4dea40346b5b47e9395ba8aaa3503c
Luke Dashjr <luke-jr+git@utopios.org>
2023-07-04 17:42:27 +0000

    Bugfix: ZMQ: Leave notifiers intact rather than shut them down if they fail a notification once
    Whatever the cause of the failure, it makes more sense to keep trying rather than kill the notifier entirely
    
    Github-Pull: #28029
    Rebased-From: 351ae1ee4e5fa94331ed500138808423ca6d324e

--- src/zmq/zmqnotificationinterface.cpp
+++ src/zmq/zmqnotificationinterface.cpp
@@ -124,16 +124,11 @@ void CZMQNotificationInterface::Shutdown()
 namespace {
 
 template <typename Function>
-void TryForEachAndRemoveFailed(std::list<std::unique_ptr<CZMQAbstractNotifier>>& notifiers, const Function& func)
+void TryForEachAndIgnoreFailed(std::list<std::unique_ptr<CZMQAbstractNotifier>>& notifiers, const Function& func)
 {
-    for (auto i = notifiers.begin(); i != notifiers.end(); ) {
+    for (auto i = notifiers.begin(); i != notifiers.end(); ++i) {
         CZMQAbstractNotifier* notifier = i->get();
-        if (func(notifier)) {
-            ++i;
-        } else {
-            notifier->Shutdown();
-            i = notifiers.erase(i);
-        }
+        func(notifier);
     }
 }
 
@@ -144,7 +139,7 @@ void CZMQNotificationInterface::UpdatedBlockTip(const CBlockIndex *pindexNew, co
     if (fInitialDownload || pindexNew == pindexFork) // In IBD or blocks were disconnected without any new ones
         return;
 
-    TryForEachAndRemoveFailed(notifiers, [pindexNew](CZMQAbstractNotifier* notifier) {
+    TryForEachAndIgnoreFailed(notifiers, [pindexNew](CZMQAbstractNotifier* notifier) {
         return notifier->NotifyBlock(pindexNew);
     });
 }
@@ -153,7 +148,7 @@ void CZMQNotificationInterface::TransactionAddedToMempool(const CTransactionRef&
 {
     const CTransaction& tx = *ptx;
 
-    TryForEachAndRemoveFailed(notifiers, [&tx, mempool_sequence](CZMQAbstractNotifier* notifier) {
+    TryForEachAndIgnoreFailed(notifiers, [&tx, mempool_sequence](CZMQAbstractNotifier* notifier) {
         return notifier->NotifyTransaction(tx) && notifier->NotifyTransactionAcceptance(tx, mempool_sequence);
     });
 }
@@ -163,7 +158,7 @@ void CZMQNotificationInterface::TransactionRemovedFromMempool(const CTransaction
     // Called for all non-block inclusion reasons
     const CTransaction& tx = *ptx;
 
-    TryForEachAndRemoveFailed(notifiers, [&tx, mempool_sequence](CZMQAbstractNotifier* notifier) {
+    TryForEachAndIgnoreFailed(notifiers, [&tx, mempool_sequence](CZMQAbstractNotifier* notifier) {
         return notifier->NotifyTransactionRemoval(tx, mempool_sequence);
     });
 }
@@ -172,13 +167,13 @@ void CZMQNotificationInterface::BlockConnected(const std::shared_ptr<const CBloc
 {
     for (const CTransactionRef& ptx : pblock->vtx) {
         const CTransaction& tx = *ptx;
-        TryForEachAndRemoveFailed(notifiers, [&tx](CZMQAbstractNotifier* notifier) {
+        TryForEachAndIgnoreFailed(notifiers, [&tx](CZMQAbstractNotifier* notifier) {
             return notifier->NotifyTransaction(tx);
         });
     }
 
     // Next we notify BlockConnect listeners for *all* blocks
-    TryForEachAndRemoveFailed(notifiers, [pindexConnected](CZMQAbstractNotifier* notifier) {
+    TryForEachAndIgnoreFailed(notifiers, [pindexConnected](CZMQAbstractNotifier* notifier) {
         return notifier->NotifyBlockConnect(pindexConnected);
     });
 }
@@ -187,13 +182,13 @@ void CZMQNotificationInterface::BlockDisconnected(const std::shared_ptr<const CB
 {
     for (const CTransactionRef& ptx : pblock->vtx) {
         const CTransaction& tx = *ptx;
-        TryForEachAndRemoveFailed(notifiers, [&tx](CZMQAbstractNotifier* notifier) {
+        TryForEachAndIgnoreFailed(notifiers, [&tx](CZMQAbstractNotifier* notifier) {
             return notifier->NotifyTransaction(tx);
         });
     }
 
     // Next we notify BlockDisconnect listeners for *all* blocks
-    TryForEachAndRemoveFailed(notifiers, [pindexDisconnected](CZMQAbstractNotifier* notifier) {
+    TryForEachAndIgnoreFailed(notifiers, [pindexDisconnected](CZMQAbstractNotifier* notifier) {
         return notifier->NotifyBlockDisconnect(pindexDisconnected);
     });
 }
