176d369180dc5b9f6f97fcf61a58589f6893cce9
Ryan Ofsky <ryan@ofsky.org>
2023-07-07 17:32:54 -0400

    refactor: Remove call to StartShutdown from qt
    Use interfaces::Node object instead.

--- src/qt/winshutdownmonitor.cpp
+++ src/qt/winshutdownmonitor.cpp
@@ -5,7 +5,7 @@
 #include <qt/winshutdownmonitor.h>
 
 #if defined(Q_OS_WIN)
-#include <shutdown.h>
+#include <interfaces/node.h>
 
 #include <windows.h>
 
@@ -25,7 +25,7 @@ bool WinShutdownMonitor::nativeEventFilter(const QByteArray &eventType, void *pM
            {
                // Initiate a client shutdown after receiving a WM_QUERYENDSESSION and block
                // Windows session end until we have finished client shutdown.
-               StartShutdown();
+               m_node.startShutdown();
                *pnResult = FALSE;
                return true;
            }
