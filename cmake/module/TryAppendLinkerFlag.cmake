dcaca5cc9ba4228a867971195489cf795724e4ad
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2023-07-16 18:42:53 +0100

    cmake: Add `TryAppendLinkerFlag` module

---
+++ cmake/module/TryAppendLinkerFlag.cmake
@@ -0,0 +1,56 @@
+# Copyright (c) 2023 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+include(CheckCXXSourceCompiles)
+
+function(try_append_linker_flag flags_var flag)
+  cmake_parse_arguments(PARSE_ARGV 2
+    TALF                              # prefix
+    ""                                # options
+    "SOURCE;RESULT_VAR"               # one_value_keywords
+    "IF_CHECK_PASSED;IF_CHECK_FAILED" # multi_value_keywords
+  )
+
+  string(MAKE_C_IDENTIFIER "${flag}" result)
+  string(TOUPPER "${result}" result)
+  set(result "LINKER_SUPPORTS_${result}")
+  unset(${result})
+  if(CMAKE_VERSION VERSION_LESS 3.14)
+    set(linker_flags_var CMAKE_REQUIRED_LIBRARIES)
+  else()
+    set(linker_flags_var CMAKE_REQUIRED_LINK_OPTIONS)
+  endif()
+
+  if(MSVC)
+    set(${linker_flags_var} ${flag} /WX)
+  elseif(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
+    set(${linker_flags_var} ${flag} -Wl,-fatal_warnings)
+  else()
+    set(${linker_flags_var} ${flag} -Wl,--fatal-warnings)
+  endif()
+
+  if(TALF_SOURCE)
+    set(source "${TALF_SOURCE}")
+    unset(${result} CACHE)
+  else()
+    set(source "int main() { return 0; }")
+  endif()
+
+  # This forces running a linker.
+  set(CMAKE_TRY_COMPILE_TARGET_TYPE EXECUTABLE)
+  check_cxx_source_compiles("${source}" ${result})
+
+  if(${result})
+    if(DEFINED TALF_IF_CHECK_PASSED)
+      string(STRIP "${${flags_var}} ${TALF_IF_CHECK_PASSED}" ${flags_var})
+    else()
+      string(STRIP "${${flags_var}} ${flag}" ${flags_var})
+    endif()
+  elseif(DEFINED TALF_IF_CHECK_FAILED)
+    string(STRIP "${${flags_var}} ${TALF_IF_CHECK_FAILED}" ${flags_var})
+  endif()
+
+  set(${flags_var} "${${flags_var}}" PARENT_SCOPE)
+  set(${TALF_RESULT_VAR} "${${result}}" PARENT_SCOPE)
+endfunction()
