0df8a45225fca4ccff77251602066828161c4c8d
Luke Dashjr <luke-jr+git@utopios.org>
2023-08-01 18:41:06 +0000

    RPC: Add support for ignore_rejects in testmempoolaccept

--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -127,6 +127,7 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "sendrawtransaction", 3, "ignore_rejects" },
     { "testmempoolaccept", 0, "rawtxs" },
     { "testmempoolaccept", 1, "maxfeerate" },
+    { "testmempoolaccept", 2, "ignore_rejects" },
     { "submitpackage", 0, "package" },
     { "combinerawtransaction", 0, "txs" },
     { "fundrawtransaction", 1, "options" },
