8a158c92d38729dcb8cacc0266f14a15c5ae10d9
Pieter Wuille <pieter@wuille.net>
2023-08-21 18:14:52 -0400

    sync: modernize CSemaphore / CSemaphoreGrant

--- src/net.cpp
+++ src/net.cpp
@@ -2860,8 +2860,9 @@ void CConnman::OpenNetworkConnection(const CAddress& addrConnect, bool fCountFai
 
     if (!pnode)
         return;
-    if (grantOutbound)
-        grantOutbound->MoveTo(pnode->grantOutbound);
+    if (grantOutbound) {
+        pnode->grantOutbound = std::move(*grantOutbound);
+    }
 
     m_msgproc->InitializeNode(*pnode, nLocalServices);
     {
