Lajandra/bitcoin

indexes: Rewrite chain sync logic, remove racy init
Add SyncChain method and rewrite Chain::attachChain to register for
notifications correctly so stale notifications aren't received and
indexing Ignore* methods can be dropped

This is also a potential performance improvement since it avoids holding
cs_main while processing indexing notifications.

src/node/chain.cpp
@@ -3,8 +3,12 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <chain.h>
+#include <chainparams.h>
 #include <interfaces/chain.h>
+#include <node/chain.h>
+#include <threadinterrupt.h>
 #include <uint256.h>
+#include <validation.h>
 
 namespace node {
 interfaces::BlockInfo MakeBlockInfo(const CBlockIndex* index, const CBlock* data)
@@ -19,5 +23,58 @@ interfaces::BlockInfo MakeBlockInfo(const CBlockIndex* index, const CBlock* data
     }
     info.data = data;
     return info;
+}
+
+bool SyncChain(const CChain& chain, const CBlockIndex* block, std::shared_ptr<interfaces::Chain::Notifications> notifications, const CThreadInterrupt& interrupt, std::function<void()> on_sync)
+{
+    auto& consensus_params = Params().GetConsensus();
+    while (true) {
+        AssertLockNotHeld(::cs_main);
+        WAIT_LOCK(::cs_main, main_lock);
+
+        bool rewind = false;
+        if (!block) {
+            block = chain.Genesis();
+        } else if (chain.Contains(block)) {
+            block = chain.Next(block);
+        } else {
+            rewind = true;
+        }
+
+        if (block) {
+            // Release cs_main while reading block data and sending notifications.
+            REVERSE_LOCK(main_lock);
+            interfaces::BlockInfo block_info = MakeBlockInfo(block);
+            block_info.chain_tip = false;
+            CBlock data;
+            if (!ReadBlockFromDisk(data, block, consensus_params)) {
+                block_info.error = strprintf("%s: Failed to read block %s from disk",
+                                             __func__, block->GetBlockHash().ToString());
+            } else {
+                block_info.data = &data;
+            }
+            if (rewind) {
+                notifications->blockDisconnected(block_info);
+                block = Assert(block->pprev);
+            } else {
+                notifications->blockConnected(block_info);
+            }
+        } else {
+            block = chain.Tip();
+        }
+
+        bool synced = block == chain.Tip();
+        if (synced || interrupt) {
+            if (synced && on_sync) on_sync();
+            if (block) {
+                CBlockLocator locator = chain.GetLocator(block);
+                // Release cs_main while calling notification handlers.
+                REVERSE_LOCK(main_lock);
+                if (synced) notifications->blockConnected(MakeBlockInfo(block));
+                notifications->chainStateFlushed(locator);
+            }
+            return synced;
+        }
+    }
 }
 } // namespace node

bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

src/kernel/chain.cpp
@@ -3,9 +3,13 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <chain.h>
+#include <chainparams.h>
 #include <interfaces/chain.h>
+#include <kernel/chain.h>
 #include <sync.h>
 #include <uint256.h>
+#include <util/threadinterrupt.h>
+#include <validation.h>
 
 class CBlock;
 
@@ -23,5 +27,57 @@ interfaces::BlockInfo MakeBlockInfo(const CBlockIndex* index, const CBlock* data
     }
     info.data = data;
     return info;
+}
+
+bool SyncChain(node::BlockManager& blockman, const CChain& chain, const CBlockIndex* block, std::shared_ptr<interfaces::Chain::Notifications> notifications, const CThreadInterrupt& interrupt, std::function<void()> on_sync)
+{
+    while (true) {
+        AssertLockNotHeld(::cs_main);
+        WAIT_LOCK(::cs_main, main_lock);
+
+        bool rewind = false;
+        if (!block) {
+            block = chain.Genesis();
+        } else if (chain.Contains(block)) {
+            block = chain.Next(block);
+        } else {
+            rewind = true;
+        }
+
+        if (block) {
+            // Release cs_main while reading block data and sending notifications.
+            REVERSE_LOCK(main_lock);
+            interfaces::BlockInfo block_info = MakeBlockInfo(block);
+            block_info.chain_tip = false;
+            CBlock data;
+            if (!blockman.ReadBlockFromDisk(data, *block)) {
+                block_info.error = strprintf("%s: Failed to read block %s from disk",
+                                             __func__, block->GetBlockHash().ToString());
+            } else {
+                block_info.data = &data;
+            }
+            if (rewind) {
+                notifications->blockDisconnected(block_info);
+                block = Assert(block->pprev);
+            } else {
+                notifications->blockConnected(block_info);
+            }
+        } else {
+            block = chain.Tip();
+        }
+
+        bool synced = block == chain.Tip();
+        if (synced || interrupt) {
+            if (synced && on_sync) on_sync();
+            if (block) {
+                CBlockLocator locator = ::GetLocator(block);
+                // Release cs_main while calling notification handlers.
+                REVERSE_LOCK(main_lock);
+                if (synced) notifications->blockConnected(MakeBlockInfo(block));
+                notifications->chainStateFlushed(locator);
+            }
+            return synced;
+        }
+    }
 }
 } // namespace kernel