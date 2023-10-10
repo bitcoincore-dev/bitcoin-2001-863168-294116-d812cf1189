08245ef1d680843d6e38b89c5c59bc03196a4fe5
Luke Dashjr <luke-jr+git@utopios.org>
2023-10-10 02:00:33 +0000

    node: Optimise getNetLocalAddresses slightly

--- src/node/interfaces.cpp
+++ src/node/interfaces.cpp
@@ -174,6 +174,7 @@ public:
     {
         std::vector<CNetAddr> ret;
         LOCK(g_maplocalhost_mutex);
+        ret.reserve(mapLocalHost.size());
         for (const std::pair<const CNetAddr, LocalServiceInfo> &item : mapLocalHost) {
             ret.emplace_back(item.first);
         }
