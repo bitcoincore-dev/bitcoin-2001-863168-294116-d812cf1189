2a174196edd3fd3ecf13635c776bc01879f9ee0a
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2023-11-06 10:59:21 +0000

    fixup! cmake: Build `leveldb` static library

--- cmake/leveldb.cmake
+++ cmake/leveldb.cmake
@@ -1,6 +1,6 @@
-# Copyright (c) 2023 The Bitcoin Core developers
+# Copyright (c) 2023-present The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+# file COPYING or https://opensource.org/license/mit/.
 
 # This file is part of the transition from Autotools to CMake. Once CMake
 # support has been merged we should switch to using the upstream CMake
@@ -41,6 +41,8 @@ add_library(leveldb STATIC EXCLUDE_FROM_ALL
   ${PROJECT_SOURCE_DIR}/src/leveldb/util/comparator.cc
   ${PROJECT_SOURCE_DIR}/src/leveldb/util/crc32c.cc
   ${PROJECT_SOURCE_DIR}/src/leveldb/util/env.cc
+  $<$<NOT:$<BOOL:${WIN32}>>:${PROJECT_SOURCE_DIR}/src/leveldb/util/env_posix.cc>
+  $<$<BOOL:${WIN32}>:${PROJECT_SOURCE_DIR}/src/leveldb/util/env_windows.cc>
   ${PROJECT_SOURCE_DIR}/src/leveldb/util/filter_policy.cc
   ${PROJECT_SOURCE_DIR}/src/leveldb/util/hash.cc
   ${PROJECT_SOURCE_DIR}/src/leveldb/util/histogram.cc
@@ -49,14 +51,6 @@ add_library(leveldb STATIC EXCLUDE_FROM_ALL
   ${PROJECT_SOURCE_DIR}/src/leveldb/util/status.cc
   ${PROJECT_SOURCE_DIR}/src/leveldb/helpers/memenv/memenv.cc
 )
-if(WIN32)
-  target_sources(leveldb PRIVATE ${PROJECT_SOURCE_DIR}/src/leveldb/util/env_windows.cc)
-  set_property(SOURCE ${PROJECT_SOURCE_DIR}/src/leveldb/util/env_windows.cc
-    APPEND PROPERTY COMPILE_OPTIONS $<$<AND:$<CXX_COMPILER_ID:MSVC>,$<CONFIG:Release>>:/wd4722>
-  )
-else()
-  target_sources(leveldb PRIVATE ${PROJECT_SOURCE_DIR}/src/leveldb/util/env_posix.cc)
-endif()
 
 target_compile_definitions(leveldb
   PRIVATE
@@ -67,19 +61,11 @@ target_compile_definitions(leveldb
     HAVE_O_CLOEXEC=$<BOOL:${HAVE_O_CLOEXEC}>
     FALLTHROUGH_INTENDED=[[fallthrough]]
     LEVELDB_IS_BIG_ENDIAN=${WORDS_BIGENDIAN}
+    $<$<NOT:$<BOOL:${WIN32}>>:LEVELDB_PLATFORM_POSIX>
+    $<$<BOOL:${WIN32}>:LEVELDB_PLATFORM_WINDOWS>
+    $<$<BOOL:${WIN32}>:_UNICODE;UNICODE>
+    $<$<BOOL:${MINGW}>:__USE_MINGW_ANSI_STDIO=1>
 )
-
-if(WIN32)
-  target_compile_definitions(leveldb
-    PRIVATE
-      LEVELDB_PLATFORM_WINDOWS
-      _UNICODE
-      UNICODE
-      __USE_MINGW_ANSI_STDIO=1
-  )
-else()
-  target_compile_definitions(leveldb PRIVATE LEVELDB_PLATFORM_POSIX)
-endif()
 
 target_include_directories(leveldb
   PRIVATE
@@ -87,6 +73,20 @@ target_include_directories(leveldb
   PUBLIC
     $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/src/leveldb/include>
 )
+
+if(MSVC)
+  target_compile_options(leveldb
+    PRIVATE
+      /wd4244
+      /wd4267
+      $<$<CONFIG:Release>:/wd4722>
+  )
+  target_compile_definitions(leveldb
+    PRIVATE
+      _CRT_NONSTDC_NO_DEPRECATE
+      _CRT_SECURE_NO_WARNINGS
+  )
+endif()
 
 #TODO: figure out how to filter out:
 # -Wconditional-uninitialized -Werror=conditional-uninitialized -Wsuggest-override -Werror=suggest-override
