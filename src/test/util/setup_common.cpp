a92e2f8902ec0a5221338b7397c479810a9d6405
Luke Dashjr <luke-jr+git@utopios.org>
2023-11-24 18:30:51 +0000

    Diff-minimise

--- src/test/util/setup_common.cpp
+++ src/test/util/setup_common.cpp
@@ -44,7 +44,6 @@
 #include <streams.h>
 #include <test/util/net.h>
 #include <test/util/random.h>
-#include <test/util/transaction_utils.h>
 #include <test/util/txmempool.h>
 #include <timedata.h>
 #include <txdb.h>
