bb002ae3c117690112c02b1c928b567cefe4945b
pablomartin4btc <pablomartin4btc@gmail.com>
2023-12-06 18:48:03 -0300

    qt: update widgets availability on wallet selection
    The Transaction View should be only enabled when a wallet is selected.
    Therefore it has been added a condition for a selected wallet on
    enableHistoryAction() since its availability also depends on the mask
    value checkbox.
    
    Github-Pull: gui#780
    Rebased-From: b2e531e70a88f5c9e1c055ae7341520a3128e15d

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -671,8 +671,10 @@ void BitcoinGUI::setClientModel(ClientModel *_clientModel, interfaces::BlockAndH
 #ifdef ENABLE_WALLET
 void BitcoinGUI::enableHistoryAction(bool privacy)
 {
-    historyAction->setEnabled(!privacy);
-    if (historyAction->isChecked()) gotoOverviewPage();
+    if (walletFrame->currentWalletModel()) {
+        historyAction->setEnabled(!privacy);
+        if (historyAction->isChecked()) gotoOverviewPage();
+    }
 }
 
 void BitcoinGUI::setWalletController(WalletController* wallet_controller)
