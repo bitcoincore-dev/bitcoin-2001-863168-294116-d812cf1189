4563432f0b9d6d060033a63dbe36ef4c4cc1499c
Luke Dashjr <luke-jr+git@utopios.org>
2023-12-27 18:59:38 +0000

    Add maxmempool RPC
    Github-Pull: #21780
    Rebased-From: 040b280c6613ac81a92a324f356f0b4cc205ef8d

--- src/txmempool.h
+++ src/txmempool.h
@@ -437,7 +437,7 @@ public:
 
     using Options = kernel::MemPoolOptions;
 
-    const int64_t m_max_size_bytes;
+    int64_t m_max_size_bytes;
     const std::chrono::seconds m_expiry;
     const CFeeRate m_incremental_relay_feerate;
     const CFeeRate m_min_relay_feerate;
