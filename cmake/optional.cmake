eb145f23f8a1d30e828f95f53a94f1dccceef1b3
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-01-25 16:53:15 +0000

    cmake: Switch from tri-state options to ON/OFF
    The tri-state options were provided for the only purpose to check the
    feature parity with the Autotools-based build system.
    
    Also a new "dev-mode" configure preset suggested.

--- cmake/optional.cmake
+++ cmake/optional.cmake
@@ -6,12 +6,12 @@
 
 if(CCACHE)
   find_program(CCACHE_COMMAND ccache)
+  mark_as_advanced(CCACHE_COMMAND)
   if(CCACHE_COMMAND)
     if(MSVC)
       if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.24)
         # ccache >= 4.8 requires compile batching turned off that is available since CMake 3.24.
         # See https://github.com/ccache/ccache/wiki/MS-Visual-Studio
-        set(CCACHE ON)
         set(MSVC_CCACHE_WRAPPER_CONTENT "\"${CCACHE_COMMAND}\" \"${CMAKE_CXX_COMPILER}\"")
         set(MSVC_CCACHE_WRAPPER_FILENAME wrapped-cl.bat)
         file(WRITE ${CMAKE_BINARY_DIR}/${MSVC_CCACHE_WRAPPER_FILENAME} "${MSVC_CCACHE_WRAPPER_CONTENT} %*")
@@ -21,117 +21,74 @@ if(CCACHE)
           "DebugInformationFormat=OldStyle"
         )
         set(CMAKE_VS_NO_COMPILE_BATCHING ON)
-      elseif(CCACHE STREQUAL "AUTO")
-        message(WARNING "ccache requested and found, but CMake >= 3.24 is required to use it properly. Disabling.\n"
-                        "To skip ccache check, use \"-DCCACHE=OFF\".\n")
-        set(CCACHE OFF)
       else()
         message(FATAL_ERROR "ccache requested and found, but CMake >= 3.24 is required to use it properly.")
       endif()
     else()
-      set(CCACHE ON)
       list(APPEND CMAKE_C_COMPILER_LAUNCHER ${CCACHE_COMMAND})
       list(APPEND CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE_COMMAND})
     endif()
-  elseif(CCACHE STREQUAL "AUTO")
-    set(CCACHE OFF)
   else()
     message(FATAL_ERROR "ccache requested, but not found.")
   endif()
-  mark_as_advanced(CCACHE_COMMAND)
 endif()
 
 if(WITH_NATPMP)
-  find_package(NATPMP MODULE)
-  if(NATPMP_FOUND)
-    set(WITH_NATPMP ON)
-  elseif(WITH_NATPMP STREQUAL "AUTO")
-    message(WARNING "libnatpmp not found, disabling.\n"
-                    "To skip libnatpmp check, use \"-DWITH_NATPMP=OFF\".\n")
-    set(WITH_NATPMP OFF)
-  else()
-    message(FATAL_ERROR "libnatpmp requested, but not found.")
-  endif()
+  find_package(NATPMP MODULE REQUIRED)
 endif()
 
 if(WITH_MINIUPNPC)
-  find_package(MiniUPnPc MODULE)
-  if(MiniUPnPc_FOUND)
-    set(WITH_MINIUPNPC ON)
-  elseif(WITH_MINIUPNPC STREQUAL "AUTO")
-    message(WARNING "libminiupnpc not found, disabling.\n"
-                    "To skip libminiupnpc check, use \"-DWITH_MINIUPNPC=OFF\".\n")
-    set(WITH_MINIUPNPC OFF)
-  else()
-    message(FATAL_ERROR "libminiupnpc requested, but not found.")
-  endif()
+  find_package(MiniUPnPc MODULE REQUIRED)
 endif()
 
 if(WITH_ZMQ)
   if(MSVC)
-    find_package(ZeroMQ CONFIG)
+    find_package(ZeroMQ CONFIG REQUIRED)
   else()
     # The ZeroMQ project has provided config files since v4.2.2.
     # TODO: Switch to find_package(ZeroMQ) at some point in the future.
     include(CrossPkgConfig)
-    cross_pkg_check_modules(libzmq IMPORTED_TARGET libzmq>=4)
-    if(libzmq_FOUND AND TARGET PkgConfig::libzmq)
-      target_compile_definitions(PkgConfig::libzmq INTERFACE
-        $<$<PLATFORM_ID:Windows>:ZMQ_STATIC>
-      )
-      target_link_libraries(PkgConfig::libzmq INTERFACE
-        $<$<PLATFORM_ID:Windows>:iphlpapi;ws2_32>
-      )
-    endif()
-  endif()
-  if(TARGET libzmq OR TARGET PkgConfig::libzmq)
-    set(WITH_ZMQ ON)
-  elseif(WITH_ZMQ STREQUAL "AUTO")
-    message(WARNING "libzmq not found, disabling.\n"
-                    "To skip libzmq check, use \"-DWITH_ZMQ=OFF\".\n")
-    set(WITH_ZMQ OFF)
-  else()
-    message(FATAL_ERROR "libzmq requested, but not found.")
+    cross_pkg_check_modules(libzmq REQUIRED IMPORTED_TARGET libzmq>=4)
+    target_compile_definitions(PkgConfig::libzmq INTERFACE
+      $<$<PLATFORM_ID:Windows>:ZMQ_STATIC>
+    )
+    target_link_libraries(PkgConfig::libzmq INTERFACE
+      $<$<PLATFORM_ID:Windows>:iphlpapi;ws2_32>
+    )
   endif()
 endif()
 
 if(WITH_USDT)
   find_path(SystemTap_INCLUDE_DIR
     NAMES sys/sdt.h
+    REQUIRED
   )
   mark_as_advanced(SystemTap_INCLUDE_DIR)
 
-  if(SystemTap_INCLUDE_DIR)
-    include(CMakePushCheckState)
-    cmake_push_check_state(RESET)
+  include(CMakePushCheckState)
+  cmake_push_check_state(RESET)
+  include(CheckCXXSourceCompiles)
+  set(CMAKE_REQUIRED_INCLUDES ${SystemTap_INCLUDE_DIR})
+  check_cxx_source_compiles("
+    #include <sys/sdt.h>
 
-    include(CheckCXXSourceCompiles)
-    set(CMAKE_REQUIRED_INCLUDES ${SystemTap_INCLUDE_DIR})
-    check_cxx_source_compiles("
-      #include <sys/sdt.h>
-
-      int main()
-      {
-        DTRACE_PROBE(context, event);
-        int a, b, c, d, e, f, g;
-        DTRACE_PROBE7(context, event, a, b, c, d, e, f, g);
-      }
-      " HAVE_USDT_H
-    )
-
-    cmake_pop_check_state()
-  endif()
+    int main()
+    {
+      DTRACE_PROBE(context, event);
+      int a, b, c, d, e, f, g;
+      DTRACE_PROBE7(context, event, a, b, c, d, e, f, g);
+    }
+    " HAVE_USDT_H
+  )
+  cmake_pop_check_state()
 
   if(HAVE_USDT_H)
     target_include_directories(core_interface INTERFACE
       ${SystemTap_INCLUDE_DIR}
     )
     set(ENABLE_TRACING TRUE)
-    set(WITH_USDT ON)
-  elseif(WITH_USDT STREQUAL "AUTO")
-    set(WITH_USDT OFF)
   else()
-    message(FATAL_ERROR "sys/sdt.h requested, but not found.")
+    message(FATAL_ERROR "sys/sdt.h found, but could not be compiled.")
   endif()
 endif()
 
@@ -139,42 +96,25 @@ if(ENABLE_WALLET)
   if(WITH_SQLITE)
     if(VCPKG_TARGET_TRIPLET)
       # Use of the `unofficial::` namespace is a vcpkg package manager convention.
-      find_package(unofficial-sqlite3 CONFIG)
+      find_package(unofficial-sqlite3 CONFIG REQUIRED)
     else()
-      find_package(SQLite3 3.7.17)
+      find_package(SQLite3 3.7.17 REQUIRED)
     endif()
-    if(TARGET unofficial::sqlite3::sqlite3 OR TARGET SQLite::SQLite3)
-      set(WITH_SQLITE ON)
-      set(USE_SQLITE ON)
-    elseif(WITH_SQLITE STREQUAL "AUTO")
-      set(WITH_SQLITE OFF)
-    else()
-      message(FATAL_ERROR "SQLite requested, but not found.")
-    endif()
+    set(USE_SQLITE ON)
   endif()
 
   if(WITH_BDB)
-    find_package(BerkeleyDB 4.8 MODULE)
-    if(BerkeleyDB_FOUND)
-      set(WITH_BDB ON)
-      set(USE_BDB ON)
-      if(NOT BerkeleyDB_VERSION VERSION_EQUAL 4.8)
-        message(WARNING "Found Berkeley DB (BDB) other than 4.8.")
-        if(WARN_INCOMPATIBLE_BDB)
-          message(WARNING "BDB (legacy) wallets opened by this build would not be portable!\n"
-                          "If this is intended, pass \"-DWARN_INCOMPATIBLE_BDB=OFF\".\n"
-                          "Passing \"-DWITH_BDB=OFF\" will suppress this warning.\n")
-        else()
-          message(WARNING "BDB (legacy) wallets opened by this build will not be portable!")
-        endif()
+    find_package(BerkeleyDB 4.8 MODULE REQUIRED)
+    set(USE_BDB ON)
+    if(NOT BerkeleyDB_VERSION VERSION_EQUAL 4.8)
+      message(WARNING "Found Berkeley DB (BDB) other than 4.8.")
+      if(WARN_INCOMPATIBLE_BDB)
+        message(WARNING "BDB (legacy) wallets opened by this build would not be portable!\n"
+                        "If this is intended, pass \"-DWARN_INCOMPATIBLE_BDB=OFF\".\n"
+                        "Passing \"-DWITH_BDB=OFF\" will suppress this warning.\n")
+      else()
+        message(WARNING "BDB (legacy) wallets opened by this build will not be portable!")
       endif()
-    else()
-      message(WARNING "Berkeley DB (BDB) required for legacy wallet support, but not found.\n"
-                      "Passing \"-DWITH_BDB=OFF\" will suppress this warning.\n")
-      set(WITH_BDB OFF)
     endif()
   endif()
-else()
-  set(WITH_SQLITE OFF)
-  set(WITH_BDB OFF)
 endif()
