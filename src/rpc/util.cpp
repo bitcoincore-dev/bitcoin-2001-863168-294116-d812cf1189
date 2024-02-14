ea064f73bb19d20a03fa3249884511d25106e9f2
MarcoFalke <*~=`'#}+{/-|&$^_@721217.xyz>
2024-02-14 17:13:34 +0100

    rpc: Add ParseFeeRate helper
    Github-Pull: #29434
    Rebased-From: fade94d11a5b93113975c4b2f62a357a70d03191

--- src/rpc/util.cpp
+++ src/rpc/util.cpp
@@ -74,6 +74,13 @@ CAmount AmountFromValue(const UniValue& value, int decimals)
     return amount;
 }
 
+CFeeRate ParseFeeRate(const UniValue& json)
+{
+    CAmount val{AmountFromValue(json)};
+    if (val >= COIN) throw JSONRPCError(RPC_INVALID_PARAMETER, "Fee rates larger than or equal to 1BTC/kvB are not accepted");
+    return CFeeRate{val};
+}
+
 uint256 ParseHashV(const UniValue& v, std::string strName)
 {
     const std::string& strHex(v.get_str());
