213a4f3f163ccdcd97e8d7df5aec5d68e1ad24f0
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-02-24 13:42:55 +0000

    cmake: Add external signer support

--- cmake/module/AddBoostIfNeeded.cmake
+++ cmake/module/AddBoostIfNeeded.cmake
@@ -19,6 +19,7 @@ function(add_boost_if_needed)
 
   set(Boost_NO_BOOST_CMAKE ON)
   find_package(Boost 1.73.0 REQUIRED)
+  mark_as_advanced(Boost_INCLUDE_DIR)
   set_target_properties(Boost::headers PROPERTIES IMPORTED_GLOBAL TRUE)
   target_compile_definitions(Boost::headers INTERFACE
     # We don't use multi_index serialization.
@@ -48,5 +49,54 @@ function(add_boost_if_needed)
     endif()
   endif()
 
-  mark_as_advanced(Boost_INCLUDE_DIR)
+  if(WITH_EXTERNAL_SIGNER)
+    include(CheckCXXSourceCompiles)
+    set(CMAKE_REQUIRED_INCLUDES ${Boost_INCLUDE_DIR})
+    # Boost 1.78 requires the following workaround.
+    # See: https://github.com/boostorg/process/issues/235
+    set(CMAKE_REQUIRED_FLAGS "-Wno-error=narrowing")
+    # Boost 1.73 requires the following workaround on systems with
+    # libc<2.34 only, as newer libc has integrated the functionality
+    # of the libpthread library.
+    set(CMAKE_REQUIRED_LIBRARIES Threads::Threads)
+    check_cxx_source_compiles("
+      #define BOOST_PROCESS_USE_STD_FS
+      #include <boost/process.hpp>
+      #include <string>
+
+      int main()
+      {
+        namespace bp = boost::process;
+        bp::opstream stdin_stream;
+        bp::ipstream stdout_stream;
+        bp::child c(\"dummy\", bp::std_out > stdout_stream, bp::std_err > stdout_stream, bp::std_in < stdin_stream);
+        stdin_stream << std::string{\"test\"} << std::endl;
+        if (c.running()) c.terminate();
+        c.wait();
+        c.exit_code();
+      }
+      " HAVE_BOOST_PROCESS
+    )
+    if(HAVE_BOOST_PROCESS)
+      if(WIN32)
+        # Boost Process for Windows uses Boost ASIO. Boost ASIO performs
+        # pre-main init of Windows networking libraries, which we do not
+        # want.
+        set(WITH_EXTERNAL_SIGNER OFF PARENT_SCOPE)
+        set(ENABLE_EXTERNAL_SIGNER OFF PARENT_SCOPE)
+      else()
+        set(WITH_EXTERNAL_SIGNER ON PARENT_SCOPE)
+        set(ENABLE_EXTERNAL_SIGNER ON PARENT_SCOPE)
+        target_compile_definitions(Boost::headers INTERFACE
+          BOOST_PROCESS_USE_STD_FS
+        )
+      endif()
+    elseif(WITH_EXTERNAL_SIGNER STREQUAL "AUTO")
+      set(WITH_EXTERNAL_SIGNER OFF PARENT_SCOPE)
+      set(ENABLE_EXTERNAL_SIGNER OFF PARENT_SCOPE)
+    else()
+      message(FATAL_ERROR "External signing is not supported for this Boost version.")
+    endif()
+  endif()
+
 endfunction()
