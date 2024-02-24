25c493e4a998079ad954e2100d78bbfc1b9f0ebc
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-02-24 13:42:55 +0000

    cmake: Add external signer support

--- cmake/module/AddBoostIfNeeded.cmake
+++ cmake/module/AddBoostIfNeeded.cmake
@@ -19,6 +19,7 @@ function(add_boost_if_needed)
 
   set(Boost_NO_BOOST_CMAKE ON)
   find_package(Boost 1.73.0 REQUIRED)
+  mark_as_advanced(Boost_INCLUDE_DIR)
   set_target_properties(Boost::headers PROPERTIES IMPORTED_GLOBAL TRUE)
   target_compile_definitions(Boost::headers INTERFACE
     # We don't use multi_index serialization.
@@ -54,5 +55,4 @@ function(add_boost_if_needed)
     endif()
   endif()
 
-  mark_as_advanced(Boost_INCLUDE_DIR)
 endfunction()
