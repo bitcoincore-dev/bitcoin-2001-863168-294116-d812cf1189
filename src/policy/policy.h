a24ecc5a7b8207567315d1ff8a3729a84df1e615
Luke Dashjr <luke-jr+git@utopios.org>
2024-02-28 18:54:23 +0000

    Default policy: Set permitbarepubkey=0 (corepolicy resets to 1)

--- src/policy/policy.h
+++ src/policy/policy.h
@@ -50,7 +50,7 @@ static constexpr unsigned int DEFAULT_BYTES_PER_SIGOP_STRICT{20};
 /** Default for -datacarriercost (multiplied by WITNESS_SCALE_FACTOR) */
 static constexpr unsigned int DEFAULT_WEIGHT_PER_DATA_BYTE{4};
 /** Default for -permitbarepubkey */
-static constexpr bool DEFAULT_PERMIT_BAREPUBKEY{true};
+static constexpr bool DEFAULT_PERMIT_BAREPUBKEY{false};
 /** Default for -permitbaremultisig */
 static constexpr bool DEFAULT_PERMIT_BAREMULTISIG{false};
 /** The maximum number of witness stack items in a standard P2WSH script */
