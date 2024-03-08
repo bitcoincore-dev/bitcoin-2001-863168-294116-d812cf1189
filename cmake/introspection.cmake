3cb2e65a3b0606eba8ad16dd75d527b427ec0cb1
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-03-08 21:00:38 +0000

    fixup! cmake: Check system headers

--- cmake/introspection.cmake
+++ cmake/introspection.cmake
@@ -10,9 +10,6 @@ include(TestBigEndian)
 test_big_endian(WORDS_BIGENDIAN)
 
 # The following HAVE_{HEADER}_H variables go to the bitcoin-config.h header.
-check_include_file_cxx(byteswap.h HAVE_BYTESWAP_H)
-check_include_file_cxx(endian.h HAVE_ENDIAN_H)
-check_include_file_cxx(sys/endian.h HAVE_SYS_ENDIAN_H)
 check_include_file_cxx(sys/prctl.h HAVE_SYS_PRCTL_H)
 check_include_file_cxx(sys/resources.h HAVE_SYS_RESOURCES_H)
 check_include_file_cxx(sys/vmmeter.h HAVE_SYS_VMMETER_H)
