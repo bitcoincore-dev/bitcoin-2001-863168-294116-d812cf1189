bitcoin/bitcoin

GUI: Add getTorInfo to ClientModel

src/qt/clientmodel.cpp
@@ -294,3 +294,14 @@ bool ClientModel::getProxyInfo(std::string& ip_port) const
     }
     return false;
 }
+
+bool ClientModel::getTorInfo(QString& out_onion) const
+{
+    for (const auto& addr : m_node.getNetLocalAddresses()) {
+        if (addr.IsTor()) {
+            out_onion = QString::fromStdString(addr.ToStringIP());
+            return true;
+        }
+    }
+    return false;
+}

bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

src/qt/clientmodel.cpp
@@ -301,6 +301,17 @@ bool ClientModel::getProxyInfo(std::string& ip_port) const
     return false;
 }
 
+bool ClientModel::getTorInfo(QString& out_onion) const
+{
+    for (const auto& addr : m_node.getNetLocalAddresses()) {
+        if (addr.IsTor()) {
+            out_onion = QString::fromStdString(addr.ToStringAddr());
+            return true;
+        }
+    }
+    return false;
+}
+
 mempoolSamples_t ClientModel::getMempoolStatsInRange(QDateTime &from, QDateTime &to)
 {
     // get stats from the core stats model