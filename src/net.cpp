bitcoin/bitcoin

Revert "net: remove SetMaxOutboundTarget"
This reverts commit 2f3f1aec1f8aadd4a6fb08ca5da7eeda31eb388f.

src/net.cpp
@@ -2920,6 +2920,12 @@ void CConnman::RecordBytesSent(uint64_t bytes)
     nMaxOutboundTotalBytesSentInCycle += bytes;
 }
 
+void CConnman::SetMaxOutboundTarget(uint64_t limit)
+{
+    LOCK(cs_totalBytesSent);
+    nMaxOutboundLimit = limit;
+}
+
 uint64_t CConnman::GetMaxOutboundTarget() const
 {
     LOCK(cs_totalBytesSent);

bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

src/net.cpp
@@ -3589,6 +3589,13 @@ void CConnman::RecordBytesSent(uint64_t bytes)
     nMaxOutboundTotalBytesSentInCycle += bytes;
 }
 
+void CConnman::SetMaxOutboundTarget(uint64_t limit)
+{
+    AssertLockNotHeld(m_total_bytes_sent_mutex);
+    LOCK(m_total_bytes_sent_mutex);
+    nMaxOutboundLimit = limit;
+}
+
 uint64_t CConnman::GetMaxOutboundTarget() const
 {
     AssertLockNotHeld(m_total_bytes_sent_mutex);