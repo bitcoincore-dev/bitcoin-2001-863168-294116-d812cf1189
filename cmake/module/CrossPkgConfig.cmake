ae5bd32039a3cbdf6095600831bfe653a7ba69cd
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-03-19 13:58:45 +0000

    fixup! cmake: Add cross-compiling support
    CMake 3.17:
    - The FindPkgConfig module now extracts include directories prefixed
    with -isystem into the *_INCLUDE_DIRS variables and
    INTERFACE_INCLUDE_DIRECTORIES target properties.

--- cmake/module/CrossPkgConfig.cmake
+++ cmake/module/CrossPkgConfig.cmake
@@ -1,34 +1,8 @@
-# Copyright (c) 2023 The Bitcoin Core developers
+# Copyright (c) 2023-present The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
-# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+# file COPYING or https://opensource.org/license/mit/.
 
 find_package(PkgConfig REQUIRED)
-
-function(remove_isystem_from_include_directories_internal target)
-  #[=[
-  A workaround for https://gitlab.kitware.com/cmake/cmake/-/issues/20652.
-
-  When the pkg-config provides CFLAGS with -isystem options, for instance:
-
-    $ pkg-config --cflags-only-I libzmq
-    -isystem /usr/include/mit-krb5 -I/usr/include/pgm-5.3 -I/usr/include/libxml2
-
-  an old CMake fails to parse them properly and the INTERFACE_INCLUDE_DIRECTORIES
-  property contains "-isystem" as a separated element:
-
-    -isystem;/usr/include/mit-krb5;/usr/include/pgm-5.3;/usr/include/libxml2
-
-  which ends with an error "Imported target includes non-existent path".
-
-  Fixing by removing the "-isystem" element from the INTERFACE_INCLUDE_DIRECTORIES.
-  ]=]
-
-  get_target_property(include_directories ${target} INTERFACE_INCLUDE_DIRECTORIES)
-  if(include_directories)
-    list(REMOVE_ITEM include_directories -isystem)
-    set_target_properties(${target} PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${include_directories}")
-  endif()
-endfunction()
 
 macro(cross_pkg_check_modules prefix)
   if(CMAKE_CROSSCOMPILING)
@@ -39,11 +13,9 @@ macro(cross_pkg_check_modules prefix)
     pkg_check_modules(${prefix} ${ARGN})
     set(ENV{PKG_CONFIG_PATH} ${pkg_config_path_saved})
     set(ENV{PKG_CONFIG_LIBDIR} ${pkg_config_libdir_saved})
+    unset(pkg_config_path_saved)
+    unset(pkg_config_libdir_saved)
   else()
     pkg_check_modules(${prefix} ${ARGN})
-  endif()
-
-  if(CMAKE_VERSION VERSION_LESS 3.17.3 AND TARGET PkgConfig::${prefix})
-    remove_isystem_from_include_directories_internal(PkgConfig::${prefix})
   endif()
 endmacro()
