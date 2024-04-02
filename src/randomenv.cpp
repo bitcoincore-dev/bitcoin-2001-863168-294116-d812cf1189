4d8ad853ee914ebfc6442f02dda9c626b4c037ce
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-04-02 12:08:57 +0100

    Drop Windows Socket dependency for `randomenv.cpp`
    The current implementation has two issues.
    1. The `gethostname` call requires the Winsock DLL being initiated in
    advance, which is not always guaranteed, for example, in the kernel
    library.
    2. It introduces a dependency on the ws2_32 library for our
    libbitcoinkernel, which may not be desirable.

--- src/randomenv.cpp
+++ src/randomenv.cpp
@@ -357,10 +357,21 @@ void RandAddStaticEnv(CSHA512& hasher)
     hasher << &hasher << &RandAddStaticEnv << &malloc << &errno << &environ;
 
     // Hostname
+#ifdef WIN32
+    // Not using gethostname from Windows Sockets because
+    // the use of the Winsock DLL might be not initiated.
+    constexpr DWORD max_size = MAX_COMPUTERNAME_LENGTH + 1;
+    char hname[max_size];
+    DWORD size = max_size;
+    if (GetComputerName(hname, &size) != 0) {
+        hasher.Write((const unsigned char*)hname, size);
+    }
+#else
     char hname[256];
     if (gethostname(hname, 256) == 0) {
         hasher.Write((const unsigned char*)hname, strnlen(hname, 256));
     }
+#endif
 
 #if HAVE_DECL_GETIFADDRS && HAVE_DECL_FREEIFADDRS
     // Network interfaces
