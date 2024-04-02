399e182d138f7d57a077d6cae77230182bc73027
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-04-02 19:12:47 +0100

    fixup! cmake: Add `Maintenance` module
    Fix lint-shell-locale.py linter warning.

--- cmake/script/macos_zip.sh
+++ cmake/script/macos_zip.sh
@@ -3,6 +3,8 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or https://opensource.org/license/mit/.
 
+export LC_ALL=C
+
 if [ -n "$SOURCE_DATE_EPOCH" ]; then
   find . -exec touch -d "@$SOURCE_DATE_EPOCH" {} +
 fi
