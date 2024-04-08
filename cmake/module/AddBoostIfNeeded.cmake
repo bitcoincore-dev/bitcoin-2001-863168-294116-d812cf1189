e4739004a1b7e87b0c4a96a3084e48e2e1cdaa71
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-04-08 22:56:38 +0200

    Add CMake-based build system
    List of the squashed commits:
    =============================
    
    cmake: Add root `CMakeLists.txt` file
    
    cmake: Introduce core interface libraries to encapsulate common flags
    
    cmake: Add `config/bitcoin-config.h` support
    
    cmake: Check system headers
    
    cmake: Check system symbols
    
    cmake: Check compiler features
    
    cmake: Add position independent code support
    
    cmake: Add platform-specific definitions and properties
    
    cmake: Build `crc32c` static library
    
    cmake: Build `leveldb` static library
    
    cmake: Build `minisketch` static library
    
    cmake: Build `secp256k1` static library
    
    cmake: Build `univalue` static library
    
    cmake: Build `bitcoin_crypto` library
    
    cmake: Build `bitcoin_util` static library
    
    cmake: Build `bitcoin_consensus` library
    
    cmake: Build `bitcoind` executable
    
    depends: Amend handling flags environment variables
    
    build: Generate `share/toolchain.cmake` in depends
    
    cmake: Add cross-compiling support
    
    cmake: Add `TristateOption` module
    
    cmake: Add `ccache` support
    
    cmake: Add `libnatpmp` optional package support
    
    cmake: Add `libminiupnpc` optional package support
    
    cmake: Add `libzmq` optional package support
    
    cmake: Add `systemtap-sdt` optional package support
    
    cmake: Build `bitcoin-cli` executable
    
    cmake: Build `bitcoin-tx` executable
    
    cmake: Build `bitcoin-util` executable
    
    cmake: Add wallet functionality
    
    cmake: Add test config and runners
    
    cmake: Build `bench_bitcoin` executable
    
    cmake: Build `test_bitcoin` executable
    
    cmake: Include CTest
    
    cmake: Add `TryAppendCXXFlags` module
    
    cmake: Add `TryAppendLinkerFlag` module
    
    cmake: Add platform-specific compiler flags
    
    cmake: Add platform-specific linker flags
    
    cmake: Redefine/adjust per-configuration flags
    
    cmake: Add general compile options
    
    cmake: Add `HARDENING` option
    
    cmake: Add `REDUCE_EXPORTS` option
    
    cmake: Add `WERROR` option
    
    cmake: Implement `make install`
    
    cmake: Generate `obj/build.h` header
    
    Revert "build, qt: Do not install *.prl files"
    
    qt, build: Drop `QT_STATICPLUGIN` macro
    
    cmake: Build `bitcoin-qt` executable
    
    cmake: Build `test_bitcoin-qt` executable
    
    qt: Drop `Q_IMPORT_PLUGIN` macros
    
    cmake: Add `libqrencode` optional package support
    
    cmake: Add `SANITIZERS` option
    
    cmake: Add external signer support
    
    cmake: Add fuzzing options
    
    cmake: Add `AddWindowsResources` module
    
    cmake: Add `Maintenance` module
    
    cmake: Migrate Guix build scripts to CMake
    
    cmake: Add vcpkg manifest file
    
    cmake: Add preset for MSVC build
    
    Fix MSVC warning C4273 "inconsistent dll linkage"
    
    cmake, doc: Update `release-process.md`
    
    cmake: Add compiler diagnostic flags
    
    test: Fix MSVC warning C4101 "unreferenced local variable"
    
    cmake: Build `bitcoinkernel` library
    
    cmake: Build `bitcoin-chainstate` executable
    
    ci: Test CMake edge cases
    
    cmake, ci: Disable qt5 feature temporarily

---
+++ cmake/module/AddBoostIfNeeded.cmake
@@ -0,0 +1,108 @@
+# Copyright (c) 2023 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+function(add_boost_if_needed)
+  #[=[
+  TODO: Not all targets, which will be added in the future, require
+        Boost. Therefore, a proper check will be appropriate here.
+
+  Implementation notes:
+  Although only Boost headers are used to build Bitcoin Core,
+  we still leverage a standard CMake's approach to handle
+  dependencies, i.e., the Boost::headers "library".
+  A command target_link_libraries(target PRIVATE Boost::headers)
+  will propagate Boost::headers usage requirements to the target.
+  For Boost::headers such usage requirements is an include
+  directory and other added INTERFACE properties.
+  ]=]
+
+  set(Boost_NO_BOOST_CMAKE ON)
+  find_package(Boost 1.73.0 REQUIRED)
+  mark_as_advanced(Boost_INCLUDE_DIR)
+  set_target_properties(Boost::headers PROPERTIES IMPORTED_GLOBAL TRUE)
+  target_compile_definitions(Boost::headers INTERFACE
+    # We don't use multi_index serialization.
+    BOOST_MULTI_INDEX_DISABLE_SERIALIZATION
+  )
+  if(DEFINED VCPKG_TARGET_TRIPLET)
+    # Workaround for https://github.com/microsoft/vcpkg/issues/36955.
+    target_compile_definitions(Boost::headers INTERFACE
+      BOOST_NO_USER_CONFIG
+    )
+  endif()
+
+  if(BUILD_TESTS)
+    include(CheckCXXSourceCompiles)
+    set(CMAKE_REQUIRED_INCLUDES ${Boost_INCLUDE_DIR})
+    check_cxx_source_compiles("
+      #define BOOST_TEST_MAIN
+      #include <boost/test/included/unit_test.hpp>
+      " HAVE_BOOST_INCLUDED_UNIT_TEST_H
+    )
+    if(NOT HAVE_BOOST_INCLUDED_UNIT_TEST_H)
+      message(FATAL_ERROR "Building test_bitcoin executable requested but boost/test/included/unit_test.hpp header not available.")
+    endif()
+
+    check_cxx_source_compiles("
+      #define BOOST_TEST_MAIN
+      #include <boost/test/included/unit_test.hpp>
+      #include <boost/test/unit_test.hpp>
+      " HAVE_BOOST_UNIT_TEST_H
+    )
+    if(NOT HAVE_BOOST_UNIT_TEST_H)
+      message(FATAL_ERROR "Building test_bitcoin executable requested but boost/test/unit_test.hpp header not available.")
+    endif()
+  endif()
+
+  if(WITH_EXTERNAL_SIGNER)
+    include(CheckCXXSourceCompiles)
+    set(CMAKE_REQUIRED_INCLUDES ${Boost_INCLUDE_DIR})
+    # Boost 1.78 requires the following workaround.
+    # See: https://github.com/boostorg/process/issues/235
+    set(CMAKE_REQUIRED_FLAGS "-Wno-error=narrowing")
+    # Boost 1.73 requires the following workaround on systems with
+    # libc<2.34 only, as newer libc has integrated the functionality
+    # of the libpthread library.
+    set(CMAKE_REQUIRED_LIBRARIES Threads::Threads)
+    check_cxx_source_compiles("
+      #define BOOST_PROCESS_USE_STD_FS
+      #include <boost/process.hpp>
+      #include <string>
+
+      int main()
+      {
+        namespace bp = boost::process;
+        bp::opstream stdin_stream;
+        bp::ipstream stdout_stream;
+        bp::child c(\"dummy\", bp::std_out > stdout_stream, bp::std_err > stdout_stream, bp::std_in < stdin_stream);
+        stdin_stream << std::string{\"test\"} << std::endl;
+        if (c.running()) c.terminate();
+        c.wait();
+        c.exit_code();
+      }
+      " HAVE_BOOST_PROCESS
+    )
+    if(HAVE_BOOST_PROCESS)
+      if(WIN32)
+        # Boost Process for Windows uses Boost ASIO. Boost ASIO performs
+        # pre-main init of Windows networking libraries, which we do not
+        # want.
+        set(WITH_EXTERNAL_SIGNER OFF PARENT_SCOPE)
+        set(ENABLE_EXTERNAL_SIGNER OFF PARENT_SCOPE)
+      else()
+        set(WITH_EXTERNAL_SIGNER ON PARENT_SCOPE)
+        set(ENABLE_EXTERNAL_SIGNER ON PARENT_SCOPE)
+        target_compile_definitions(Boost::headers INTERFACE
+          BOOST_PROCESS_USE_STD_FS
+        )
+      endif()
+    elseif(WITH_EXTERNAL_SIGNER STREQUAL "AUTO")
+      set(WITH_EXTERNAL_SIGNER OFF PARENT_SCOPE)
+      set(ENABLE_EXTERNAL_SIGNER OFF PARENT_SCOPE)
+    else()
+      message(FATAL_ERROR "External signing is not supported for this Boost version.")
+    endif()
+  endif()
+
+endfunction()
