5bd592c003cfcee2be0f79f3d640784fedce7a1d
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-04-09 10:04:52 +0200

    ci: Migrate CI scripts to CMake

--- cmake/minisketch.cmake
+++ cmake/minisketch.cmake
@@ -57,6 +57,9 @@ if(HAVE_CLMUL)
       core_base_interface
       minisketch_common
   )
+  set_target_properties(minisketch_clmul PROPERTIES
+    EXPORT_COMPILE_COMMANDS OFF
+  )
 endif()
 
 add_library(minisketch STATIC EXCLUDE_FROM_ALL
@@ -81,4 +84,8 @@ target_link_libraries(minisketch
     core_base_interface
     minisketch_common
     $<TARGET_NAME_IF_EXISTS:minisketch_clmul>
+)
+
+set_target_properties(minisketch PROPERTIES
+  EXPORT_COMPILE_COMMANDS OFF
 )
