2400170f86a5806ddc9823575537575fa8979249
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-04-21 13:48:28 +0100

    Add CMake-based build system
    List of the squashed commits:
    =============================
    
    cmake: Add root `CMakeLists.txt` file
    
    cmake: Introduce core interface libraries to encapsulate common flags
    
    cmake: Add `config/bitcoin-config.h` support
    
    cmake: Check system headers
    
    cmake: Check system symbols
    
    cmake: Check compiler features
    
    cmake: Add position independent code support
    
    cmake: Add platform-specific definitions and properties
    
    cmake: Build `crc32c` static library
    
    cmake: Build `leveldb` static library
    
    cmake: Build `minisketch` static library
    
    cmake: Build `secp256k1` static library
    
    cmake: Build `univalue` static library
    
    cmake: Build `bitcoin_crypto` library
    
    cmake: Build `bitcoin_util` static library
    
    cmake: Build `bitcoin_consensus` library
    
    cmake: Build `bitcoind` executable
    
    depends: Amend handling flags environment variables
    
    build: Generate `share/toolchain.cmake` in depends
    
    cmake: Add cross-compiling support
    
    cmake: Add `TristateOption` module
    
    cmake: Add `ccache` support
    
    cmake: Add `libnatpmp` optional package support
    
    cmake: Add `libminiupnpc` optional package support
    
    cmake: Add `libzmq` optional package support
    
    cmake: Add `systemtap-sdt` optional package support
    
    cmake: Build `bitcoin-cli` executable
    
    cmake: Build `bitcoin-tx` executable
    
    cmake: Build `bitcoin-util` executable
    
    cmake: Add wallet functionality
    
    cmake: Add test config and runners
    
    cmake: Build `bench_bitcoin` executable
    
    cmake: Build `test_bitcoin` executable
    
    cmake: Include CTest
    
    cmake: Add `TryAppendCXXFlags` module
    
    cmake: Add `TryAppendLinkerFlag` module
    
    cmake: Add platform-specific compiler flags
    
    cmake: Add platform-specific linker flags
    
    cmake: Redefine/adjust per-configuration flags
    
    cmake: Add general compile options
    
    cmake: Add `HARDENING` option
    
    cmake: Add `REDUCE_EXPORTS` option
    
    cmake: Add `WERROR` option
    
    cmake: Implement `make install`
    
    cmake: Generate `obj/build.h` header
    
    Revert "build, qt: Do not install *.prl files"
    
    qt, build: Drop `QT_STATICPLUGIN` macro
    
    cmake: Build `bitcoin-qt` executable
    
    cmake: Build `test_bitcoin-qt` executable
    
    qt: Drop `Q_IMPORT_PLUGIN` macros
    
    cmake: Add `libqrencode` optional package support
    
    cmake: Add `SANITIZERS` option
    
    cmake: Add external signer support
    
    cmake: Add fuzzing options
    
    cmake: Add `AddWindowsResources` module
    
    cmake: Add `Maintenance` module
    
    cmake: Migrate Guix build scripts to CMake
    
    cmake: Add vcpkg manifest file
    
    cmake: Add preset for MSVC build
    
    Fix MSVC warning C4273 "inconsistent dll linkage"
    
    cmake, doc: Update `release-process.md`
    
    cmake: Add compiler diagnostic flags
    
    test: Fix MSVC warning C4101 "unreferenced local variable"
    
    cmake: Build `bitcoinkernel` library
    
    cmake: Build `bitcoin-chainstate` executable
    
    cmake, docs: Update MSVC build docs
    
    cmake: Remove legacy MSVC build system
    
    depends: Bump libmultiprocess source
    
    cmake: Add `MULTIPROCESS` option
    
    ci: Test CMake edge cases

---
+++ cmake/module/AddTargetMacros.cmake
@@ -0,0 +1,51 @@
+# Copyright (c) 2024-present The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or https://opensource.org/license/mit/.
+
+include_guard(GLOBAL)
+
+# Functions in this module are drop-in replacements for CMake's add_library
+# and add_executable functions. They are mandatory for use in the Bitcoin
+# Core project, except for imported and interface libraries.
+
+function(subtree_add_library name)
+  if(NOT name MATCHES "^crc32c.*|^leveldb.*|^minisketch.*|^secp256k1.*")
+    message(FATAL_ERROR "${CMAKE_CURRENT_FUNCTION}() was called to add a non-subtree target \"${name}\".")
+  endif()
+  cmake_parse_arguments(PARSE_ARGV 1 FWD "" "" "")
+  add_library("${name}" ${FWD_UNPARSED_ARGUMENTS})
+  target_link_libraries("${name}" PRIVATE core_base_interface)
+  cmake_language(EVAL CODE "
+    cmake_language(DEFER
+      DIRECTORY ${PROJECT_SOURCE_DIR}
+      CALL target_link_libraries ${name} PRIVATE extra_flags_interface
+    )
+  ")
+endfunction()
+
+function(bitcoincore_add_library name)
+  if(name MATCHES "^crc32c.*|^leveldb.*|^minisketch.*|^secp256k1.*")
+    message(FATAL_ERROR "${CMAKE_CURRENT_FUNCTION}() was called to add a subtree target \"${name}\".")
+  endif()
+  cmake_parse_arguments(PARSE_ARGV 1 FWD "" "" "")
+  add_library("${name}" ${FWD_UNPARSED_ARGUMENTS})
+  target_link_libraries("${name}" PRIVATE core_interface)
+  cmake_language(EVAL CODE "
+    cmake_language(DEFER
+      DIRECTORY ${PROJECT_SOURCE_DIR}
+      CALL target_link_libraries ${name} PRIVATE extra_flags_interface
+    )
+  ")
+endfunction()
+
+function(bitcoincore_add_executable name)
+  cmake_parse_arguments(PARSE_ARGV 1 FWD "" "" "")
+  add_executable("${name}" ${FWD_UNPARSED_ARGUMENTS})
+  target_link_libraries("${name}" PRIVATE core_interface)
+  cmake_language(EVAL CODE "
+    cmake_language(DEFER
+      DIRECTORY ${PROJECT_SOURCE_DIR}
+      CALL target_link_libraries ${name} PRIVATE extra_flags_interface
+    )
+  ")
+endfunction()
---
+++ cmake/optional.cmake
@@ -0,0 +1,221 @@
+# Copyright (c) 2023 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+# Optional features and packages.
+
+if(CCACHE)
+  set(ccache_hints)
+  if(MSVC AND EXISTS "$ENV{ChocolateyInstall}")
+    # Bypass a shim executable provided by Chocolatey.
+    # See https://docs.chocolatey.org/en-us/features/shim
+    file(GLOB ccache_hints "$ENV{ChocolateyInstall}/lib/ccache/tools/ccache-*")
+  endif()
+  find_program(CCACHE_COMMAND ccache HINTS ${ccache_hints})
+  unset(ccache_hints)
+
+  if(CCACHE_COMMAND)
+    if(MSVC)
+      if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.24)
+        # ccache >= 4.8 requires compile batching turned off that is available since CMake 3.24.
+        # See https://github.com/ccache/ccache/wiki/MS-Visual-Studio
+        set(CCACHE ON)
+        file(COPY_FILE ${CCACHE_COMMAND} ${CMAKE_BINARY_DIR}/cl.exe ONLY_IF_DIFFERENT)
+        list(APPEND CMAKE_VS_GLOBALS
+          "CLToolExe=cl.exe"
+          "CLToolPath=${CMAKE_BINARY_DIR}"
+          "DebugInformationFormat=OldStyle"
+        )
+        set(CMAKE_VS_NO_COMPILE_BATCHING ON)
+        # By default Visual Studio generators will use /Zi which is not compatible
+        # with ccache, so tell Visual Studio to use /Z7 instead.
+        set(CMAKE_MSVC_DEBUG_INFORMATION_FORMAT "$<$<CONFIG:Debug,RelWithDebInfo>:Embedded>")
+      elseif(CCACHE STREQUAL "AUTO")
+        message(WARNING "ccache requested and found, but CMake >= 3.24 is required to use it properly. Disabling.\n"
+                        "To skip ccache check, use \"-DCCACHE=OFF\".\n")
+        set(CCACHE OFF)
+      else()
+        message(FATAL_ERROR "ccache requested and found, but CMake >= 3.24 is required to use it properly.")
+      endif()
+    else()
+      set(CCACHE ON)
+      list(APPEND CMAKE_C_COMPILER_LAUNCHER ${CCACHE_COMMAND})
+      list(APPEND CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE_COMMAND})
+    endif()
+  elseif(CCACHE STREQUAL "AUTO")
+    set(CCACHE OFF)
+  else()
+    message(FATAL_ERROR "ccache requested, but not found.")
+  endif()
+  mark_as_advanced(CCACHE_COMMAND)
+endif()
+
+if(WITH_NATPMP)
+  find_package(NATPMP MODULE)
+  if(NATPMP_FOUND)
+    set(WITH_NATPMP ON)
+  elseif(WITH_NATPMP STREQUAL "AUTO")
+    message(WARNING "libnatpmp not found, disabling.\n"
+                    "To skip libnatpmp check, use \"-DWITH_NATPMP=OFF\".\n")
+    set(WITH_NATPMP OFF)
+  else()
+    message(FATAL_ERROR "libnatpmp requested, but not found.")
+  endif()
+endif()
+
+if(WITH_MINIUPNPC)
+  find_package(MiniUPnPc MODULE)
+  if(MiniUPnPc_FOUND)
+    set(WITH_MINIUPNPC ON)
+  elseif(WITH_MINIUPNPC STREQUAL "AUTO")
+    message(WARNING "libminiupnpc not found, disabling.\n"
+                    "To skip libminiupnpc check, use \"-DWITH_MINIUPNPC=OFF\".\n")
+    set(WITH_MINIUPNPC OFF)
+  else()
+    message(FATAL_ERROR "libminiupnpc requested, but not found.")
+  endif()
+endif()
+
+if(WITH_ZMQ)
+  if(MSVC)
+    find_package(ZeroMQ CONFIG)
+  else()
+    # The ZeroMQ project has provided config files since v4.2.2.
+    # TODO: Switch to find_package(ZeroMQ) at some point in the future.
+    include(CrossPkgConfig)
+    cross_pkg_check_modules(libzmq IMPORTED_TARGET libzmq>=4)
+    if(libzmq_FOUND AND TARGET PkgConfig::libzmq)
+      target_compile_definitions(PkgConfig::libzmq INTERFACE
+        $<$<PLATFORM_ID:Windows>:ZMQ_STATIC>
+      )
+      target_link_libraries(PkgConfig::libzmq INTERFACE
+        $<$<PLATFORM_ID:Windows>:iphlpapi;ws2_32>
+      )
+    endif()
+  endif()
+  if(TARGET libzmq OR TARGET PkgConfig::libzmq)
+    set(WITH_ZMQ ON)
+  elseif(WITH_ZMQ STREQUAL "AUTO")
+    message(WARNING "libzmq not found, disabling.\n"
+                    "To skip libzmq check, use \"-DWITH_ZMQ=OFF\".\n")
+    set(WITH_ZMQ OFF)
+  else()
+    message(FATAL_ERROR "libzmq requested, but not found.")
+  endif()
+endif()
+
+if(WITH_USDT)
+  find_path(SystemTap_INCLUDE_DIR
+    NAMES sys/sdt.h
+  )
+  mark_as_advanced(SystemTap_INCLUDE_DIR)
+
+  if(SystemTap_INCLUDE_DIR)
+    include(CMakePushCheckState)
+    cmake_push_check_state(RESET)
+
+    include(CheckCXXSourceCompiles)
+    set(CMAKE_REQUIRED_INCLUDES ${SystemTap_INCLUDE_DIR})
+    check_cxx_source_compiles("
+      #include <sys/sdt.h>
+
+      int main()
+      {
+        DTRACE_PROBE(context, event);
+        int a, b, c, d, e, f, g;
+        DTRACE_PROBE7(context, event, a, b, c, d, e, f, g);
+      }
+      " HAVE_USDT_H
+    )
+
+    cmake_pop_check_state()
+  endif()
+
+  if(HAVE_USDT_H)
+    target_include_directories(core_interface INTERFACE
+      ${SystemTap_INCLUDE_DIR}
+    )
+    set(ENABLE_TRACING TRUE)
+    set(WITH_USDT ON)
+  elseif(WITH_USDT STREQUAL "AUTO")
+    set(WITH_USDT OFF)
+  else()
+    message(FATAL_ERROR "sys/sdt.h requested, but not found.")
+  endif()
+endif()
+
+if(ENABLE_WALLET)
+  if(WITH_SQLITE)
+    if(VCPKG_TARGET_TRIPLET)
+      # Use of the `unofficial::` namespace is a vcpkg package manager convention.
+      find_package(unofficial-sqlite3 CONFIG)
+    else()
+      find_package(SQLite3 3.7.17)
+    endif()
+    if(TARGET unofficial::sqlite3::sqlite3 OR TARGET SQLite::SQLite3)
+      set(WITH_SQLITE ON)
+      set(USE_SQLITE ON)
+    elseif(WITH_SQLITE STREQUAL "AUTO")
+      set(WITH_SQLITE OFF)
+    else()
+      message(FATAL_ERROR "SQLite requested, but not found.")
+    endif()
+  endif()
+
+  if(WITH_BDB)
+    find_package(BerkeleyDB 4.8 MODULE)
+    if(BerkeleyDB_FOUND)
+      set(WITH_BDB ON)
+      set(USE_BDB ON)
+      if(NOT BerkeleyDB_VERSION VERSION_EQUAL 4.8)
+        message(WARNING "Found Berkeley DB (BDB) other than 4.8.")
+        if(WARN_INCOMPATIBLE_BDB)
+          message(WARNING "BDB (legacy) wallets opened by this build would not be portable!\n"
+                          "If this is intended, pass \"-DWARN_INCOMPATIBLE_BDB=OFF\".\n"
+                          "Passing \"-DWITH_BDB=OFF\" will suppress this warning.\n")
+        else()
+          message(WARNING "BDB (legacy) wallets opened by this build will not be portable!")
+        endif()
+      endif()
+    else()
+      message(WARNING "Berkeley DB (BDB) required for legacy wallet support, but not found.\n"
+                      "Passing \"-DWITH_BDB=OFF\" will suppress this warning.\n")
+      set(WITH_BDB OFF)
+    endif()
+  endif()
+else()
+  set(WITH_SQLITE OFF)
+  set(WITH_BDB OFF)
+endif()
+
+if(WITH_GUI AND WITH_QRENCODE)
+  if(MSVC)
+    # TODO: vcpkg fails to build libqrencode package due to
+    #       the build error in its libiconv dependency.
+    #       See: https://github.com/microsoft/vcpkg/issues/36924.
+  else()
+    cross_pkg_check_modules(libqrencode libqrencode IMPORTED_TARGET)
+  endif()
+  if(TARGET PkgConfig::libqrencode)
+    set_target_properties(PkgConfig::libqrencode PROPERTIES
+      INTERFACE_COMPILE_DEFINITIONS USE_QRCODE
+    )
+    set(WITH_QRENCODE ON)
+  elseif(WITH_QRENCODE STREQUAL "AUTO")
+    set(WITH_QRENCODE OFF)
+  else()
+    message(FATAL_ERROR "libqrencode requested, but not found.")
+  endif()
+endif()
+
+if(MULTIPROCESS)
+  find_package(Libmultiprocess CONFIG)
+  find_package(LibmultiprocessGen CONFIG)
+  if(TARGET Libmultiprocess::multiprocess AND TARGET Libmultiprocess::mpgen)
+    set(MULTIPROCESS ON)
+  elseif(MULTIPROCESS STREQUAL "AUTO")
+    set(MULTIPROCESS OFF)
+  else()
+    message(FATAL_ERROR "\"-DMULTIPROCESS=ON\" specified, but libmultiprocess library was not found.")
+  endif()
+endif()
