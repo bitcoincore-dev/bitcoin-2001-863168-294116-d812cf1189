1b26d5b3bf501174b9d51fd5c37d6e01f5017204
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-04-27 09:40:56 +0100

    cmake [KILL 3-STATE]: Replace WITH_GUI with BUILD_GUI w/ default OFF

---
+++ cmake/module/FindQt.cmake
@@ -0,0 +1,59 @@
+# Copyright (c) 2024-present The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or https://opensource.org/license/mit/.
+
+#[=======================================================================[
+FindQt
+------
+
+Finds the Qt headers and libraries.
+
+This is a wrapper around find_package command that facilitates searching
+in various build environments.
+
+#]=======================================================================]
+
+set(_qt_homebrew_prefix)
+if(CMAKE_HOST_APPLE)
+  find_program(HOMEBREW_EXECUTABLE brew REQUIRED)
+  execute_process(
+    COMMAND ${HOMEBREW_EXECUTABLE} --prefix qt@5
+    OUTPUT_VARIABLE _qt_homebrew_prefix
+    ERROR_QUIET
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+endif()
+
+# Save CMAKE_FIND_ROOT_PATH_MODE_LIBRARY state.
+unset(_qt_find_root_path_mode_library_saved)
+if(DEFINED CMAKE_FIND_ROOT_PATH_MODE_LIBRARY)
+  set(_qt_find_root_path_mode_library_saved ${CMAKE_FIND_ROOT_PATH_MODE_LIBRARY})
+endif()
+
+# The find_package(Qt) function internally uses find_library() calls for
+# all dependencies to ensure their availability. In turn, the find_library()
+# inspects the well-known locations on the file system; therefore, it must
+# be able to find platform-specific system libraries, for example:
+# /usr/x86_64-w64-mingw32/lib/libm.a or /usr/arm-linux-gnueabihf/lib/libm.a.
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
+
+find_package(Qt5 ${Qt_FIND_VERSION}
+  COMPONENTS ${Qt_FIND_COMPONENTS}
+  HINTS ${_qt_homebrew_prefix}
+  PATH_SUFFIXES Qt5  # Required on OpenBSD systems.
+)
+unset(_qt_homebrew_prefix)
+
+# Restore CMAKE_FIND_ROOT_PATH_MODE_LIBRARY state.
+if(DEFINED _qt_find_root_path_mode_library_saved)
+  set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ${_qt_find_root_path_mode_library_saved})
+  unset(_qt_find_root_path_mode_library_saved)
+else()
+  unset(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY)
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Qt
+  REQUIRED_VARS Qt5_DIR
+  VERSION_VAR Qt5_VERSION
+)
