5989b1f5924262639842151c41bc77397dca9711
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-05-06 15:27:31 +0100

    cmake: Add `Coverage` and `CoverageFuzz` scripts

---
+++ cmake/script/CoverageFuzz.cmake
@@ -0,0 +1,38 @@
+# Copyright (c) 2024-present The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or https://opensource.org/license/mit/.
+
+include(${CMAKE_CURRENT_LIST_DIR}/CoverageInclude.cmake)
+
+if(NOT DEFINED FUZZ_SEED_CORPUS_DIR)
+  set(FUZZ_SEED_CORPUS_DIR ${CMAKE_CURRENT_SOURCE_DIR}/qa-assets/fuzz_seed_corpus)
+endif()
+
+execute_process(
+  COMMAND ${CMAKE_COMMAND} -E env TIMEOUT=15 test/fuzz/test_runner.py ${FUZZ_SEED_CORPUS_DIR} --loglevel DEBUG
+  WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+)
+execute_process(
+  COMMAND ${LCOV_COMMAND} --capture --directory src --test-name fuzz-tests --output-file fuzz.info
+  WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+)
+execute_process(
+  COMMAND ${LCOV_COMMAND} --zerocounters --directory src
+  WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+)
+execute_process(
+  COMMAND ${LCOV_FILTER_COMMAND} fuzz.info fuzz_filtered.info
+  WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+)
+execute_process(
+  COMMAND ${LCOV_COMMAND} --add-tracefile fuzz_filtered.info --output-file fuzz_filtered.info
+  WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+)
+execute_process(
+  COMMAND ${LCOV_COMMAND} --add-tracefile baseline_filtered.info --add-tracefile fuzz_filtered.info --output-file fuzz_coverage.info
+  WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+)
+execute_process(
+  COMMAND ${GENHTML_COMMAND} fuzz_coverage.info --output-directory fuzz.coverage
+  WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+)
