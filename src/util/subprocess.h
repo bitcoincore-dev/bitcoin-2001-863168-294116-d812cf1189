e3e31446c308a14051df485e0376107d5d1eafaf
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-05-09 17:43:32 +0100

    test, subprocess: Improve coverage report correctness
    See: https://gcc.gnu.org/onlinedocs/gcc/Gcov-and-Optimization.html

--- src/util/subprocess.h
+++ src/util/subprocess.h
@@ -75,6 +75,10 @@ extern "C" {
 #else
   #include <sys/wait.h>
   #include <unistd.h>
+#ifdef CODE_COVERAGE_ENABLED
+void __gcov_dump(void);
+void __gcov_reset(void);
+#endif
 #endif
   #include <csignal>
   #include <fcntl.h>
@@ -1353,6 +1357,11 @@ namespace detail {
       if (stream.err_write_ != -1 && stream.err_write_ > 2)
         close(stream.err_write_);
 
+#ifdef CODE_COVERAGE_ENABLED
+      __gcov_dump();
+      __gcov_reset();
+#endif
+
       // Replace the current image with the executable
       sys_ret = execvp(parent_->exe_name_.c_str(), parent_->cargv_.data());
 
@@ -1365,6 +1374,11 @@ namespace detail {
       //ATTN: Can we do something on error here ?
       util::write_n(err_wr_pipe_, err_msg.c_str(), err_msg.length());
     }
+
+#ifdef CODE_COVERAGE_ENABLED
+      __gcov_dump();
+      __gcov_reset();
+#endif
 
     // Calling application would not get this
     // exit failure
