0de7c1e03002975c4f59350c1236b90820c20a2e
Luke Dashjr <luke-jr+git@utopios.org>
2024-06-21 19:01:21 +0000

    CI: i686_multiprocess: Use clang-15
    Needed to get past Clang 14 optimization bug: https://github.com/llvm/llvm-project/issues/96267

--- ci/test/00_setup_env_i686_multiprocess.sh
+++ ci/test/00_setup_env_i686_multiprocess.sh
@@ -9,9 +9,9 @@ export LC_ALL=C.UTF-8
 export HOST=i686-pc-linux-gnu
 export CONTAINER_NAME=ci_i686_multiprocess
 export CI_IMAGE_NAME_TAG="docker.io/amd64/ubuntu:22.04"
-export PACKAGES="llvm clang g++-multilib"
+export PACKAGES="llvm-15 clang-15 g++-multilib"
 export DEP_OPTS="DEBUG=1 MULTIPROCESS=1"
 export GOAL="install"
-export BITCOIN_CONFIG="--enable-debug CC='clang -m32' CXX='clang++ -m32' \
+export BITCOIN_CONFIG="--enable-debug CC='clang-15 -m32' CXX='clang++-15 -m32' \
 CPPFLAGS='-DBOOST_MULTI_INDEX_ENABLE_SAFE_MODE'"
 export BITCOIND=bitcoin-node  # Used in functional tests
