dcafbb8fc87211f44912280bf3fdf8b742754fba
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-07-04 01:15:07 +0100

    cmake: crc32c fix

--- cmake/crc32c.cmake
+++ cmake/crc32c.cmake
@@ -104,7 +104,7 @@ if(HAVE_SSE42)
   add_library(crc32c_sse42 STATIC EXCLUDE_FROM_ALL
     ${PROJECT_SOURCE_DIR}/src/crc32c/src/crc32c_sse42.cc
   )
-  target_compile_definitions(crc32c_sse42 PUBLIC HAVE_SSE42=1)
+  target_compile_definitions(crc32c_sse42 PUBLIC HAVE_SSE42=$<BOOL:${HAVE_SSE42}>)
   target_compile_options(crc32c_sse42 PRIVATE ${SSE42_CXXFLAGS})
   target_link_libraries(crc32c_sse42 PRIVATE crc32c_common)
   set_target_properties(crc32c_sse42 PROPERTIES EXPORT_COMPILE_COMMANDS OFF)
@@ -115,7 +115,7 @@ if(HAVE_ARM64_CRC32C)
   add_library(crc32c_arm64 STATIC EXCLUDE_FROM_ALL
     ${PROJECT_SOURCE_DIR}/src/crc32c/src/crc32c_arm64.cc
   )
-  target_compile_definitions(crc32c_arm64 PUBLIC HAVE_ARM64_CRC32C=1)
+  target_compile_definitions(crc32c_arm64 PUBLIC HAVE_ARM64_CRC32C=$<BOOL:${HAVE_ARM64_CRC32C}>)
   target_compile_options(crc32c_arm64 PRIVATE ${ARM64_CRC_CXXFLAGS})
   target_link_libraries(crc32c_arm64 PRIVATE crc32c_common)
   set_target_properties(crc32c_arm64 PROPERTIES EXPORT_COMPILE_COMMANDS OFF)
